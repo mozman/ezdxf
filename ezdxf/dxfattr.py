66d19fedccee3c0df3f2ec9b1b60efdf575d8aed
mozman <mozman@gmx.at>
2011-04-28 15:23:21 +0200

    added tests for dxfattr

--- ezdxf/dxfattr.py
+++ ezdxf/dxfattr.py
@@ -8,18 +8,23 @@ from collections import namedtuple
 
 DXFAttr = namedtuple('DXFAttr', 'code xtype')
 DXFAttr3 = namedtuple('DXFAttr3', 'code xtype subclass')
-DefSubclass = namedtuple('SubclassDef', 'name attribs')
+DefSubclass = namedtuple('DefSubclass', 'name attribs')
 
 class DXFAttributes:
     def __init__(self, *subclassdefs):
-        def add(subclass, index):
-            for name, dxfattrib in subclass.attribs.items():
-                self._attribs[name] = DXFAttr3(dxfattrib.code, dxfattrib.xtype, index)
-
-        self._defs = subclassdefs
+        self._subclasses = []
         self._attribs = {}
-        for index, subclass in enumerate(self._defs):
-            add(subclass, index)
+        for subclass in subclassdefs:
+            self.add_subclass(subclass)
+            
+    def add_subclass(self, subclass):
+        subclass_index = len(self._subclasses)
+        self._subclasses.append(subclass)
+        self._add_subclass_attribs(subclass, subclass_index)
+        
+    def _add_subclass_attribs(self, subclass, subclass_index):
+        for name, dxfattrib in subclass.attribs.items():
+            self._attribs[name] = DXFAttr3(dxfattrib.code, dxfattrib.xtype, subclass_index)
 
     def __getitem__(self, name):
         return self._attribs[name]
@@ -29,15 +34,7 @@ class DXFAttributes:
 
     def keys(self):
         return self._attribs.keys()
-
+    
     def subclasses(self):
-        return iter(self._defs)
-
-    def subclass(self, pos):
-        return self._defs[pos]
-
-    def index(self, name, start=0):
-        for pos, subclass in enumerate(self._defs):
-            if pos >= start and subclass.name == name:
-                return pos
-        raise ValueError("subclass '%s' not found." % name)
+        return iter(self._subclasses)
+    
---
+++ tests/test_dxfattr.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python
+#coding:utf-8
+# Author:  mozman --<mozman@gmx.at>
+# Purpose: test dxfattr
+# Created: 2011-04-28
+
+import sys
+import unittest
+
+from ezdxf.entity import GenericWrapper
+from ezdxf.tags import ExtendedTags
+from ezdxf.dxfattr import DXFAttr, DefSubclass, DXFAttributes
+
+XTEMPLATE = """  0
+LINE
+  5
+0
+330
+0
+100
+AcDbEntity
+  8
+0
+100
+AcDbLine
+ 10
+0.0
+ 20
+0.0
+ 30
+0.0
+ 11
+1.0
+ 21
+1.0
+ 31
+1.0
+"""
+
+class AttributeChecker(GenericWrapper):
+    TEMPLATE = XTEMPLATE
+    DXFATTRIBS = DXFAttributes(
+        DefSubclass(None, {
+            'handle': DXFAttr(5, None),
+            'block_record': DXFAttr(330, None),
+            }), 
+        DefSubclass('AcDbEntity', {
+            'paperspace': DXFAttr(67, None),
+            'layer': DXFAttr(8, None),
+            'linetype': DXFAttr(6, None),
+            'ltscale': DXFAttr(48, None),
+            'invisible': DXFAttr(60, None),
+            'color': DXFAttr(62, None),
+            }), 
+        DefSubclass('AcDbLine', {
+            'start': DXFAttr(10, 'Point2D/3D'),
+            'end': DXFAttr(11, 'Point2D/3D'),
+            'thickness': DXFAttr(39, None),
+            'extrusion': DXFAttr(210, 'Point3D'),
+        }))
+
+class TestDXFAttributes(unittest.TestCase):
+    def setUp(self):
+        self.dxfattribs = AttributeChecker.DXFATTRIBS
+        
+    def test_init(self):
+        count = len(list(self.dxfattribs.subclasses()))
+        self.assertEqual(3, count)
+        
+class TestAttributeAccess(unittest.TestCase):
+    def setUp(self):
+        self.entity = AttributeChecker(ExtendedTags.fromtext(XTEMPLATE))
+        
+    def test_get_from_none_subclass(self):
+        self.assertEqual('0', self.entity.dxf.handle)
+        
+    def test_set_to_none_subclass(self):
+        self.entity.dxf.handle = 'ABCD'
+        self.assertEqual('ABCD', self.entity.dxf.handle)
+        
+    def test_get_from_AcDbEntity_subclass(self):
+        self.assertEqual('0', self.entity.dxf.layer)
+        
+    def test_set_to_AcDbEntity_subclass(self):
+        self.entity.dxf.layer = 'LAYER'
+        self.assertEqual('LAYER', self.entity.dxf.layer)
+        
+    def test_get_new_from_AcDbEntity_subclass(self):
+        self.assertEqual(7, self.entity.dxf.get('color', 7))
+        
+    def test_set_new_to_AcDbEntity_subclass(self):
+        self.entity.dxf.color = 7
+        self.assertEqual(7, self.entity.dxf.color)
+        
+    def test_get_from_AcDbLine_subclass(self):
+        self.assertEqual((0, 0, 0), self.entity.dxf.start)
+        
+if __name__=='__main__':
+    unittest.main()
