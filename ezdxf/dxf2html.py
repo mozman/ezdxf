0fd652f1730a18fb27bd839a8f80519786180063
Manfred Moitzi <mozman@gmx.at>
2013-05-20 19:52:21 +0200

    working on dxf2html()

--- ezdxf/dxf2html.py
+++ ezdxf/dxf2html.py
@@ -1,69 +1,91 @@
-# Purpose:
+# Purpose: Create a structured HTML view of the DXF tags - not a CAD drawing!
 # Created: 20.05.13
 # Copyright (C) 2013, Manfred Moitzi
-# License: GPLv3
+# License: MIT License
+"""Creates a structured HTML view of the DXF tags - not a CAD drawing!
+"""
+
 import sys
+import os
+
 from ezdxf import readfile
+from ezdxf.c23 import escape, ustr
+
 
 HTML_TEMPLATE = """<!DOCTYPE html>
 <html>
 <head>
 <meta charset="utf-8">
-<title>{title}</title>
+<link rel="stylesheet" href="{name}.css">
+<title>{name}.dxf</title>
 </head>
 <body>
+<div class="dxf-file">
+<h1>DXF-FILE: {name}.dxf</h1>
 {body}
+</div>
 </body>
 """
+TAG_TEMPLATE = '<div class="dxf-tag"><span class="tag-code">{code}</span><span class="tag-value">{value}</span></div>'
+HDRVAR_TEMPLATE = '<div class="dxf-hdrvar"><span class="var-name">{name}</span> = <span class="var-value">{value}</span></div>'
 
 def get_html_body(drawing):
     def section2html(section):
         if section.name == 'header':
-            return section_info(section) + hdrvars2html(section.hdrvars)
+            return section_info(section).format(hdrvars2html(section.hdrvars))
         elif section.name in ('classes', 'objects', 'entities'):
-            return section_info(section) + entities2html(iter(section))
+            return section_info(section).format(entities2html(iter(section)))
         elif section.name == 'tables':
-            return section_info(section) + tables2html(iter(section))
+            return section_info(section).format(tables2html(iter(section)))
         elif section.name == 'blocks':
-            return section_info(section) + blocks2html(iter(section))
+            return section_info(section).format(blocks2html(iter(section)))
         else:
-            return section_info(section) + tags2html(section.tags)
+            return section_info(section).format(tags2html(section.tags))
 
     def section_info(section):
-        return "<p>SECTION-NAME: {}</p>\n".format(section.name)
+        return '<div class="dxf-section"><h2>SECTION: {}</h2>\n{{}}\n</div>\n'.format(section.name)
 
     def hdrvars2html(hdrvars):
-        return "<p>CONTENT: hdrvars<p>"
+        def var2str(hdrvar):
+            if hdrvar.ispoint:
+                return  ustr(hdrvar.getpoint())
+            else:
+                return escape(ustr(hdrvar.value))
+
+        varstrings = [HDRVAR_TEMPLATE.format(name=name, value=var2str(value)) for name, value in hdrvars.items()]
+        return '<div class="dxf-hdrvars">\n{}\n</div>'.format("\n".join(varstrings))
 
     def tags2html(tags):
-        return "<p>CONTENT: tags<p>"
+        return '<div class="dxf-tags">CONTENT: tags</div>'
 
     def entities2html(entities):
-        return "<p>CONTENT: entities<p>"
+        return '<div class="dxf-entities">CONTENT: entities</div>'
 
     def tables2html(tables):
-        return "<p>CONTENT: tables<p>"
+        return '<div class="dxf-tables">CONTENT: tables</div>'
 
-    def blocks2html(tables):
-        return "<p>CONTENT: blocks<p>"
+    def blocks2html(blocks):
+        return '<div class="dxf-blocks">CONTENT: blocks</div>'
 
     sections = [section2html(section) for section in drawing.sections]
     return "\n".join(sections)
 
-def dxf2html(dwg, outname=None):
-    body = get_html_body(dwg)
-    if outname is None:
+def dxf2html(dwg):
+    """ Creates a structured HTML view of the DXF tags - not a CAD drawing!
+    """
+    def get_name():
         if dwg.filename is None:
-            outname = "dxf2html.html"
+            return "unknown"
         else:
-            outname = dwg.filename + '.html'
+            filename = os.path.basename(dwg.filename)
+            return os.path.splitext(filename)[0]
 
-
-    with open(outname, mode='wt') as fp:
-        fp.write(HTML_TEMPLATE.format(title=outname, body=body))
+    return HTML_TEMPLATE.format(name=get_name(), body=get_html_body(dwg))
 
 if __name__ == "__main__":
-    filename = sys.argv[1]
-    dwg = readfile(filename)
-    dxf2html(dwg)
+    dwg = readfile(sys.argv[1])
+    html_filename = os.path.splitext(dwg.filename)[0] + '.html'
+    with open(html_filename, mode='wt') as fp:
+        fp.write(dxf2html(dwg))
+
 
