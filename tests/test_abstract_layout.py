d93cc4d0d70da10ddb54648b1c4e7e2b48d95afb
Manfred Moitzi <mozman@gmx.at>
2013-07-16 17:50:30 +0200

    refactorings

--- tests/test_abstract_layout.py
+++ tests/test_abstract_layout.py
@@ -41,7 +41,7 @@ class Host(BaseLayout):
     def _set_paperspace(self, entity):
         self.paperspace = True
 
-    def _get_entity_by_handle(self, handle):
+    def get_entity_by_handle(self, handle):
         entity = self._dxffactory.wrap_handle(handle)
         entity.set_builder(self)
         return entity
@@ -51,50 +51,46 @@ class TestAbstractLayout(unittest.TestCase):
     def setUp(self):
         self.host = Host(range(10))
 
-    def test_append_entity(self):
-        self.host._append_entity(Entity(4711))
-        self.assertEqual(4711, self.host._entityspace[-1])
-
     def test_insert_entities_begin(self):
         handles = [20, 21, 22, 23, 24]
         entities = [Entity(handle) for handle in handles]
-        self.host._insert_entities(0, entities)
+        self.host.insert_entities(0, entities)
         self.assertEqual(handles, self.host._entityspace[:5])
 
     def test_insert_entities_mid(self):
         handles = [20, 21, 22, 23, 24]
         entities = [Entity(handle) for handle in handles]
-        self.host._insert_entities(5, entities)
+        self.host.insert_entities(5, entities)
         self.assertEqual(handles, self.host._entityspace[5:10])
 
     def test_insert_entities_end(self):
         handles = [20, 21, 22, 23, 24]
         entities = [Entity(handle) for handle in handles]
-        self.host._insert_entities(15, entities)
+        self.host.insert_entities(15, entities)
         self.assertEqual(handles, self.host._entityspace[-5:])
 
     def test_get_position(self):
-        pos = self.host._get_index(Entity(5))
+        pos = self.host.get_index_of_entity(Entity(5))
         self.assertEqual(5, pos)
 
     def test_remove_entities_begin(self):
-        self.host._remove_entities(0, 3)
+        self.host.remove_entities(0, 3)
         self.assertEqual([3, 4, 5, 6, 7, 8, 9], self.host._entityspace)
 
     def test_remove_entities_mid(self):
-        self.host._remove_entities(5, 3)
+        self.host.remove_entities(5, 3)
         self.assertEqual([0, 1, 2, 3, 4, 8, 9], self.host._entityspace)
 
     def test_remove_entities_end(self):
-        self.host._remove_entities(8, 3)
+        self.host.remove_entities(8, 3)
         self.assertEqual([0, 1, 2, 3, 4, 5, 6, 7], self.host._entityspace)
 
     def test_get_entity(self):
-        entity = self.host._get_entity_at_index(7)
+        entity = self.host.get_entity_at_index(7)
         self.assertEqual(7, entity.dxf.handle)
 
     def test_build_entity(self):
-        entity = self.host._build_entity('TEST', {})
+        entity = self.host.build_entity('TEST', {})
         self.assertEqual('TEST', entity.dxf.handle)
         self.assertTrue(self.host.paperspace)
 
