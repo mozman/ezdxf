305512a30977786a70d722334f69a6275f89d652
Manfred Moitzi <mozman@gmx.at>
2014-04-17 08:30:46 +0200

    removed unused methods from BaseLayout()

--- tests/test_abstract_layout.py
+++ tests/test_abstract_layout.py
@@ -53,44 +53,6 @@ class TestAbstractLayout(unittest.TestCase):
     def setUp(self):
         self.host = Host(range(10))
 
-    def test_insert_entities_begin(self):
-        handles = [20, 21, 22, 23, 24]
-        entities = [Entity(handle) for handle in handles]
-        self.host.insert_entities(0, entities)
-        self.assertEqual(handles, self.host._entityspace[:5])
-
-    def test_insert_entities_mid(self):
-        handles = [20, 21, 22, 23, 24]
-        entities = [Entity(handle) for handle in handles]
-        self.host.insert_entities(5, entities)
-        self.assertEqual(handles, self.host._entityspace[5:10])
-
-    def test_insert_entities_end(self):
-        handles = [20, 21, 22, 23, 24]
-        entities = [Entity(handle) for handle in handles]
-        self.host.insert_entities(15, entities)
-        self.assertEqual(handles, self.host._entityspace[-5:])
-
-    def test_get_position(self):
-        pos = self.host.get_index_of_entity(Entity(5))
-        self.assertEqual(5, pos)
-
-    def test_remove_entities_begin(self):
-        self.host.remove_entities(0, 3)
-        self.assertEqual([3, 4, 5, 6, 7, 8, 9], self.host._entityspace)
-
-    def test_remove_entities_mid(self):
-        self.host.remove_entities(5, 3)
-        self.assertEqual([0, 1, 2, 3, 4, 8, 9], self.host._entityspace)
-
-    def test_remove_entities_end(self):
-        self.host.remove_entities(8, 3)
-        self.assertEqual([0, 1, 2, 3, 4, 5, 6, 7], self.host._entityspace)
-
-    def test_get_entity(self):
-        entity = self.host.get_entity_at_index(7)
-        self.assertEqual(7, entity.dxf.handle)
-
     def test_build_entity(self):
         entity = self.host.build_entity('TEST', {})
         self.assertEqual('TEST', entity.dxf.handle)
