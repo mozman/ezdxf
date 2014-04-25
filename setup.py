9ac24a23cc5470f494f020ce2d92db96f6972281
Manfred Moitzi <mozman@gmx.at>
2014-04-25 12:36:31 +0200

    removed explicit CPython3.3 tests

--- setup.py
+++ setup.py
@@ -41,7 +41,6 @@ setup(
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.3",
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: Implementation :: CPython",
         "Programming Language :: Python :: Implementation :: PyPy",
--- test.bat
+++ test.bat
@@ -2,8 +2,6 @@
 
 ECHO Running Python 2.7 tests
 CALL test27.bat
-ECHO Running Python 3.3 tests
-CALL test33.bat
 ECHO Running Python 3.4 tests
 CALL test34.bat
 ECHO Running pypy tests
