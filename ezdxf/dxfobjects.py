9948a97deec2555aee9aa776268cc591a503ada4
Manfred Moitzi <mozman@gmx.at>
2014-04-28 17:00:11 +0200

    added PLOTSETTINGS object

--- ezdxf/dxfobjects.py
+++ ezdxf/dxfobjects.py
@@ -6,23 +6,25 @@
 from __future__ import unicode_literals
 __author__ = "mozman <mozman@gmx.at>"
 
-from .tags import DXFTag, Tags
+from .tags import DXFTag
 from .classifiedtags import ClassifiedTags
 from .dxfattr import DXFAttr, DXFAttributes, DefSubclass
 from .dxfentity import DXFEntity
 
 ENTRY_NAME_CODE = 3
 
+none_subclass = DefSubclass(None, {
+    'handle': DXFAttr(5),
+    'owner': DXFAttr(330),
+})
+
 
 class DXFDictionary(DXFEntity):
     DXFATTRIBS = DXFAttributes(
-        DefSubclass(None, {
-            'handle': DXFAttr(5, None),
-            'parent': DXFAttr(330, None),
-        }),
+        none_subclass,
         DefSubclass('AcDbDictionary', {
-            'hard_owned': DXFAttr(280, None),
-            'cloning': DXFAttr(281, None),
+            'hard_owned': DXFAttr(280),
+            'cloning': DXFAttr(281),
         }),
     )
 
@@ -129,16 +131,13 @@ class DXFDictionary(DXFEntity):
 
 class DXFDictionaryWithDefault(DXFDictionary):
     DXFATTRIBS = DXFAttributes(
-        DefSubclass(None, {
-            'handle': DXFAttr(5, None),
-            'parent': DXFAttr(330, None),
-        }),
+        none_subclass,
         DefSubclass('AcDbDictionary', {
-            'hard_owned': DXFAttr(280, None),
-            'cloning': DXFAttr(281, None),
+            'hard_owned': DXFAttr(280),
+            'cloning': DXFAttr(281),
         }),
         DefSubclass('AcDbDictionaryWithDefault', {
-            'default': DXFAttr(340, None),
+            'default': DXFAttr(340),
         }),
     )
 
@@ -147,6 +146,111 @@ class DXFDictionaryWithDefault(DXFDictionary):
         value. Parameter *default* is always ignored!
         """
         return super(DXFDictionaryWithDefault, self).get(key, default=self.dxf.default)
+
+
+plot_settings_subclass = DefSubclass('AcDbPlotSettings', {
+    'page_setup_name': DXFAttr(1),
+    'plot_configuration_file': DXFAttr(2),
+    'paper_size': DXFAttr(4),
+    'plot_view_name': DXFAttr(6),
+    'left_margin': DXFAttr(40),  # in mm
+    'bottom_margin': DXFAttr(41),  # in mm
+    'right_margin': DXFAttr(42),  # in mm
+    'top_margin': DXFAttr(43),  # in mm
+    'paper_width': DXFAttr(44),  # in mm
+    'paper_height': DXFAttr(45),  # in mm
+    'plot_origin_x_offset': DXFAttr(46),  # in mm
+    'plot_origin_y_offset': DXFAttr(47),  # in mm
+    'plot_window_x1': DXFAttr(48),
+    'plot_window_y1': DXFAttr(49),
+    'plot_window_x2': DXFAttr(140),
+    'plot_window_y2': DXFAttr(141),
+    'custom_print_scale_numerator': DXFAttr(142),  # Numerator of custom print scale: real world (paper) units
+    'custom_print_scale_denominator': DXFAttr(142),  # Denominator of custom print scale: drawing units
+    'plot_layout_flags': DXFAttr(70),
+        # 1 = Plot Viewport Borders
+        # 2 = Show Plot Styles
+        # 4 = Plot Centered
+        # 8 = Plot Hidden
+        # 16 = Use Standard Scale
+        # 32 = Plot Plot Styles
+        # 64 = Scale Lineweights
+        # 128 = Print Lineweights
+        # 512 = Draw Viewports First
+        # 1024 = Model Type
+        # 2048 = Update Paper
+        # 4096 = Zoom To Paper On Update
+        # 8192 = Initializing
+        # 16384 = Prev PlotInit
+    'plot_paper_units': DXFAttr(72),  # 0 = Plot in inches; 1 = Plot in millimeters; 2 = Plot in pixels
+    'plot_rotation': DXFAttr(73),
+        # 0 = No rotation
+        # 1 = 90 degrees counterclockwise
+        # 2 = Upside-down
+        # 3 = 90 degrees clockwise
+    'plot_type': DXFAttr(74),
+        # 0 = Last screen display
+        # 1 = Drawing extents
+        # 2 = Drawing limits
+        # 3 = View specified by code 6
+        # 4 = Window specified by codes 48, 49, 140, and 141
+        # 5 = Layout information
+    'current_style_sheet': DXFAttr(7),
+    'standard_scale_type': DXFAttr(75),
+        # 0 = Scaled to Fit
+        # 1 = 1/128"=1'
+        # 2 = 1/64"=1'
+        # 3 = 1/32"=1'
+        # 4 = 1/16"=1'
+        # 5 = 3/32"=1'
+        # 6 = 1/8"=1'
+        # 7 = 3/16"=1'
+        # 8 = 1/4"=1'
+        # 9 = 3/8"=1'
+        # 10 = 1/2"=1'
+        # 11 = 3/4"=1'
+        # 12 = 1"=1'
+        # 13 = 3"=1'
+        # 14 = 6"=1'
+        # 15 = 1'=1'
+        # 16 = 1:1
+        # 17 = 1:2
+        # 18 = 1:4
+        # 19 = 1:8
+        # 20 = 1:10
+        # 21 = 1:16
+        # 22 = 1:20
+        # 23 = 1:30
+        # 24 = 1:40
+        # 25 = 1:50
+        # 26 = 1:100
+        # 27 = 2:1
+        # 28 = 4:1
+        # 29 = 8:1
+        # 30 = 10:1
+        # 31 = 100:1
+        # 32 = 1000:1
+    'shade_plot_mode': DXFAttr(76),  # 0 = As Displayed; 1 = Wireframe; 2 = Hidden; 3 = Rendered
+    'shade_plot_resolution_level': DXFAttr(77),
+        # 0 = Draft
+        # 1 = Preview
+        # 2 = Normal
+        # 3 = Presentation
+        # 4 = Maximum
+        # 5 = Custom
+    'shade_plot_custom_dpi': DXFAttr(78),
+    # Valid range: 100 to 32767, Only applied when the shade_plot_resolution level is set to 5 (Custom)
+    'scale_factor': DXFAttr(147),
+    # A floating point scale factor that represents the standard scale value specified in code 75
+    'paper_image_origin_x': DXFAttr(148),
+    'paper_image_origin_y': DXFAttr(149),
+    'shade_plot_handle': DXFAttr(333),
+})
+
+
+class PlotSettings(DXFEntity):
+    DXFATTRIBS = DXFAttributes(none_subclass, plot_settings_subclass)
+
 
 _LAYOUT_TPL = """  0
 LAYOUT
@@ -286,11 +390,8 @@ Layoutname
 class DXFLayout(DXFEntity):
     TEMPLATE = ClassifiedTags.from_text(_LAYOUT_TPL)
     DXFATTRIBS = DXFAttributes(
-        DefSubclass(None, {
-            'handle': DXFAttr(5,),
-            'owner': DXFAttr(330,),
-        }),
-        DefSubclass('AcDbPlotSettings', {}),
+        none_subclass,
+        plot_settings_subclass,
         DefSubclass('AcDbLayout', {
             'name': DXFAttr(1),  # layout name
             'flags': DXFAttr(70),
@@ -331,10 +432,7 @@ class DXFLayout(DXFEntity):
 
 class XRecord(DXFEntity):
     DXFATTRIBS = DXFAttributes(
-        DefSubclass(None, {
-            'handle': DXFAttr(5, None),
-            'owner': DXFAttr(330, None),
-        }),
+        none_subclass,
         DefSubclass('AcDbXrecord', {
             'cloning': DXFAttr(280, None),
         }),
