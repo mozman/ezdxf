fd3f31f177a5fcff098e2f9918ae201d0dadb12a
Manfred Moitzi <mozman@gmx.at>
2014-05-02 10:02:32 +0200

    started version 0.6.2; optimized ZipReader() class;

--- ezdxf/zipmanager.py
+++ ezdxf/zipmanager.py
@@ -14,22 +14,21 @@ from.tags import dxf_info
 
 
 class ZipReader(object):
-    def __init__(self, zip_archive_name, dxf_file_name=None):
+    def __init__(self, zip_archive_name):
         if not zipfile.is_zipfile(zip_archive_name):
             raise IOError("'{}' is not a zip archive.".format(zip_archive_name))
         self.zip_achive_name = zip_archive_name
         self.zip_archive = None
-        self.dxf_file_name = dxf_file_name
+        self.dxf_file_name = None
         self.dxf_file = None
         self.encoding = 'cp1252'
 
-    def open(self):
+    def open(self, dxf_file_name=None):
         def open_dxf_file():
             return self.zip_archive.open(self.dxf_file_name, mode='rU')
 
         self.zip_archive = zipfile.ZipFile(self.zip_achive_name)
-        if self.dxf_file_name is None:
-            self.dxf_file_name = self.get_first_dxf_filename()
+        self.dxf_file_name = dxf_file_name if dxf_file_name is not None else self.get_first_dxf_file_name()
         self.dxf_file = open_dxf_file()
         if not self.is_dxf_file():
             raise IOError("'{}' is not a DXF file.".format(self.dxf_file_name))
@@ -37,11 +36,15 @@ class ZipReader(object):
         self.get_dxf_encoding()
         self.dxf_file = open_dxf_file()  # restart
 
-    def get_first_dxf_filename(self):
-        for name in self.zip_archive.namelist():
-            if name.lower().endswith('.dxf'):
-                return name
-        raise IOError("'{}' has no DXF files.")
+    def get_first_dxf_file_name(self):
+        dxf_file_names = self.get_dxf_file_names()
+        if len(dxf_file_names) > 0:
+            return dxf_file_names[0]
+        else:
+            raise IOError("'{}' has no DXF files.")
+
+    def get_dxf_file_names(self):
+        return [name for name in self.zip_archive.namelist() if name.lower().endswith('.dxf')]
 
     def is_dxf_file(self):
         # TODO: check for DXF file
@@ -51,6 +54,7 @@ class ZipReader(object):
         info = dxf_info(self)
         self.encoding = info.encoding
 
+    # interface to TagReader()
     def readline(self):
         next_line = self.dxf_file.readline()
         return ustr(next_line, self.encoding)
@@ -61,7 +65,7 @@ class ZipReader(object):
 
 @contextmanager
 def ctxZipReader(zip_achive, filename=None):
-    zip_reader = ZipReader(zip_achive, filename)
-    zip_reader.open()
+    zip_reader = ZipReader(zip_achive)
+    zip_reader.open(filename)
     yield zip_reader
     zip_reader.close()
