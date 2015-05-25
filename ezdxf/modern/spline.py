492cf161ae2a4e557cca0689ccd17d7030896c51
Manfred Moitzi <mozman@gmx.at>
2015-05-25 13:24:55 +0200

    HATCH: read/write polyline boundary path /wo tests

--- ezdxf/modern/spline.py
+++ ezdxf/modern/spline.py
@@ -1,4 +1,4 @@
-# Purpose: support for the AcDb1015 SPLINE entity
+# Purpose: support for the Ac1015 SPLINE entity
 # Created: 24.05.2015
 # Copyright (C) 2015, Manfred Moitzi
 # License: MIT License
