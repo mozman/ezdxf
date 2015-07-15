5e46063b94e31d462228c5f86393d9f040d62d01
Manfred Moitzi <mozman@gmx.at>
2015-07-15 18:41:23 +0200

    added GROUP entity (readonly)

--- examples/using_group.py
+++ examples/using_group.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 #coding:utf-8
 # Author:  mozman -- <mozman@gmx.at>
-# Purpose: using true color and transparency
-# Created: 04.07.2015
+# Purpose: using DXF groups
+# Created: 15.07.2015
 # Copyright (C) , Manfred Moitzi
 # License: MIT License
 
