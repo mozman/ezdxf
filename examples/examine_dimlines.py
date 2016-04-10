4da61b181b90a4983642377249f788f6d73ae4e8
mozman <mozman@gmx.at>
2016-04-10 16:43:53 +0200

    working on dimlines

---
+++ examples/examine_dimlines.py
@@ -0,0 +1,12 @@
+import os
+import ezdxf
+
+DIMPATH = r"D:\Source\dxftest\dimlines"
+
+dwg = ezdxf.readfile(os.path.join(DIMPATH, "linear_R2000.dxf"))
+
+for dimstyle in dwg.dimstyles:
+    print("Dimstyle: {}".format(dimstyle.dxf.name))
+    print("flags: {:x}".format(dimstyle.dxf.flags))
+    dimstyle.print()
+    print("-"*20)
