6b03d44c34a4a75c5775cfa7c01203c04deceefe
Manfred Moitzi <mozman@gmx.at>
2016-04-12 07:40:09 +0200

    added fast and simple DXF R12 file writer

---
+++ examples/using_fast_file_writer.py
@@ -0,0 +1,21 @@
+from random import random
+import ezdxf
+
+MAX_X_COORD = 1000.0
+MAX_Y_COORD = 1000.0
+CIRCLE_COUNT = 10000
+
+with ezdxf.fast_file_writer("quick_and_dirty_dxf_r12.dxf") as dxf:
+    dxf.add_line((0, 0), (17, 23))
+    dxf.add_circle((0, 0), radius=2)
+    dxf.add_arc((0, 0), radius=3, start=0, end=175)
+    dxf.add_solid([(0, 0), (1, 0), (0, 1), (1, 1)])
+    dxf.add_point((1.5, 1.5))
+    dxf.add_polyline([(5, 5), (7, 3), (7, 6)])  # 2d polyline
+    dxf.add_polyline([(4, 3, 2), (8, 5, 0), (2, 4, 9)])  # 3d polyline
+
+with ezdxf.fast_file_writer("much_circles.dxf") as dxf:
+    for i in range(CIRCLE_COUNT):
+        dxf.add_circle((MAX_X_COORD*random(), MAX_Y_COORD*random()), radius=2)
+
+
