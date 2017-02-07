43e28a03f15d5b4f536e4f69e7b6062f5d47b106
mozman <mozman@gmx.at>
2017-02-07 05:25:30 +0100

    added text style usage to text example

--- examples/tut/text/simple_text.py
+++ examples/tut/text/simple_text.py
@@ -3,7 +3,12 @@ import ezdxf
 dwg = ezdxf.new('AC1009')  # TEXT is a basic entity and exists in every DXF standard
 
 msp = dwg.modelspace()
+
 # use set_pos() for proper TEXT alignment - the relations between halign, valign, insert and align_point are tricky.
-msp.add_text("simple text").set_pos((2, 3), align='MIDDLE_RIGHT')
+msp.add_text("A Simple Text").set_pos((2, 3), align='MIDDLE_RIGHT')
+
+# using text styles
+dwg.styles.new('custom', dxfattribs={'font': 'arial.ttf', 'width': 0.8})  # Arial, default width factor of 0.8
+msp.add_text("Text Style Example", dxfattribs={'style': 'custom', 'height': 0.35}).set_pot((2, 6), align='LEFT')
 
 dwg.saveas("simple_text.dxf")
