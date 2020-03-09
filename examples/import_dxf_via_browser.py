1bc529c0ba1c94359ae86a7ce3a7928c90253030
Engineer_coding <truckpup@gmail.com>
2020-03-09 18:29:25 +1000

    example

---
+++ examples/import_dxf_via_browser.py
@@ -0,0 +1,37 @@
+def get_dxf_doc_from_upload_data(dxf_uploaded_base64_data):
+    """
+    This function turns the dxf data provided by Dash Plotly upload component into an EZDXF doc
+    Dash plotly upload compoment only provides base 64 encoded data.
+    This returns an ezdxf doc object
+    """
+    if dxf_uploaded_base64_data is not None:
+        # turn contents list into string. Contents is 64 encoded as per dash plotly
+        contents = str(dxf_uploaded_base64_data)
+        # remove the first part of the decoded info Raw Content
+        # example: data:application/vnd.ms-excel;base64,ICAwU0VDVElPTiAgMkhFQURFUiAgOSRBQ0FEVkVSICAxQUMxMDI0ICA5JEFDQURNQUlOVFZFUiA3MDYgIDkkRFdHQ09ERVBBR0
+        content_type, content_string = contents.split(',')
+        # Decode this into binary data string
+        binary_decoded_unformatted = base64.b64decode(content_string)
+        # fix the end of line problem for the EOF. ie remove the last \r\n so
+        # EZDXF can find the EOF:
+        binary_decoded_no_EOF = binary_decoded_unformatted.rstrip(b'\r\n')
+        # Change the \r\n encoding (from windows / 64 encoding) to \n (ezdxf compatible)
+        binary_decoded = binary_decoded_no_EOF.replace(b'\r\n', b'\n')
+        # decode this into unicode utf-8 which is the format required for dxf
+        utf_decoded = binary_decoded.decode("utf-8", errors="ignore")
+        # Create stringIO object to treat the data like a file and write the data to the object
+        # like a file opened in text mode https://docs.python.org/3/library/io.html
+        temp_tx_stream = io.StringIO(utf_decoded)
+        # Get info about the file
+        info = dxf_stream_info(temp_tx_stream)
+        encoding = info.encoding
+        # Re code the binary data using the encoding found in the "info":
+        decoded_data = binary_decoded.decode(encoding, errors="ignore")
+        # Create new text stream for use on the correctly decoded data:
+        tx_stream = io.StringIO(decoded_data)
+        # read the text stream
+        doc = ezdxf.read(tx_stream)
+        # Close the other stream IO objects used for reading the file contents
+        tx_stream.close()
+        temp_tx_stream.close()
+        return doc
