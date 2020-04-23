5b67ba23b1fa3a795c73d95e1f6ca75dff8aff33
mozman <me@mozman.at>
2020-04-23 07:51:22 +0200

    update NEWS and version.py for Bug fix release v0.12.1

--- src/ezdxf/version.py
+++ src/ezdxf/version.py
@@ -27,5 +27,5 @@
 #   1. bug fix release beta0: VERSION = "0.9.1b0"; version = (0, 9, 1, 'b0')
 #   2. bug fix release: VERSION = "0.9.2"; version = (0, 9, 2, 'release')
 
-version = (0, 12, 0, 'release')
-__version__ = "0.12"
+version = (0, 12, 1, 'b0')
+__version__ = "0.12.1b0"
