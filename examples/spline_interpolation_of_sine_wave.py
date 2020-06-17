b88c7c678d86d3dc7fb9f7721ae79a1cbd53c4fa
mozman <me@mozman.at>
2020-06-17 07:25:25 +0200

    refactor spline_interpolation_of_sine_wave.py

--- examples/spline_interpolation_of_sine_wave.py
+++ examples/spline_interpolation_of_sine_wave.py
@@ -16,7 +16,7 @@ def sine_wave(count: int, scale: float = 1.0) -> Iterable[Vector]:
 doc = ezdxf.new()
 msp = doc.modelspace()
 
-# Calculate 8 points on sin-curve as interpolation data
+# Calculate 8 points on sine wave as interpolation data
 data = list(sine_wave(count=8, scale=2.0))
 
 # Reference curve as fine approximation
@@ -25,7 +25,7 @@ msp.add_lwpolyline(sine_wave(count=800, scale=2.0), dxfattribs={'color': 1, 'lay
 # AutoCAD/BricsCAD interpolation
 msp.add_spline(data, dxfattribs={'layer': 'BricsCAD B-spline', 'color': 2})
 
-# ezdxf interpolation
+# tangent estimation method
 METHOD = 'diff'
 
 # create not normalized tangents (default is normalized)
@@ -35,13 +35,15 @@ tangents = estimate_tangents(data, METHOD, normalize=False)
 for p, t in zip(data, tangents):
     msp.add_line(p, p + t, dxfattribs={'color': 5, 'layer': f'Estimated tangents ({METHOD})'})
 
-# local interpolation require normalized tangents
+# local interpolation: a normalized tangent vector for each data point is required,
 s = local_cubic_bspline_interpolation(data, tangents=[t.normalize() for t in tangents])
+# or set argument 'method' for automatic tangent estimation, default method is '5-points' interpolation
+# s = local_cubic_bspline_interpolation(data, method=METHOD)
 msp.add_spline(dxfattribs={'color': 3, 'layer': f'Local interpolation ({METHOD})'}).apply_construction_tool(s)
 
-# global interpolation argument 'tangents' accepts only the start- and the end tangent
+# global interpolation: the optional argument 'tangents' accepts only start- and end tangent
 s = global_bspline_interpolation(data, tangents=(tangents[0], tangents[-1]))
 msp.add_spline(dxfattribs={'color': 4, 'layer': f'Global interpolation ({METHOD})'}).apply_construction_tool(s)
 
 doc.set_modelspace_vport(5, center=(4, 1))
-doc.saveas(DIR / 'sine-wave.dxf')
+doc.saveas(DIR / f'sine-wave-{METHOD}.dxf')
