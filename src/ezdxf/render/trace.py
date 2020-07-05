44e5f820907dc36c57f3b12dbff1458f30810c82
mozman <me@mozman.at>
2020-07-05 17:45:15 +0200

    rewrite trace.py
    Previous implementation wasn't capable to create expected
    bulge traces, start- and end segments were per definition
    slightly angled.

--- src/ezdxf/render/trace.py
+++ src/ezdxf/render/trace.py
@@ -1,32 +1,73 @@
 # Copyright (c) 2020, Manfred Moitzi
 # License: MIT License
 from typing import List, TYPE_CHECKING, Iterable, Tuple, Dict, Union, cast
+from abc import abstractmethod
 from collections import namedtuple
 from collections.abc import Sequence
 import math
 from ezdxf.math import Vec2, BSpline, linspace, ConstructionRay, ParallelRaysError, bulge_to_arc, ConstructionArc
 
 if TYPE_CHECKING:
-    from ezdxf.eztypes import Vertex, Solid, Trace, Face3d, Drawing, DXFGraphic, LWPolyline, Polyline
+    from ezdxf.eztypes import Vertex, Drawing, DXFGraphic
 
-__all__ = ['TraceBuilder']
+__all__ = ['TraceBuilder', 'LinearTrace', 'CurveStation']
 
-Station = namedtuple('Station', ('vertex', 'start_width', 'end_width'))
-
+LinearStation = namedtuple('LinearStation', ('vertex', 'start_width', 'end_width'))
 # start_width of the next (following) segment
 # end_width of the next (following) segment
+
+CurveStation = namedtuple('CurveStation', ('vertex0', 'vertex1'))
+
 Face = Tuple[Vec2, Vec2, Vec2, Vec2]
+Quadrilateral = Union['Solid', 'Trace', 'Face3d']
 
 
-class TraceBuilder(Sequence):
-    """ Build 2D banded lines like polylines with start- and end width.
+class AbstractTrace:
+    @abstractmethod
+    def faces(self) -> Iterable[Face]:
+        pass
+
+    def virtual_entities(self, dxftype='TRACE', dxfattribs: Dict = None, doc: 'Drawing' = None) -> Quadrilateral:
+        """
+        Yields faces as SOLID, TRACE or 3DFACE entities with DXF attributes given in `dxfattribs`.
+
+        If a document is given, the doc attribute of the new entities will be set and the new
+        entities will be automatically added to the entity database of that document.
+
+        Args:
+            dxftype: DXF type as string, "SOLID", "TRACE" or "3DFACE"
+            dxfattribs: DXF attributes for SOLID, TRACE or 3DFACE entities
+            doc: associated document
+
+        """
+        from ezdxf.entities.factory import new
+
+        if dxftype not in {'SOLID', 'TRACE', '3DFACE'}:
+            raise TypeError(f'Invalid dxftype {dxftype}.')
+        dxfattribs = dxfattribs or {}
+        for face in self.faces():
+            for i in range(4):
+                dxfattribs[f'vtx{i}'] = face[i]
+
+            if dxftype != '3DFACE':
+                # weird vertex order for SOLID and TRACE
+                dxfattribs['vtx2'] = face[3]
+                dxfattribs['vtx3'] = face[2]
+            entity = new(dxftype, dxfattribs, doc)
+            if doc:
+                doc.entitydb.add(entity)
+            yield entity
+
+
+class LinearTrace(AbstractTrace):
+    """ Linear 2D banded lines like polylines with start- and end width.
 
     Accepts 3D input, but z-axis is ignored.
 
     """
 
-    def __init__(self, default_start_width=None):
-        self._stations: List[Station] = []
+    def __init__(self):
+        self._stations: List[LinearStation] = []
         self.abs_tol = 1e-12
 
     def __len__(self):
@@ -70,35 +111,7 @@ class TraceBuilder(Sequence):
         if self.is_started and self.last_vertex.isclose(point, self.abs_tol):
             # replace last station
             self._stations.pop()
-        self._stations.append(Station(point, float(start_width), float(end_width)))
-
-    def add_spline(self, spline: BSpline, start_width: float, end_width: float, segments: int) -> None:
-        """
-        Add a B-spline by approximation of `segments`, start- and end-width of segments is linear interpolated from
-        `start_width` to `end_width`.
-
-        Use :class:`~ezdxf.math.BSpline` constructors :meth:`~ezdxf.math.BSpline.from_arc` and
-        :meth:`~ezdxf.math.BSpline.from_ellipse` to add arcs and ellipses, but check for continuity and reverse
-        splines if necessary, because the DXF format stores ARC and ELLIPSE entities always in counter clockwise
-        direction, it is not possible to represent clockwise arcs or ellipses.
-
-        Continuity means the first control point of a clamped spline should be close to the actual
-        :attr:`TraceBuilder.last_vertex`. It is not a problem if this is not the case, the last station
-        will be connected to the first spline vertex, but this is often not the expected result.
-
-        Args:
-            spline: :class:`~ezdxf.math.BSpline` object
-            start_width: overall start width
-            end_width: overall end width
-            segments: count of approximation segments
-
-        """
-        start_width = float(start_width)
-        end_width = float(end_width)
-        widths = list(linspace(start_width, end_width, segments + 1))
-        widths.append(widths[-1])  # end width of last segment
-        for index, vertex in enumerate(spline.approximate(int(segments))):
-            self.add_station(vertex, widths[index], widths[index + 1])
+        self._stations.append(LinearStation(point, float(start_width), float(end_width)))
 
     def faces(self) -> Iterable[Face]:
         """ Yields all faces as 4-tuples of :class:`~ezdxf.math.Vec2` objects.
@@ -176,8 +189,111 @@ class TraceBuilder(Sequence):
                 vtx3 = up2
             yield vtx0, vtx1, vtx2, vtx3
 
-    def virtual_entities(self, dxftype='TRACE', dxfattribs: Dict = None, doc: 'Drawing' = None) -> Union[
-        'Solid', 'Trace', 'Face3d']:
+
+def _normal_offset_points(start: Vec2, end: Vec2, start_width: float, end_width: float) -> Face:
+    dir_vector = (end - start).normalize()
+    ortho = dir_vector.orthogonal(True)
+    offset_start = ortho.normalize(start_width / 2)
+    offset_end = ortho.normalize(end_width / 2)
+    return start + offset_start, end + offset_end, start - offset_start, end - offset_end
+
+
+class CurvedTrace(AbstractTrace):
+    """ 2D banded curves like arcs or splines with start- and end width.
+
+    Represents one curved entity.
+
+    Accepts 3D input, but z-axis is ignored.
+
+    """
+
+    def __init__(self):
+        self._stations: List[CurveStation] = []
+
+    def __len__(self):
+        return len(self._stations)
+
+    def __getitem__(self, item):
+        return self._stations[item]
+
+    @classmethod
+    def from_spline(cls, spline: BSpline, start_width: float, end_width: float, segments: int) -> 'CurvedTrace':
+        curve_trace = cls()
+        count = segments + 1
+        t = linspace(0, spline.max_t, count)
+        for ((point, derivative), width) in zip(spline.derivatives(t, n=1), linspace(start_width, end_width, count)):
+            normal = Vec2(derivative).orthogonal(True)
+            curve_trace.append(Vec2(point), normal, width)
+        return curve_trace
+
+    @classmethod
+    def from_arc(cls, arc: ConstructionArc, start_width: float, end_width: float, segments: int) -> 'CurvedTrace':
+        curve_trace = cls()
+        count = segments + 1
+        center = Vec2(arc.center)
+        for point, width in zip(arc.vertices(arc.angles(count)), linspace(start_width, end_width, count)):
+            curve_trace.append(point, point - center, width)
+        return curve_trace
+
+    def append(self, point: Vec2, normal: Vec2, width: float) -> None:
+        """
+        Add a curve trace station (like a vertex) at location `point`^.
+
+        Args:
+            point: 2D curve location (vertex), z-axis of 3D vertices is ignored.
+            normal: curve normal
+            width:  width of station
+
+        """
+        normal = normal.normalize(width / 2)
+        self._stations.append(CurveStation(point + normal, point - normal))
+
+    def faces(self) -> Iterable[Face]:
+        """ Yields all faces as 4-tuples of :class:`~ezdxf.math.Vec2` objects.
+        """
+        count = len(self._stations)
+        if count < 2:
+            raise ValueError('Two or more stations required.')
+
+        vtx0 = None
+        vtx1 = None
+        for vtx2, vtx3 in self._stations:
+            if vtx0 is None:
+                vtx0 = vtx3
+                vtx1 = vtx2
+                continue
+            yield vtx0, vtx1, vtx2, vtx3
+            vtx0 = vtx3
+            vtx1 = vtx2
+
+
+class TraceBuilder(Sequence):
+    """ Sequence of 2D banded lines like polylines with start- and end width or curves with start- and end width.
+
+    Accepts 3D input, but z-axis is ignored.
+
+    """
+
+    def __init__(self, default_start_width=None):
+        self._traces: List[AbstractTrace] = []
+        self.abs_tol = 1e-12
+
+    def __len__(self):
+        return len(self._traces)
+
+    def __getitem__(self, item):
+        return self._traces[item]
+
+    def append(self, trace: AbstractTrace) -> None:
+        self._traces.append(trace)
+
+    def faces(self) -> Iterable[Face]:
+        """ Yields all faces as 4-tuples of :class:`~ezdxf.math.Vec2` objects.
+        """
+        for trace in self._traces:
+            yield from trace.faces()
+
+    def virtual_entities(self, dxftype='TRACE', dxfattribs: Dict = None, doc: 'Drawing' = None) -> Quadrilateral:
         """
         Yields faces as SOLID, TRACE or 3DFACE entities with DXF attributes given in `dxfattribs`.
 
@@ -190,23 +306,8 @@ class TraceBuilder(Sequence):
             doc: associated document
 
         """
-        from ezdxf.entities.factory import new
-
-        if dxftype not in {'SOLID', 'TRACE', '3DFACE'}:
-            raise TypeError(f'Invalid dxftype {dxftype}.')
-        dxfattribs = dxfattribs or {}
-        for face in self.faces():
-            for i in range(4):
-                dxfattribs[f'vtx{i}'] = face[i]
-
-            if dxftype != '3DFACE':
-                # weird vertex order for SOLID and TRACE
-                dxfattribs['vtx2'] = face[3]
-                dxfattribs['vtx3'] = face[2]
-            entity = new(dxftype, dxfattribs, doc)
-            if doc:
-                doc.entitydb.add(entity)
-            yield entity
+        for trace in self._traces:
+            yield from trace.virtual_entities(dxftype, dxfattribs, doc)
 
     @classmethod
     def from_polyline(cls, polyline: 'DXFGraphic', segments: int = 40) -> 'TraceBuilder':
@@ -244,23 +345,18 @@ class TraceBuilder(Sequence):
                 points.append((location, start_width, end_width, bulge))
         else:
             raise TypeError(f'Invalid DXF type {dxftype}')
-        trace = cls()
-        prev_point = None
-        prev_start_width = None
-        prev_end_width = None
-        prev_bulge = None
-        # todo: closed polygons
-        for point, start_width, end_width, bulge in points:
-            if prev_point is None:
-                prev_point = point
-                prev_start_width = start_width
-                prev_end_width = end_width
-                prev_bulge = bulge
-                trace.add_station(point, start_width, end_width)
-                continue
 
-            if prev_bulge != 0:  # arc from prev_point to point
-                center, start_angle, end_angle, radius = bulge_to_arc(prev_point, point, prev_bulge)
+        trace = cls()
+        store_bulge = None
+        store_start_width = None
+        store_end_width = None
+        store_point = None
+
+        # todo: closed polygons
+        linear_trace = LinearTrace()
+        for point, start_width, end_width, bulge in points:
+            if store_bulge:
+                center, start_angle, end_angle, radius = bulge_to_arc(store_point, point, store_bulge)
                 arc = ConstructionArc(
                     center,
                     radius,
@@ -268,23 +364,28 @@ class TraceBuilder(Sequence):
                     math.degrees(end_angle),
                     is_counter_clockwise=True,
                 )
-                spline = BSpline.from_arc(arc)
-                if not spline.control_points[0].isclose(trace.last_vertex):
-                    spline = spline.reverse()
-                trace.add_spline(spline, prev_start_width, prev_end_width, segments)
+                if arc.start_point.isclose(point):
+                    sw = store_end_width
+                    ew = store_start_width
+                else:
+                    ew = store_end_width
+                    sw = store_start_width
 
-            trace.add_station(point, start_width, end_width)
-            prev_point = point
-            prev_bulge = bulge
-            prev_start_width = start_width
-            prev_end_width = end_width
+                trace.append(CurvedTrace.from_arc(arc, sw, ew, segments))
+                store_bulge = None
 
+            if bulge != 0:  # arc from prev_point to point
+                if linear_trace.is_started:
+                    linear_trace.add_station(point, start_width, end_width)
+                    trace.append(linear_trace)
+                    linear_trace = LinearTrace()
+                store_bulge = bulge
+                store_start_width = start_width
+                store_end_width = end_width
+                store_point = point
+                continue
+
+            linear_trace.add_station(point, start_width, end_width)
+        if linear_trace.is_started:
+            trace.append(linear_trace)
         return trace
-
-
-def _normal_offset_points(start: Vec2, end: Vec2, start_width: float, end_width: float) -> Face:
-    dir_vector = (end - start).normalize()
-    ortho = dir_vector.orthogonal(True)
-    offset_start = ortho.normalize(start_width / 2)
-    offset_end = ortho.normalize(end_width / 2)
-    return start + offset_start, end + offset_end, start - offset_start, end - offset_end
--- tests/test_07_render/test_707_trace.py
+++ tests/test_07_render/test_707_trace.py
@@ -3,7 +3,7 @@
 import pytest
 import math
 import ezdxf
-from ezdxf.render.trace import TraceBuilder
+from ezdxf.render.trace import TraceBuilder, LinearTrace, CurvedTrace
 from ezdxf.math import BSpline, Vec2
 
 
@@ -13,7 +13,7 @@ def test_trace_builder_init():
 
 
 def test_add_station_2d():
-    t = TraceBuilder()
+    t = LinearTrace()
     t.add_station((0, 0), 1, 2)
     assert len(t) == 1
     assert t[0].vertex == (0, 0)
@@ -28,29 +28,18 @@ def test_add_station_2d():
 
 def test_add_station_3d():
     # z-axis is ignored
-    t = TraceBuilder()
+    t = LinearTrace()
     t.add_station((0, 0, 0), 1, 2)
     assert t[0].vertex == (0, 0)
 
 
 def test_add_spline_segment():
-    t = TraceBuilder()
-    t.add_station((0, 0), 1)
-    t.add_station((1, 0), 1)
-    assert len(t) == 2
-    t.add_spline(BSpline.from_fit_points([(1, 0), (3, 1), (5, -1), (6, 0)]), start_width=2, end_width=1, segments=10)
-    assert len(t) == 12
-    assert t.last_station.vertex == (6, 0)  # last vertex
-    assert math.isclose(t.last_station.start_width, 1.0)
-    assert math.isclose(t.last_station.end_width, 1.0)
-    # last station connects to following station with
-    # end width of spline, the following element
-    # (line, spline) should start at the last spline vertex
-    # and replaces the actual last_station.
+    t = CurvedTrace.from_spline(BSpline.from_fit_points([(1, 0), (3, 1), (5, -1), (6, 0)]), start_width=2, end_width=1, segments=10)
+    assert len(t) == 11
 
 
 def test_square_face():
-    t = TraceBuilder()
+    t = LinearTrace()
     t.add_station((0, 0), 1, 1)
     t.add_station((1, 0), 0, 0)
     face = list(t.faces())[0]
@@ -61,7 +50,7 @@ def test_square_face():
 
 
 def test_two_straight_faces():
-    t = TraceBuilder()
+    t = LinearTrace()
     t.add_station((0, 0), 1, 1)
     t.add_station((2, 0), 1, 1)
     t.add_station((4, 0), 1, 1)
@@ -77,7 +66,7 @@ def test_two_straight_faces():
 
 
 def test_two_angled_faces():
-    t = TraceBuilder()
+    t = LinearTrace()
     t.add_station((0, 0), 1, 0.5)
     t.add_station((2, 0), 1, 1)
     t.add_station((4, 2), 1, 1)
@@ -93,7 +82,7 @@ def test_two_angled_faces():
 def test_virtual_entities():
     doc = ezdxf.new()
     msp = doc.modelspace()
-    t = TraceBuilder()
+    t = LinearTrace()
     t.add_station((0, 0), 1, 1)
     t.add_station((1, 0), 0, 0)
 
@@ -117,6 +106,7 @@ def test_virtual_entities():
     msp.add_entity(face)
 
     assert len(msp) == 3
+
 
 if __name__ == '__main__':
     pytest.main([__file__])
