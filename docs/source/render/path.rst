9d116d55a4b3ec7f69c2c96ea266cec0a582dfb1
mozman <me@mozman.at>
2020-07-10 07:13:52 +0200

    define interface of Path()

--- docs/source/render/path.rst
+++ docs/source/render/path.rst
@@ -1,1 +1,50 @@
-.. automodule:: ezdxf.render.path
+.. module:: ezdxf.render.path
+
+Path
+====
+
+This module implements a geometrical :class:`Path` supported by several render backends,
+with the goal to create such paths from LWPOLYLINE, POLYLINE and HATCH boundary paths
+and send them to the render backend, see :mod:`ezdxf.addons.drawing`.
+
+Minimum common interface:
+
+- matplotlib: `PathPatch`_
+    - matplotlib.path.Path() codes:
+    - MOVETO
+    - LINETO
+    - CURVE4 - cubic bezier curve
+
+- PyQt: `QPainterPath`_
+    - moveTo()
+    - lineTo()
+    - cubicTo() - cubic bezier curve
+
+- SVG: `SVG-Path`_
+    - "M" - absolute move to
+    - "L" - absolute line to
+    - "C" - absolute cubic bezier curve
+
+ARC and ELLIPSE entities are approximated by multiple cubic Bézier curves, which are close enough
+for display rendering. Non-rational SPLINES of 3rd degree can be represented exact as multiple
+cubic Bézier curves, other B-splines will be approximated.
+
+.. class:: Path
+
+    .. autoattribute:: start
+
+    .. automethod:: from_lwpolyline
+
+    .. automethod:: from_polyline
+
+    .. automethod:: from_hatch_polyline_path
+
+    .. automethod:: from_hatch_edge_path
+
+    .. automethod:: line_to(location: Vector)
+
+    .. automethod:: cubic_to(location: Vector, ctrl1: Vector, ctrl2: Vector)
+
+.. _PathPatch: https://matplotlib.org/3.1.1/api/_as_gen/matplotlib.patches.PathPatch.html#matplotlib.patches.PathPatch
+.. _QPainterPath: https://doc.qt.io/qtforpython/PySide2/QtGui/QPainterPath.html
+.. _SVG-Path: https://developer.mozilla.org/en-US/docs/Web/SVG/Tutorial/Paths
--- src/ezdxf/render/path.py
+++ src/ezdxf/render/path.py
@@ -1,39 +1,57 @@
 # Copyright (c) 2020, Manfred Moitzi
 # License: MIT License
 # Created: 2020-07-10
-"""
-Path
-====
+from typing import TYPE_CHECKING, List, Tuple
+from collections.abc import Sequence
+from ezdxf.math import Vector, NULLVEC
 
-This module implement a geometrical :class:`Path` supported by several render backends,
-with the goal to create such paths from LWPOLYLINE, 2D POLYLINE and HATCH boundary paths
-and send them to the render backend, see :mod:`ezdxf.addons.drawing`.
+if TYPE_CHECKING:
+    from ezdxf.eztypes import LWPolyline, Polyline
+    from ezdxf.entities.hatch import PolylinePath, EdgePath
 
-Minimum common interface:
-
-- matplotlib: `PathPatch`_
-    - matplotlib.path.Path() codes: 
-    - MOVETO
-    - LINETO
-    - CURVE3 - quadratic bezier curve
-    - CURVE4 - cubic bezier curve
-    
-- PyQt: `QPainterPath`_
-    - moveTo()
-    - lineTo()
-    - quadTo() - quadratic bezier curve
-    - cubicTo() - cubic bezier curve
-
-- SVG: `SVG-Path`_
-    - "M" - absolute move to
-    - "L" - absolute line to
-    - "Q" - absolute quadratic bezier curve
-    - "C" - absolute cubic bezier curve
+__all__ = ['Path']
 
 
-.. _PathPatch: https://matplotlib.org/3.1.1/api/_as_gen/matplotlib.patches.PathPatch.html#matplotlib.patches.PathPatch
-.. _QPainterPath: https://doc.qt.io/qtforpython/PySide2/QtGui/QPainterPath.html
-.. _SVG-Path: https://developer.mozilla.org/en-US/docs/Web/SVG/Tutorial/Paths
+class Path(Sequence):
+    def __init__(self, start: Vector = NULLVEC):
+        self._start = Vector(start)
+        self._commands: List[Tuple] = []
 
-"""
+    def __len__(self):
+        return self._commands
 
+    def __getitem__(self, item):
+        return self._commands[item]
+
+    def __iter__(self):
+        return iter(self._commands)
+
+    @property
+    def start(self) -> Vector:
+        return self._start
+
+    @start.setter
+    def start(self, location: Vector) -> None:
+        self._start = Vector(location)
+
+    @classmethod
+    def from_lwpolyline(cls, lwpolyline: 'LWPolyline') -> 'Path':
+        pass
+
+    @classmethod
+    def from_polyline(cls, polyline: 'Polyline') -> 'Path':
+        pass
+
+    @classmethod
+    def from_hatch_polyline_path(cls, path: 'PolylinePath') -> 'Path':
+        pass
+
+    @classmethod
+    def from_hatch_edge_path(cls, path: 'EdgePath') -> 'Path':
+        pass
+
+    def line_to(self, location: Vector) -> None:
+        self._commands.append(('line', Vector(location)))
+
+    def cubic_to(self, location: Vector, ctrl1: Vector, ctrl2: Vector) -> None:
+        self._commands.append(('cubic-bezier', Vector(location), Vector(ctrl1), Vector(ctrl2)))
