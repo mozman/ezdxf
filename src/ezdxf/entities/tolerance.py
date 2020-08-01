8d4945e40f153727fad40171ec368cf425dc41f1
mozman <me@mozman.at>
2020-08-01 14:58:38 +0200

    add DXF attribute validation for TOLERANCE
    Reformat code to 80 cols

--- src/ezdxf/entities/tolerance.py
+++ src/ezdxf/entities/tolerance.py
@@ -2,26 +2,44 @@
 # License: MIT License
 # Created 2019-03-12
 from typing import TYPE_CHECKING
-from ezdxf.math import Vector
-from ezdxf.math.transformtools import transform_extrusion
-from ezdxf.lldxf.attributes import DXFAttr, DXFAttributes, DefSubclass, XType
+from ezdxf.lldxf import validator
+from ezdxf.lldxf.attributes import (
+    DXFAttr, DXFAttributes, DefSubclass, XType, RETURN_DEFAULT,
+)
 from ezdxf.lldxf.const import SUBCLASS_MARKER, DXF2000
+from ezdxf.math import NULLVEC, Z_AXIS, X_AXIS
+from ezdxf.math.transformtools import transform_extrusion
 from .dxfentity import base_class, SubclassProcessor
 from .dxfgfx import DXFGraphic, acdb_entity
 from .factory import register_entity
 
 if TYPE_CHECKING:
-    from ezdxf.eztypes import TagWriter, DXFNamespace, UCS, Matrix44
+    from ezdxf.eztypes import TagWriter, DXFNamespace, Matrix44
 
 __all__ = ['Tolerance']
 
-
 acdb_tolerance = DefSubclass('AcDbFcf', {
-    'dimstyle': DXFAttr(3, default='Standard'),
-    'insert': DXFAttr(10, xtype=XType.point3d, default=Vector(0, 0, 0)),  # Insertion point (in WCS)
-    'content': DXFAttr(1, default=""),  # String representing the visual representation of the tolerance
-    'extrusion': DXFAttr(210, xtype=XType.point3d, default=Vector(0, 0, 1), optional=True),  # Extrusion direction
-    'x_axis_vector': DXFAttr(11, xtype=XType.point3d, default=Vector(1, 0, 0)),  # X-axis direction vector (in WCS)
+    'dimstyle': DXFAttr(
+        3, default='Standard',
+        validator=validator.is_valid_table_name,
+    ),
+    # Insertion point (in WCS):
+    'insert': DXFAttr(10, xtype=XType.point3d, default=NULLVEC),
+
+    # String representing the visual representation of the tolerance:
+    'content': DXFAttr(1, default=""),
+    'extrusion': DXFAttr(
+        210, xtype=XType.point3d, default=Z_AXIS, optional=True,
+        validator=validator.is_not_null_vector,
+        fixer=RETURN_DEFAULT,
+    ),
+    # X-axis direction vector (in WCS):
+    'x_axis_vector': DXFAttr(
+        11, xtype=XType.point3d, default=X_AXIS,
+        validator=validator.is_not_null_vector,
+        fixer=RETURN_DEFAULT,
+    ),
+
 })
 
 
@@ -32,19 +50,19 @@ class Tolerance(DXFGraphic):
     DXFATTRIBS = DXFAttributes(base_class, acdb_entity, acdb_tolerance)
     MIN_DXF_VERSION_FOR_EXPORT = DXF2000
 
-    def load_dxf_attribs(self, processor: SubclassProcessor = None) -> 'DXFNamespace':
+    def load_dxf_attribs(
+            self, processor: SubclassProcessor = None) -> 'DXFNamespace':
         dxf = super().load_dxf_attribs(processor)
         if processor:
             tags = processor.load_dxfattribs_into_namespace(dxf, acdb_tolerance)
             if len(tags):
-                processor.log_unprocessed_tags(tags, subclass=acdb_tolerance.name)
+                processor.log_unprocessed_tags(
+                    tags, subclass=acdb_tolerance.name)
         return dxf
 
     def export_entity(self, tagwriter: 'TagWriter') -> None:
         """ Export entity specific data as DXF tags. """
-        # base class export is done by parent class
         super().export_entity(tagwriter)
-        # AcDbEntity export is done by parent class
         tagwriter.write_tag2(SUBCLASS_MARKER, acdb_tolerance.name)
         self.dxf.export_dxf_attribs(tagwriter, [
             'dimstyle', 'insert', 'content', 'extrusion', 'x_axis_vector'
