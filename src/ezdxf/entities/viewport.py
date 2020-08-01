0d1d8fc8c0f7ac021446cf5cfe825324cd0845ed
mozman <me@mozman.at>
2020-08-01 15:52:40 +0200

    add DXF attribute validation for VIEWPORT
    Reformat code to 80 cols

--- src/ezdxf/entities/viewport.py
+++ src/ezdxf/entities/viewport.py
@@ -2,14 +2,19 @@
 # License: MIT License
 # Created 2019-02-22
 from typing import TYPE_CHECKING, List, Iterable
-from ezdxf.math import Vector
-from ezdxf.lldxf.attributes import DXFAttr, DXFAttributes, DefSubclass, XType
+from ezdxf.lldxf import validator
+from ezdxf.lldxf import const
+from ezdxf.lldxf.attributes import (
+    DXFAttr, DXFAttributes, DefSubclass, XType, RETURN_DEFAULT,
+)
 from ezdxf.lldxf.types import DXFTag, DXFVertex
 from ezdxf.lldxf.tags import Tags
+from ezdxf.lldxf.const import (
+    DXF12, SUBCLASS_MARKER, DXFStructureError, DXFInternalEzdxfError,
+    DXFValueError, DXFTableEntryError,
+)
+from ezdxf.math import Vector, NULLVEC, X_AXIS, Y_AXIS
 from ezdxf.tools import set_flag_state
-from ezdxf.lldxf import const
-from ezdxf.lldxf.const import DXF12, SUBCLASS_MARKER, DXFStructureError, DXFInternalEzdxfError
-from ezdxf.lldxf.const import DXFValueError, DXFTableEntryError
 from .dxfentity import base_class, SubclassProcessor
 from .dxfgfx import DXFGraphic, acdb_entity
 from .factory import register_entity
@@ -20,31 +25,45 @@ if TYPE_CHECKING:
 __all__ = ['Viewport']
 
 acdb_viewport = DefSubclass('AcDbViewport', {
-    'center': DXFAttr(10, xtype=XType.point3d, default=Vector(0, 0, 0)),  # Center point (in WCS)
-    'width': DXFAttr(40, default=1),  # Width in paper space units
-    'height': DXFAttr(41, default=1),  # Height in paper space units
-    'status': DXFAttr(68, default=0),  # Viewport status field:
-    # -1 = On, but is fully off screen, or is one of the viewports that is not active because the $MAXACTVP count is
-    #      currently being exceeded.
+    # Center point (in WCS)
+    'center': DXFAttr(10, xtype=XType.point3d, default=NULLVEC),
+
+    # Width in paper space units:
+    'width': DXFAttr(40, default=1),
+
+    # Height in paper space units:
+    'height': DXFAttr(41, default=1),
+
+    # Viewport status field:
+    # -1 = On, but is fully off screen, or is one of the viewports that is not
+    #      active because the $MAXACTVP count is currently being exceeded.
     #  0 = Off
-    # <positive value > = On and active. The value indicates the order of stacking for the viewports, where 1 is the
-    # active viewport, 2 is the next, and so forth
+    # <positive value> = On and active. The value indicates the order of
+    # stacking for the viewports, where 1 is the active viewport, 2 is the next,
+    # and so on:
+    'status': DXFAttr(68, default=0),
     'id': DXFAttr(69, default=2),
-    'view_center_point': DXFAttr(12, xtype=XType.point2d, default=Vector(0, 0)),  # View center point (in DCS)
-    'snap_base_point': DXFAttr(13, xtype=XType.point2d, default=Vector(0, 0)),
+
+    # View center point (in DCS):
+    'view_center_point': DXFAttr(12, xtype=XType.point2d, default=NULLVEC),
+    'snap_base_point': DXFAttr(13, xtype=XType.point2d, default=NULLVEC),
     'snap_spacing': DXFAttr(14, xtype=XType.point2d, default=Vector(10, 10)),
     'grid_spacing': DXFAttr(15, xtype=XType.point2d, default=Vector(10, 10)),
-    'view_direction_vector': DXFAttr(16, xtype=XType.point3d, default=Vector(0, 0, 0)),  # View direction vector (WCS)
-    'view_target_point': DXFAttr(17, xtype=XType.point3d, default=Vector(0, 0, 0)),  # View target point (in WCS)
+    # View direction vector (WCS):
+    'view_direction_vector': DXFAttr(16, xtype=XType.point3d, default=NULLVEC),
+    # View target point (in WCS):
+    'view_target_point': DXFAttr(17, xtype=XType.point3d, default=NULLVEC),
     'perspective_lens_length': DXFAttr(42, default=50),
     'front_clip_plane_z_value': DXFAttr(43, default=0),
     'back_clip_plane_z_value': DXFAttr(44, default=0),
-    'view_height': DXFAttr(45, default=1),  # View height (in model space units)
+    # View height (in model space units):
+    'view_height': DXFAttr(45, default=1),
     'snap_angle': DXFAttr(50, default=0),
     'view_twist_angle': DXFAttr(51, default=0),
     'circle_zoom': DXFAttr(72, default=100),
+
     # 331: Frozen layer object ID/handle (multiple entries may exist) (optional)
-    'flags': DXFAttr(90, default=0),
+
     # Viewport status bit-coded flags:
     # 1 (0x1) = Enables perspective mode
     # 2 (0x2) = Enables front clipping
@@ -69,9 +88,14 @@ acdb_viewport = DefSubclass('AcDbViewport', {
     # 524288 (0x80000) = Enable adaptive grid display
     # 1048576 (0x100000) = Enables subdivision of the grid below the set grid spacing when the grid display is adaptive
     # 2097152 (0x200000) = Enables grid follows workplane switching
+    'flags': DXFAttr(90, default=0),
+
     'clipping_boundary_handle': DXFAttr(340, default=0, optional=True),
-    'plot_style_name': DXFAttr(1, default=''),  # Plot style sheet name assigned to this viewport
-    'render_mode': DXFAttr(281, default=0),
+
+    # Plot style sheet name assigned to this viewport
+    'plot_style_name': DXFAttr(1, default=''),
+
+    # Render mode:
     # 0 = 2D Optimized (classic 2D)
     # 1 = Wireframe
     # 2 = Hidden line
@@ -79,17 +103,39 @@ acdb_viewport = DefSubclass('AcDbViewport', {
     # 4 = Gouraud shaded
     # 5 = Flat shaded with wireframe
     # 6 = Gouraud shaded with wireframe
-    'ucs_per_viewport': DXFAttr(71, default=0),
+    'render_mode': DXFAttr(
+        281, default=0,
+        validator=validator.is_in_integer_range(0, 7),
+        fixer=RETURN_DEFAULT,
+    ),
+    'ucs_per_viewport': DXFAttr(
+        71, default=0,
+        validator=validator.is_integer_bool,
+        fixer=RETURN_DEFAULT,
+    ),
     'ucs_icon': DXFAttr(74, default=0),
-    'ucs_origin': DXFAttr(110, xtype=XType.point3d, default=Vector(0, 0, 0)),
-    'ucs_x_axis': DXFAttr(111, xtype=XType.point3d, default=Vector(1, 0, 0)),
-    'ucs_y_axis': DXFAttr(112, xtype=XType.point3d, default=Vector(0, 1, 0)),
+    'ucs_origin': DXFAttr(110, xtype=XType.point3d, default=NULLVEC),
+    'ucs_x_axis': DXFAttr(
+        111, xtype=XType.point3d, default=X_AXIS,
+        validator=validator.is_not_null_vector,
+        fixer=RETURN_DEFAULT,
+    ),
+    'ucs_y_axis': DXFAttr(
+        112, xtype=XType.point3d, default=Y_AXIS,
+        validator=validator.is_not_null_vector,
+        fixer=RETURN_DEFAULT,
+    ),
+
+    # Handle of AcDbUCSTableRecord if UCS is a named UCS.
+    # If not present, then UCS is unnamed:
     'ucs_handle': DXFAttr(345),
-    # ID/handle of AcDbUCSTableRecord if UCS is a named UCS. If not present, then UCS is unnamed
+
+    # Handle of AcDbUCSTableRecord of base UCS if UCS is orthographic (79 code
+    # is non-zero). If not present and 79 code is non-zero, then base UCS is
+    # taken to be WORLD:
     'ucs_base_handle': DXFAttr(346, optional=True),
-    # ID/handle of AcDbUCSTableRecord of base UCS if UCS is orthographic (79 code is non-zero). If not present and 79
-    # code is non-zero, then base UCS is taken to be WORLD
-    'ucs_ortho_type': DXFAttr(79, default=0),
+
+    # UCS ortho type:
     # 0 = not orthographic
     # 1 = Top
     # 2 = Bottom
@@ -97,34 +143,65 @@ acdb_viewport = DefSubclass('AcDbViewport', {
     # 4 = Back
     # 5 = Left
     # 6 = Right
+    'ucs_ortho_type': DXFAttr(
+        79, default=0,
+        validator=validator.is_in_integer_range(0, 7),
+        fixer=RETURN_DEFAULT,
+    ),
     'elevation': DXFAttr(146, default=0),
-    'shade_plot_mode': DXFAttr(170, dxfversion='AC1018'),
+
+    # Shade plot mode:
     # 0 = As Displayed
     # 1 = Wireframe
     # 2 = Hidden
     # 3 = Rendered
+    'shade_plot_mode': DXFAttr(
+        170, dxfversion='AC1018',
+        validator=validator.is_in_integer_range(0, 4),
+        fixer=RETURN_DEFAULT,
+    ),
     # Frequency of major grid lines compared to minor grid lines
     'grid_frequency': DXFAttr(61, dxfversion='AC1021'),
     'background_handle': DXFAttr(332, dxfversion='AC1021'),
     'shade_plot_handle': DXFAttr(333, dxfversion='AC1021'),
     'visual_style_handle': DXFAttr(348, dxfversion='AC1021'),
-    'default_lighting_flag': DXFAttr(292, dxfversion='AC1021', default=1, optional=True),
-    'default_lighting_type': DXFAttr(282, dxfversion='AC1021'),
+    'default_lighting_flag': DXFAttr(292, dxfversion='AC1021', default=1,
+                                     optional=True),
+    # Default lighting type:
     # 0 = One distant light
     # 1 = Two distant lights
+    'default_lighting_type': DXFAttr(
+        282, default=0, dxfversion='AC1021',
+        validator=validator.is_integer_bool,
+        fixer=RETURN_DEFAULT,
+    ),
     'view_brightness': DXFAttr(141, dxfversion='AC1021'),
     'view_contrast': DXFAttr(142, dxfversion='AC1021'),
-    'ambient_light_color_1': DXFAttr(63, dxfversion='AC1021'),  # as AutoCAD Color Index
-    'ambient_light_color_2': DXFAttr(421, dxfversion='AC1021'),  # as True Color
-    'ambient_light_color_3': DXFAttr(431, dxfversion='AC1021'),  # as True Color
+
+    # as AutoCAD Color Index
+    'ambient_light_color_1': DXFAttr(
+        63, dxfversion='AC1021',
+        validator=validator.is_valid_aci_color,
+    ),
+    # as True Color:
+    'ambient_light_color_2': DXFAttr(421, dxfversion='AC1021'),
+
+    # as True Color:
+    'ambient_light_color_3': DXFAttr(431, dxfversion='AC1021'),
+
     'sun_handle': DXFAttr(361, dxfversion='AC1021'),
-    'ref_vp_object_1': DXFAttr(335, dxfversion='AC1021'),  # unknown meaning, don't ask mozman
-    'ref_vp_object_2': DXFAttr(343, dxfversion='AC1021'),  # unknown meaning, don't ask mozman
-    'ref_vp_object_3': DXFAttr(344, dxfversion='AC1021'),  # unknown meaning, don't ask mozman
-    'ref_vp_object_4': DXFAttr(91, dxfversion='AC1021'),  # unknown meaning, don't ask mozman
+    'ref_vp_object_1': DXFAttr(335, dxfversion='AC1021'),
+    # unknown meaning, don't ask mozman
+    'ref_vp_object_2': DXFAttr(343, dxfversion='AC1021'),
+    # unknown meaning, don't ask mozman
+    'ref_vp_object_3': DXFAttr(344, dxfversion='AC1021'),
+    # unknown meaning, don't ask mozman
+    'ref_vp_object_4': DXFAttr(91, dxfversion='AC1021'),
+    # unknown meaning, don't ask mozman
 })
 # Note:
-# The ZOOM XP factor is calculated with the following formula: group_41 / group_45 (or pspace_height / mspace_height).
+# The ZOOM XP factor is calculated with the following formula:
+# group_41 / group_45 (or pspace_height / mspace_height).
 
 FROZEN_LAYER_GROUP_CODE = 331
 
@@ -154,22 +231,22 @@ class Viewport(DXFGraphic):
 
     @property
     def frozen_layers(self) -> List[str]:
-        """ Set/get frozen layers as list of layer names.
-        """
+        """ Set/get frozen layers as list of layer names. """
         type_ = self._frozen_layers_content_type
         if type_ == 'names':
             return self._frozen_layers
 
-        bag = []  # type: List[str]
+        bag: List[str] = []
         entitydb = self.doc.entitydb
         if type_ == 'handles':
             for handle in self._frozen_layers:
                 try:
                     bag.append(entitydb[handle].dxf.name)
-                except KeyError:  # ignore non existing layers
+                except KeyError:  # ignore non-existing layers
                     pass
         else:
-            raise DXFInternalEzdxfError('invalid frozen_layer_content_type: "{}"'.format(type_))
+            raise DXFInternalEzdxfError(
+                f'invalid frozen_layer_content_type: "{type_}"')
         self._frozen_layers = bag
         self._frozen_layers_content_type = 'names'
         return bag
@@ -179,7 +256,8 @@ class Viewport(DXFGraphic):
         self._frozen_layers = list(names)
         self._frozen_layers_content_type = 'names'
 
-    def load_dxf_attribs(self, processor: SubclassProcessor = None) -> 'DXFNamespace':
+    def load_dxf_attribs(
+            self, processor: SubclassProcessor = None) -> 'DXFNamespace':
         dxf = super().load_dxf_attribs(processor)
         if processor:
             tags = processor.load_dxfattribs_into_namespace(dxf, acdb_viewport)
@@ -189,7 +267,8 @@ class Viewport(DXFGraphic):
                 if len(tags):
                     tags = self.load_frozen_layer_handles(tags)
                 if len(tags):
-                    processor.log_unprocessed_tags(tags, subclass=acdb_viewport.name)
+                    processor.log_unprocessed_tags(
+                        tags, subclass=acdb_viewport.name)
         return dxf
 
     def load_frozen_layer_handles(self, tags: Tags) -> Tags:
@@ -213,7 +292,8 @@ class Viewport(DXFGraphic):
         flags = set_flag_state(flags, const.VSF_FAST_ZOOM, bool(tags[11]))
         flags = set_flag_state(flags, const.VSF_SNAP_MODE, bool(tags[13]))
         flags = set_flag_state(flags, const.VSF_GRID_MODE, bool(tags[14]))
-        flags = set_flag_state(flags, const.VSF_ISOMETRIC_SNAP_STYLE, bool(tags[15]))
+        flags = set_flag_state(flags, const.VSF_ISOMETRIC_SNAP_STYLE,
+                               bool(tags[15]))
         flags = set_flag_state(flags, const.VSF_HIDE_PLOT_MODE, bool(tags[24]))
         try:
             dxf.view_target_point = tags[0]
@@ -246,18 +326,18 @@ class Viewport(DXFGraphic):
 
     def export_entity(self, tagwriter: 'TagWriter') -> None:
         """ Export entity specific data as DXF tags. """
-        # base class export is done by parent class
         super().export_entity(tagwriter)
-        # AcDbEntity export is done by parent class
         if tagwriter.dxfversion == DXF12:
             self.export_acdb_viewport_r12(tagwriter)
         else:
             tagwriter.write_tag2(SUBCLASS_MARKER, acdb_viewport.name)
             self.dxf.export_dxf_attribs(tagwriter, [
-                'center', 'width', 'height', 'status', 'id', 'view_center_point', 'snap_base_point', 'snap_spacing',
-                'grid_spacing', 'view_direction_vector', 'view_target_point', 'perspective_lens_length',
-                'front_clip_plane_z_value', 'back_clip_plane_z_value', 'view_height', 'snap_angle', 'view_twist_angle',
-                'circle_zoom',
+                'center', 'width', 'height', 'status', 'id',
+                'view_center_point', 'snap_base_point', 'snap_spacing',
+                'grid_spacing', 'view_direction_vector', 'view_target_point',
+                'perspective_lens_length', 'front_clip_plane_z_value',
+                'back_clip_plane_z_value', 'view_height', 'snap_angle',
+                'view_twist_angle', 'circle_zoom',
             ])
             if len(self.frozen_layers):
                 layers = self.doc.layers
@@ -267,16 +347,20 @@ class Viewport(DXFGraphic):
                     except DXFTableEntryError:
                         pass
                     else:
-                        tagwriter.write_tag2(FROZEN_LAYER_GROUP_CODE, layer.dxf.handle)
+                        tagwriter.write_tag2(FROZEN_LAYER_GROUP_CODE,
+                                             layer.dxf.handle)
 
             self.dxf.export_dxf_attribs(tagwriter, [
-                'flags', 'clipping_boundary_handle', 'plot_style_name', 'render_mode',
-                'ucs_per_viewport', 'ucs_icon', 'ucs_origin', 'ucs_x_axis', 'ucs_y_axis', 'ucs_handle',
-                'ucs_base_handle', 'ucs_ortho_type', 'elevation', 'shade_plot_mode', 'grid_frequency',
-                'background_handle', 'shade_plot_handle', 'visual_style_handle', 'default_lighting_flag',
-                'default_lighting_type', 'view_brightness', 'view_contrast', 'ambient_light_color_1',
-                'ambient_light_color_2', 'ambient_light_color_3', 'sun_handle', 'ref_vp_object_1', 'ref_vp_object_2',
-                'ref_vp_object_3', 'ref_vp_object_4'
+                'flags', 'clipping_boundary_handle', 'plot_style_name',
+                'render_mode', 'ucs_per_viewport', 'ucs_icon', 'ucs_origin',
+                'ucs_x_axis', 'ucs_y_axis', 'ucs_handle', 'ucs_base_handle',
+                'ucs_ortho_type', 'elevation', 'shade_plot_mode',
+                'grid_frequency', 'background_handle', 'shade_plot_handle',
+                'visual_style_handle', 'default_lighting_flag',
+                'default_lighting_type', 'view_brightness', 'view_contrast',
+                'ambient_light_color_1', 'ambient_light_color_2',
+                'ambient_light_color_3', 'sun_handle', 'ref_vp_object_1',
+                'ref_vp_object_2', 'ref_vp_object_3', 'ref_vp_object_4'
             ])
 
     def export_acdb_viewport_r12(self, tagwriter: 'TagWriter'):
@@ -322,7 +406,8 @@ class Viewport(DXFGraphic):
             DXFTag(1070, flag(const.VSF_HIDE_PLOT_MODE)),
             DXFTag(1002, '{'),  # start frozen layer list
         ]
-        tags.extend(DXFTag(1003, layer_name) for layer_name in self.frozen_layers)
+        tags.extend(
+            DXFTag(1003, layer_name) for layer_name in self.frozen_layers)
         tags.extend([
             DXFTag(1002, '}'),  # end of frozen layer list
             DXFTag(1002, '}'),  # MVIEW
@@ -332,4 +417,7 @@ class Viewport(DXFGraphic):
     def rename_frozen_layer(self, old_name: str, new_name: str) -> None:
         key = self.doc.layers.key
         old_key = key(old_name)
-        self.frozen_layers = [(name if key(name) != old_key else new_name) for name in self.frozen_layers]
+        self.frozen_layers = [
+            (name if key(name) != old_key else new_name)
+            for name in self.frozen_layers
+        ]
