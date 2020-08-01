c93f8b1b8c861c9d098d9ff8ccf911f048d909dd
mozman <me@mozman.at>
2020-08-01 14:03:46 +0200

    add DXF attribute validation for POINT
    Reformat code to 80 cols

--- src/ezdxf/entities/point.py
+++ src/ezdxf/entities/point.py
@@ -2,16 +2,21 @@
 # License: MIT License
 # Created 2019-02-15
 from typing import TYPE_CHECKING
-from ezdxf.math import Vector, Matrix44
-from ezdxf.math.transformtools import transform_thickness_and_extrusion_without_ocs
-from ezdxf.lldxf.attributes import DXFAttr, DXFAttributes, DefSubclass, XType
+from ezdxf.lldxf import validator
+from ezdxf.lldxf.attributes import (
+    DXFAttr, DXFAttributes, DefSubclass, XType, RETURN_DEFAULT,
+)
 from ezdxf.lldxf.const import DXF12, SUBCLASS_MARKER
+from ezdxf.math import Vector, Matrix44, NULLVEC, Z_AXIS
+from ezdxf.math.transformtools import (
+    transform_thickness_and_extrusion_without_ocs
+)
 from .dxfentity import base_class, SubclassProcessor
 from .dxfgfx import DXFGraphic, acdb_entity
 from .factory import register_entity
 
 if TYPE_CHECKING:
-    from ezdxf.eztypes import TagWriter, DXFNamespace, UCS
+    from ezdxf.eztypes import TagWriter, DXFNamespace
 
 __all__ = ['Point']
 
@@ -35,11 +40,19 @@ __all__ = ['Point']
 # >0 = Specifies an absolute size
 
 acdb_point = DefSubclass('AcDbPoint', {
-    'location': DXFAttr(10, xtype=XType.point3d, default=Vector(0, 0, 0)),
+    # Point location:
+    'location': DXFAttr(10, xtype=XType.point3d, default=NULLVEC),
+
+    # Thickness could be negative:
     'thickness': DXFAttr(39, default=0, optional=True),
-    'extrusion': DXFAttr(210, xtype=XType.point3d, default=Vector(0, 0, 1), optional=True),
-    # angle of the X axis for the UCS in effect when the point was drawn (optional, default = 0); used when PDMODE is
-    # nonzero
+
+    'extrusion': DXFAttr(
+        210, xtype=XType.point3d, default=Z_AXIS, optional=True,
+        validator=validator.is_not_null_vector,
+        fixer=RETURN_DEFAULT,
+    ),
+    # angle of the x-axis for the UCS in effect when the point was drawn;
+    # used when PDMODE is nonzero:
     'angle': DXFAttr(50, default=0, optional=True),
 })
 
@@ -50,7 +63,8 @@ class Point(DXFGraphic):
     DXFTYPE = 'POINT'
     DXFATTRIBS = DXFAttributes(base_class, acdb_entity, acdb_point)
 
-    def load_dxf_attribs(self, processor: SubclassProcessor = None) -> 'DXFNamespace':
+    def load_dxf_attribs(
+            self, processor: SubclassProcessor = None) -> 'DXFNamespace':
         """ Loading interface. (internal API) """
         dxf = super().load_dxf_attribs(processor)
         if processor:
@@ -67,7 +81,9 @@ class Point(DXFGraphic):
         if tagwriter.dxfversion > DXF12:
             tagwriter.write_tag2(SUBCLASS_MARKER, acdb_point.name)
         # for all DXF versions
-        self.dxf.export_dxf_attribs(tagwriter, ['location', 'thickness', 'extrusion', 'angle'])
+        self.dxf.export_dxf_attribs(tagwriter, [
+            'location', 'thickness', 'extrusion', 'angle'
+        ])
 
     def transform(self, m: Matrix44) -> 'Point':
         """ Transform POINT entity by transformation matrix `m` inplace.
@@ -81,8 +97,8 @@ class Point(DXFGraphic):
         return self
 
     def translate(self, dx: float, dy: float, dz: float) -> 'Point':
-        """ Optimized POINT translation about `dx` in x-axis, `dy` in y-axis and `dz` in z-axis,
-        returns `self` (floating interface).
+        """ Optimized POINT translation about `dx` in x-axis, `dy` in y-axis and
+        `dz` in z-axis.
 
         .. versionadded:: 0.13
 
