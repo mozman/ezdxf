d7d3503f547546c0247ca0209e2280c51102d68e
mozman <me@mozman.at>
2020-08-01 14:19:10 +0200

    add DXF attribute validation for SHAPE
    Reformat code to 80 cols

--- src/ezdxf/entities/shape.py
+++ src/ezdxf/entities/shape.py
@@ -2,28 +2,52 @@
 # License: MIT License
 # Created 2019-02-21
 from typing import TYPE_CHECKING
-from ezdxf.math import Vector
-from ezdxf.math.transformtools import OCSTransform
-from ezdxf.lldxf.attributes import DXFAttr, DXFAttributes, DefSubclass, XType
+from ezdxf.lldxf import validator
+from ezdxf.lldxf.attributes import (
+    DXFAttr, DXFAttributes, DefSubclass, XType, RETURN_DEFAULT,
+)
 from ezdxf.lldxf.const import DXF12, SUBCLASS_MARKER
+from ezdxf.math import NULLVEC, Z_AXIS
+from ezdxf.math.transformtools import OCSTransform
 from .dxfentity import base_class, SubclassProcessor
 from .dxfgfx import DXFGraphic, acdb_entity
 from .factory import register_entity
 
 if TYPE_CHECKING:
-    from ezdxf.eztypes import TagWriter, DXFNamespace, UCS, Matrix44
+    from ezdxf.eztypes import TagWriter, DXFNamespace, Matrix44
 
 __all__ = ['Shape']
 
 acdb_shape = DefSubclass('AcDbShape', {
-    'thickness': DXFAttr(39, default=0, optional=True),  # Thickness
-    'insert': DXFAttr(10, xtype=XType.point3d, default=Vector(0, 0, 0)),  # Insertion point (in WCS)
+    # Thickness could be negative:
+    'thickness': DXFAttr(39, default=0, optional=True),
+
+    # Insertion point (in WCS)
+    'insert': DXFAttr(10, xtype=XType.point3d, default=NULLVEC),
+
+    # Shape size:
     'size': DXFAttr(40, default=1),
-    'name': DXFAttr(2, default=''),  # Shape name
-    'rotation': DXFAttr(50, default=0, optional=True),  # Rotation angle in degrees
-    'xscale': DXFAttr(41, default=1, optional=True),  # Relative X scale factor
-    'oblique': DXFAttr(51, default=0, optional=True),  # Oblique angle
-    'extrusion': DXFAttr(210, xtype=XType.point3d, default=Vector(0, 0, 1), optional=True),  # Extrusion direction
+
+    # Shape name:
+    'name': DXFAttr(2, default=''),
+
+    # Rotation angle in degrees:
+    'rotation': DXFAttr(50, default=0, optional=True),
+
+    # Relative X scale factor
+    'xscale': DXFAttr(
+        41, default=1, optional=True,
+        validator=validator.is_not_zero,
+        fixer=RETURN_DEFAULT,
+    ),
+    # Oblique angle in degrees:
+    'oblique': DXFAttr(51, default=0, optional=True),
+
+    'extrusion': DXFAttr(
+        210, xtype=XType.point3d, default=Z_AXIS, optional=True,
+        validator=validator.is_not_null_vector,
+        fixer=RETURN_DEFAULT,
+    ),
 })
 
 
@@ -33,7 +57,8 @@ class Shape(DXFGraphic):
     DXFTYPE = 'SHAPE'
     DXFATTRIBS = DXFAttributes(base_class, acdb_entity, acdb_shape)
 
-    def load_dxf_attribs(self, processor: SubclassProcessor = None) -> 'DXFNamespace':
+    def load_dxf_attribs(
+            self, processor: SubclassProcessor = None) -> 'DXFNamespace':
         dxf = super().load_dxf_attribs(processor)
         if processor:
             tags = processor.load_dxfattribs_into_namespace(dxf, acdb_shape)
@@ -43,14 +68,12 @@ class Shape(DXFGraphic):
 
     def export_entity(self, tagwriter: 'TagWriter') -> None:
         """ Export entity specific data as DXF tags. """
-        # base class export is done by parent class
         super().export_entity(tagwriter)
-        # AcDbEntity export is done by parent class
         if tagwriter.dxfversion > DXF12:
             tagwriter.write_tag2(SUBCLASS_MARKER, acdb_shape.name)
-        # for all DXF versions
         self.dxf.export_dxf_attribs(tagwriter, [
-            'insert', 'size', 'name', 'thickness', 'rotation', 'xscale', 'oblique', 'extrusion',
+            'insert', 'size', 'name', 'thickness', 'rotation', 'xscale',
+            'oblique', 'extrusion',
         ])
 
     def transform(self, m: 'Matrix44') -> 'Shape':
@@ -64,11 +87,12 @@ class Shape(DXFGraphic):
         ocs = OCSTransform(self.dxf.extrusion, m)
 
         dxf.rotation = ocs.transform_deg_angle(dxf.rotation)
-        # dxf.oblique = ocs.transform_deg_angle(dxf.oblique)
         dxf.size = ocs.transform_length((0, dxf.size, 0))
-        dxf.x_scale = ocs.transform_length((dxf.x_scale, 0, 0), reflection=dxf.x_scale)
-        if dxf.hasattr('thickness'):  # thickness can be negative
-            dxf.thickness = ocs.transform_length((0, 0, dxf.thickness), reflection=dxf.thickness)
+        dxf.x_scale = ocs.transform_length(
+            (dxf.x_scale, 0, 0), reflection=dxf.x_scale)
+        if dxf.hasattr('thickness'):
+            dxf.thickness = ocs.transform_length(
+                (0, 0, dxf.thickness), reflection=dxf.thickness)
 
         dxf.extrusion = ocs.new_extrusion
         return self
