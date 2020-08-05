ad4df6868eab96c68ee348d2eece8ad13293dd1d
mozman <me@mozman.at>
2020-08-05 17:19:26 +0200

    reformat code and remove unnecessary DXF attribute validation

--- src/ezdxf/layouts/base.py
+++ src/ezdxf/layouts/base.py
@@ -2,20 +2,22 @@
 # Copyright (c) 2019-2020, Manfred Moitzi
 # License: MIT License
 from typing import TYPE_CHECKING, Iterable, cast
-
 from ezdxf.entities import factory
-from ezdxf.lldxf.const import DXFValueError, DXFStructureError, LATEST_DXF_VERSION, DXFTypeError
+from ezdxf.lldxf.const import (
+    DXFValueError, DXFStructureError, LATEST_DXF_VERSION, DXFTypeError,
+)
 from ezdxf.query import EntityQuery
 from ezdxf.groupby import groupby
 from ezdxf.entitydb import EntityDB, EntitySpace
 from ezdxf.graphicsfactory import CreatorInterface
 
 if TYPE_CHECKING:
-    from ezdxf.eztypes import BlockRecord, DXFGraphic, Dictionary, KeyFunc, Drawing
+    from ezdxf.eztypes import BlockRecord, DXFGraphic, Dictionary, KeyFunc
 
 SUPPORTED_FOREIGN_ENTITY_TYPES = {
-    'ARC', 'LINE', 'CIRCLE', 'ELLIPSE', 'POINT', 'LWPOLYLINE', 'SPLINE', '3DFACE', 'SOLID', 'TRACE', 'SHAPE',
-    'POLYLINE', 'MESH', 'TEXT', 'MTEXT', 'HATCH', 'ATTRIB', 'ATTDEF',
+    'ARC', 'LINE', 'CIRCLE', 'ELLIPSE', 'POINT', 'LWPOLYLINE', 'SPLINE',
+    '3DFACE', 'SOLID', 'TRACE', 'SHAPE', 'POLYLINE', 'MESH', 'TEXT', 'MTEXT',
+    'HATCH', 'ATTRIB', 'ATTDEF',
 }
 
 
@@ -41,9 +43,11 @@ class _AbstractLayout(CreatorInterface):
     def __getitem__(self, index):
         """ Get entity at `index`.
 
-        The underlying data structure for storing entities is organized like a standard Python list, therefore `index`
-        can be any valid list indexing or slicing term, like a single index ``layout[-1]`` to get the last entity, or
-        an index slice ``layout[:10]`` to get the first 10 or less entities as ``List[DXFGraphic]``.
+        The underlying data structure for storing entities is organized like a
+        standard Python list, therefore `index` can be any valid list indexing
+        or slicing term, like a single index ``layout[-1]`` to get the last
+        entity, or an index slice ``layout[:10]`` to get the first 10 or less
+        entities as ``List[DXFGraphic]``.
 
         """
         return self.entity_space[index]
@@ -57,13 +61,15 @@ class _AbstractLayout(CreatorInterface):
 
     def groupby(self, dxfattrib: str = "", key: 'KeyFunc' = None) -> dict:
         """
-        Returns a ``dict`` of entity lists, where entities are grouped by a `dxfattrib` or a `key` function.
+        Returns a ``dict`` of entity lists, where entities are grouped by a
+        `dxfattrib` or a `key` function.
 
         Args:
             dxfattrib: grouping by DXF attribute like ``'layer'``
-            key: key function, which accepts a :class:`DXFGraphic` entity as argument and returns the grouping key of an
-                 entity or ``None`` to ignore the entity. Reason for ignoring: a queried DXF attribute is not
-                 supported by entity.
+            key: key function, which accepts a :class:`DXFGraphic` entity as
+                argument and returns the grouping key of an entity or ``None``
+                to ignore the entity. Reason for ignoring: a queried DXF
+                attribute is not supported by entity.
 
         """
         return groupby(iter(self), dxfattrib, key)
@@ -76,7 +82,8 @@ class BaseLayout(_AbstractLayout):
     def __init__(self, block_record: 'BlockRecord'):
         super().__init__(block_record.doc)
         self.entity_space = block_record.entity_space
-        self.block_record = block_record  # is the central management structure
+        # This is the real central layout management structure:
+        self.block_record = block_record
 
     @property
     def block_record_handle(self):
@@ -87,7 +94,8 @@ class BaseLayout(_AbstractLayout):
     def layout_key(self) -> str:
         """ Returns the layout key as hex string.
 
-        The layout key is the handle of the associated BLOCK_RECORD entry in the BLOCK_RECORDS table.
+        The layout key is the handle of the associated BLOCK_RECORD entry in the
+        BLOCK_RECORDS table.
 
         (internal API)
         """
@@ -120,12 +128,15 @@ class BaseLayout(_AbstractLayout):
 
     @property
     def is_block_layout(self) -> bool:
-        """ ``True`` if not any kind of modelspace or paperspace layout, just a regular block definition. """
+        """ ``True`` if not any kind of modelspace or paperspace layout, just a
+        regular block definition.
+        """
         return self.block_record.is_block_layout
 
     @property
     def units(self) -> int:
-        """ Get/Set layout/block drawing units as enum, see also :ref:`set drawing units`.
+        """ Get/Set layout/block drawing units as enum, see also :ref:`set
+        drawing units`.
         """
         # todo: doesn't care about units stored in XDATA, see ezdxf/units.py
         # Don't know what this units are used for, but header var $INSUNITS
@@ -135,47 +146,48 @@ class BaseLayout(_AbstractLayout):
     @units.setter
     def units(self, value: int) -> None:
         """ Set layout/block drawing units as enum. """
-        value = int(value)
-        if 0 <= value < 25:
-            self.block_record.dxf.units = value
-        else:
-            raise DXFValueError(f'Invalid unit enum: {value}')
+        self.block_record.dxf.units = value  # has a DXF validator
 
     def get_extension_dict(self) -> 'Dictionary':
-        """
-        Returns the associated extension dictionary, creates a new one if necessary.
-
+        """ Returns the associated extension dictionary, creates a new one if
+        necessary.
         """
         return self.block_record.get_extension_dict()
 
     def add_entity(self, entity: 'DXFGraphic') -> None:
-        """
-        Add an existing :class:`DXFGraphic` entity to a layout, but be sure to unlink (:meth:`~BaseLayout.unlink_entity`)
-        entity from the previous owner layout. Adding entities from a different DXF drawing is not supported.
-
+        """ Add an existing :class:`DXFGraphic` entity to a layout, but be sure
+        to unlink (:meth:`~BaseLayout.unlink_entity`) entity from the previous
+        owner layout. Adding entities from a different DXF drawing is not
+        supported.
         """
         if entity.doc != self.doc:
-            raise DXFStructureError('Adding entities from a different DXF drawing is not supported.')
+            raise DXFStructureError(
+                'Adding entities from a different DXF drawing is not supported.'
+            )
 
         self.block_record.add_entity(entity)
         entity.added_to_layout(self)
 
     def add_foreign_entity(self, entity: 'DXFGraphic', copy=True) -> None:
         """
-        Add a foreign DXF entity to a layout, this foreign entity could be from another DXF document or an entity
-        without an assigned DXF document. The intention of this method is to add **simple** entities from another
+        Add a foreign DXF entity to a layout, this foreign entity could be from
+        another DXF document or an entity without an assigned DXF document.
+        The intention of this method is to add **simple** entities from another
         DXF document or from a DXF iterator, for more complex operations use the
-        :mod:`~ezdxf.addons.importer` add-on. Especially objects with BLOCK section (INSERT, DIMENSION,
-        MLEADER) or OBJECTS section dependencies (IMAGE, UNDERLAY) can not be supported by
-        this simple method.
+        :mod:`~ezdxf.addons.importer` add-on. Especially objects with BLOCK
+        section (INSERT, DIMENSION, MLEADER) or OBJECTS section dependencies
+        (IMAGE, UNDERLAY) can not be supported by this simple method.
 
-        Not all DXF types are supported and every dependency or resource reference from another DXF document will be
-        removed except attribute layer will be preserved but only with default attributes like color ``7`` and
-        linetype ``CONTINUOUS`` because the layer attribute doesn't need a layer table entry.
+        Not all DXF types are supported and every dependency or resource
+        reference from another DXF document will be removed except attribute
+        layer will be preserved but only with default attributes like
+        color ``7`` and linetype ``CONTINUOUS`` because the layer attribute
+        doesn't need a layer table entry.
 
-        If the entity is part of another DXF document, it will be unlinked from this document and its
-        entity database if argument `copy` is ``False``, else the entity will be copied. Unassigned entities like
-        from DXF iterators will just be added.
+        If the entity is part of another DXF document, it will be unlinked from
+        this document and its entity database if argument `copy` is ``False``,
+        else the entity will be copied. Unassigned entities like from DXF
+        iterators will just be added.
 
         Supported DXF types:
 
@@ -200,9 +212,8 @@ class BaseLayout(_AbstractLayout):
 
         Args:
             entity: DXF entity to copy or move
-            copy: if ``True`` copy entity from other document else unlink from other document
-
-        .. versionadded:: 0.11.1
+            copy: if ``True`` copy entity from other document else unlink from
+                other document
 
         """
         foreign_doc = entity.doc
@@ -243,46 +254,46 @@ class BaseLayout(_AbstractLayout):
         self.add_entity(entity)
 
     def unlink_entity(self, entity: 'DXFGraphic') -> None:
-        """
-        Unlink `entity` from layout but does not delete entity from the entity database, this removes `entity` just
-        from the layout entity space.
-
+        """ Unlink `entity` from layout but does not delete entity from the
+        entity database, this removes `entity` just from the layout entity space.
         """
         self.block_record.unlink_entity(entity)
 
     def delete_entity(self, entity: 'DXFGraphic') -> None:
-        """ Delete `entity` from layout entity space and the entity database, this destroys the `entity`. """
+        """ Delete `entity` from layout entity space and the entity database,
+        this destroys the `entity`.
+        """
         self.block_record.delete_entity(entity)
 
     def delete_all_entities(self) -> None:
+        """ Delete all entities from layout entity space and from entity database,
+        this destroys all entities in this layout.
         """
-        Delete all entities from layout entity space and from entity database, this destroys all entities in this
-        layout.
-        """
-        # noinspection PyTypeChecker
-        for entity in list(self):  # temp list, because delete modifies the base data structure of the iterator
+        # Create list, because delete modifies the base data structure of
+        # the iterator:
+        for entity in list(self):
             self.delete_entity(entity)
 
     def get_entity_by_handle(self, handle: str) -> 'DXFGraphic':
-        """
-        Get entity by handle.
+        """ Get entity by handle.
 
         (internal API)
         """
         return self.entitydb[handle]
 
-    def move_to_layout(self, entity: 'DXFGraphic', layout: 'BaseLayout') -> None:
-        """
-        Move entity to another layout.
+    def move_to_layout(self, entity: 'DXFGraphic',
+                       layout: 'BaseLayout') -> None:
+        """ Move entity to another layout.
 
         Args:
             entity: DXF entity to move
-            layout: any layout (modelspace, paperspace, block) from **same** drawing
+            layout: any layout (modelspace, paperspace, block) from
+                **same** drawing
 
         """
         if entity.doc != layout.doc:
-            raise DXFStructureError('Moving between different DXF drawings is not supported.')
-
+            raise DXFStructureError(
+                'Moving between different DXF drawings is not supported.')
         try:
             self.unlink_entity(entity)
         except ValueError:
@@ -298,8 +309,7 @@ class BaseLayout(_AbstractLayout):
 
 
 class VirtualLayout(_AbstractLayout):
-    """
-    Helper class to disassemble complex entities into basic DXF
+    """ Helper class to disassemble complex entities into basic DXF
     entities by rendering into a virtual layout.
 
     All entities do not have an assigned DXF document and therefore
