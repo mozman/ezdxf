80672eb9700d72d851a44d425f1a1c059868655c
mozman <me@mozman.at>
2020-08-18 12:05:52 +0200

    add profiling for tags loader

---
+++ profiling/tag_loader.py
@@ -0,0 +1,37 @@
+#  Copyright (c) 2020, Manfred Moitzi
+#  License: MIT License
+import os
+import time
+from ezdxf.lldxf.tagger import ascii_tags_loader
+from ezdxf.recover import bytes_loader
+
+DXF_TEST_FILES_DIR = os.environ.get('EZDXF_TEST_FILES', r'D:\Source\dxftest')
+BIG_FILE = os.path.join(DXF_TEST_FILES_DIR, 'CADKitSamples', 'torso_uniform.dxf')
+
+
+def load_ascii():
+    with open(BIG_FILE, 'rt') as fp:
+        list(ascii_tags_loader(fp))
+
+
+def load_bytes():
+    with open(BIG_FILE, 'rb') as fp:
+        list(bytes_loader(fp))
+
+
+def print_result(time, text):
+    print(f'Operation: {text} takes {time:.2f} s\n')
+
+
+def run(func):
+    start = time.perf_counter()
+    func()
+    end = time.perf_counter()
+    return end - start
+
+
+if __name__ == '__main__':
+    print_result(run(load_ascii), 'ascii_tags_loader()')
+    print_result(run(load_bytes), 'bytes_loader()')
+
+
