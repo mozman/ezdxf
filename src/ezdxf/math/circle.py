005dfc1091e5b13addf87816c5aa4fce969fd168
mozman <me@mozman.at>
2020-08-30 13:16:00 +0200

    reformat code

--- src/ezdxf/math/circle.py
+++ src/ezdxf/math/circle.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2010-2019 Manfred Moitzi
+# Copyright (c) 2010-2020 Manfred Moitzi
 # License: MIT License
 from typing import TYPE_CHECKING, Sequence
 import math
@@ -27,12 +27,17 @@ class ConstructionCircle:
             raise ValueError("Radius has to be > 0.")
 
     def __str__(self) -> str:
-        """ Returns string representation of circle ``ConstructionCircle(center, radius)``. """
-        return 'ConstructionCircle({0.center}, {0.radius})'.format(self)
+        """ Returns string representation of circle
+        "ConstructionCircle(center, radius)".
+        """
+        return f'ConstructionCircle({self.center}, {self.radius})'
 
     @staticmethod
-    def from_3p(p1: 'Vertex', p2: 'Vertex', p3: 'Vertex') -> 'ConstructionCircle':
-        """ Creates a circle from three points, all points have to be compatible to :class:`Vec2` class. """
+    def from_3p(p1: 'Vertex', p2: 'Vertex',
+                p3: 'Vertex') -> 'ConstructionCircle':
+        """ Creates a circle from three points, all points have to be compatible
+        to :class:`Vec2` class.
+        """
         p1 = Vec2(p1)
         p2 = Vec2(p2)
         p3 = Vec2(p3)
@@ -50,8 +55,7 @@ class ConstructionCircle:
         return BoundingBox2d((self.center - rvec, self.center + rvec))
 
     def translate(self, dx: float, dy: float) -> None:
-        """
-        Move circle about `dx` in x-axis and about `dy` in y-axis.
+        """ Move circle about `dx` in x-axis and about `dy` in y-axis.
 
         Args:
             dx: translation in x-axis
@@ -61,8 +65,7 @@ class ConstructionCircle:
         self.center += Vec2((dx, dy))
 
     def point_at(self, angle: float) -> Vec2:
-        """
-        Returns point on circle at `angle` as :class:`Vec2` object.
+        """ Returns point on circle at `angle` as :class:`Vec2` object.
 
         Args:
             angle: angle in radians
@@ -75,8 +78,8 @@ class ConstructionCircle:
         return self.radius >= self.center.distance(Vec2(point))
 
     def tangent(self, angle: float) -> ConstructionRay:
-        """
-        Returns tangent to circle at `angle` as :class:`ConstructionRay` object.
+        """ Returns tangent to circle at `angle` as :class:`ConstructionRay`
+        object.
 
         Args:
             angle: angle in radians
@@ -86,9 +89,10 @@ class ConstructionCircle:
         ray = ConstructionRay(self.center, point_on_circle)
         return ray.orthogonal(point_on_circle)
 
-    def intersect_ray(self, ray: ConstructionRay, abs_tol: float = 1e-10) -> Sequence[Vec2]:
-        """
-        Returns intersection points of circle and `ray` as sequence of :class:`Vec2` objects.
+    def intersect_ray(self, ray: ConstructionRay,
+                      abs_tol: float = 1e-10) -> Sequence[Vec2]:
+        """ Returns intersection points of circle and `ray` as sequence of
+        :class:`Vec2` objects.
 
         Args:
             ray: intersection ray
@@ -110,29 +114,34 @@ class ConstructionCircle:
         intersection_point = ray.intersect(ortho_ray)
         dist = self.center.distance(intersection_point)
         result = []
-        if dist < self.radius:  # intersect in two points
-            if math.isclose(dist, 0., abs_tol=abs_tol):  # if ray goes through center point
+        # Intersect in two points:
+        if dist < self.radius:
+            # Ray goes through center point:
+            if math.isclose(dist, 0., abs_tol=abs_tol):
                 angle = ortho_ray.angle
                 alpha = HALF_PI
             else:
-                # the exact direction of angle (all 4 quadrants Q1-Q4) is important:
-                # ortho_ray.angle is only at the center point correct
+                # The exact direction of angle (all 4 quadrants Q1-Q4) is
+                # important: ortho_ray.angle is only correct at the center point
                 angle = (intersection_point - self.center).angle
-                alpha = math.acos(intersection_point.distance(self.center) / self.radius)
+                alpha = math.acos(intersection_point.distance(
+                    self.center) / self.radius)
             result.append(self.point_at(angle + alpha))
             result.append(self.point_at(angle - alpha))
-        elif math.isclose(dist, self.radius, abs_tol=abs_tol):  # ray is a tangent of circle
+        # Ray is a tangent of the circle:
+        elif math.isclose(dist, self.radius, abs_tol=abs_tol):
             result.append(intersection_point)
-        # else no intersection
+        # else: No intersection
         return tuple(result)
 
-    def intersect_circle(self, other: 'ConstructionCircle', abs_tol: float = 1e-10) -> Sequence[Vec2]:
-        """
-        Returns intersection points of two circles as sequence of :class:`Vec2` objects.
+    def intersect_circle(self, other: 'ConstructionCircle',
+                         abs_tol: float = 1e-10) -> Sequence[Vec2]:
+        """ Returns intersection points of two circles as sequence of
+        :class:`Vec2` objects.
 
         Args:
             other: intersection circle
-            abs_tol: absolute tolerance for tests (e.g. test for circle touch point)
+            abs_tol: absolute tolerance for tests
 
         Returns:
             tuple of :class:`Vec2` objects
@@ -154,11 +163,13 @@ class ConstructionCircle:
         result = []
         if d_min <= d <= d_max:
             angle = (other.center - self.center).angle
-            # if circles touches in one point
-            if math.isclose(d, d_max, abs_tol=abs_tol) or math.isclose(d, d_min, abs_tol=abs_tol):
+            # Circles touches at one point:
+            if math.isclose(d, d_max, abs_tol=abs_tol) or math.isclose(
+                    d, d_min, abs_tol=abs_tol):
                 result.append(self.point_at(angle))
-            else:  # circles intersect in two points
-                alpha = math.acos((r2 ** 2 - r1 ** 2 - d ** 2) / (-2. * r1 * d))  # 'Cosinus-Satz'
+            else:  # Circles intersect in two points:
+                # Law of Cosines:
+                alpha = math.acos((r2 ** 2 - r1 ** 2 - d ** 2) / (-2. * r1 * d))
                 result.append(self.point_at(angle + alpha))
                 result.append(self.point_at(angle - alpha))
         return tuple(result)
