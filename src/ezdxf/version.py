d73392a2a6549b66133a9bb5205e27c87b7c6dcb
mozman <me@mozman.at>
2020-09-27 11:02:45 +0200

    inc version

--- src/ezdxf/version.py
+++ src/ezdxf/version.py
@@ -24,5 +24,5 @@
 #   1. bug fix release beta0: VERSION = "0.9.1b0"; version = (0, 9, 1, 'b0')
 #   2. bug fix release: VERSION = "0.9.2"; version = (0, 9, 2, 'release')
 
-version = (0, 15, 0, 'a1')
-__version__ = "0.15a1"
+version = (0, 15, 0, 'a2')
+__version__ = "0.15a2"
