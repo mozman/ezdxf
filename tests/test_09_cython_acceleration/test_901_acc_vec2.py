a5a079b263268cd567cf4e7ed123598a3cb82bb5
mozman <me@mozman.at>
2020-11-25 06:32:07 +0100

    add arg "optional=True" to build_ext

---
+++ tests/test_09_cython_acceleration/test_901_acc_vec2.py
@@ -0,0 +1,83 @@
+#  Copyright (c) 2020, Manfred Moitzi
+#  License: MIT License
+# Test only basic features of Cython implementation,
+# Full testing and compatibility check with Python implementation
+# is located in test suite 603.
+
+import pytest
+
+fastmath = pytest.importorskip('ezdxf.acc.fastmath')
+Vec2 = fastmath.Vec2
+
+
+def test_default_constructor():
+    v = Vec2()
+    assert v.x == 0
+    assert v.y == 0
+
+
+def test_init_x_y():
+    v = Vec2(1, 2)
+    assert v.x == 1
+    assert v.y == 2
+
+
+def test_init_2_tuple():
+    v = Vec2((1, 2))
+    assert v.x == 1
+    assert v.y == 2
+
+
+def test_init_vec2():
+    v = Vec2(Vec2(1, 2))
+    assert v.x == 1
+    assert v.y == 2
+
+
+def test_init_3_tuple():
+    v = Vec2((1, 2, 3))
+    assert v.x == 1
+    assert v.y == 2
+
+
+def test_init_vec3():
+    v = Vec2(fastmath.Vec3(1, 2, 3))
+    assert v.x == 1
+    assert v.y == 2
+
+
+def test_init_type_error():
+    with pytest.raises(TypeError):
+        Vec2(1)
+    with pytest.raises(TypeError):
+        Vec2(1, 2, 3, 4)
+    with pytest.raises(TypeError):
+        Vec2('xyz')
+    with pytest.raises(TypeError):
+        Vec2('xyz', 'abc')
+
+
+def test_compare():
+    assert Vec2(1, 2) == Vec2(1, 2)
+    assert Vec2(1, 2) == (1, 2)
+    # abs_tol is 1e-12
+    assert Vec2(1, 2) == (1, 2.0000000000001)
+    assert (1, 2) == Vec2(1, 2)
+    assert Vec2(1, 2) != (2, 1)
+
+
+def test_lower_than():
+    assert Vec2(1, 2) < Vec2(2, 2)
+    assert Vec2(2, 2) > Vec2(1, 2)
+
+
+def test_is_null():
+    assert Vec2().is_null is True
+
+
+def test_bool():
+    assert bool(Vec2()) is False
+
+
+if __name__ == '__main__':
+    pytest.main([__file__])
--- tests/test_09_cython_acceleration/test_902_acc_vec3.py
+++ tests/test_09_cython_acceleration/test_902_acc_vec3.py
@@ -1,78 +1,66 @@
 #  Copyright (c) 2020, Manfred Moitzi
 #  License: MIT License
+# Test only basic features of Cython implementation,
+# Full testing and compatibility check with Python implementation
+# is located in test suite 602.
+
 import pytest
 
 fastmath = pytest.importorskip('ezdxf.acc.fastmath')
-Vec2 = fastmath.Vec2
+Vec3 = fastmath.Vec3
 
 
 def test_default_constructor():
-    v = Vec2()
+    v = Vec3()
     assert v.x == 0
     assert v.y == 0
+    assert v.z == 0
 
 
 def test_init_x_y():
-    v = Vec2(1, 2)
+    v = Vec3(1, 2, 3)
     assert v.x == 1
     assert v.y == 2
+    assert v.z == 3
 
 
 def test_init_2_tuple():
-    v = Vec2((1, 2))
+    v = Vec3((1, 2))
     assert v.x == 1
     assert v.y == 2
+    assert v.z == 0
 
 
 def test_init_vec2():
-    v = Vec2(Vec2(1, 2))
+    v = Vec3(fastmath.Vec2(1, 2))
     assert v.x == 1
     assert v.y == 2
+    assert v.z == 0
 
 
 def test_init_3_tuple():
-    v = Vec2((1, 2, 3))
+    v = Vec3((1, 2, 3))
     assert v.x == 1
     assert v.y == 2
+    assert v.z == 3
 
 
 def test_init_vec3():
-    v = Vec2(fastmath.Vec3(1, 2, 3))
+    v = Vec3(Vec3(1, 2, 3))
     assert v.x == 1
     assert v.y == 2
+    assert v.z == 3
 
 
 def test_init_type_error():
     with pytest.raises(TypeError):
-        Vec2(1)
+        Vec3(1)
     with pytest.raises(TypeError):
-        Vec2(1, 2, 3, 4)
+        Vec3(1, 2, 3, 4)
     with pytest.raises(TypeError):
-        Vec2('xyz')
+        Vec3('xyz')
     with pytest.raises(TypeError):
-        Vec2('xyz', 'abc')
-
-
-def test_compare():
-    assert Vec2(1, 2) == Vec2(1, 2)
-    assert Vec2(1, 2) == (1, 2)
-    # abs_tol is 1e-12
-    assert Vec2(1, 2) == (1, 2.0000000000001)
-    assert (1, 2) == Vec2(1, 2)
-    assert Vec2(1, 2) != (2, 1)
-
-
-def test_lower_than():
-    assert Vec2(1, 2) < Vec2(2, 2)
-    assert Vec2(2, 2) > Vec2(1, 2)
-
-
-def test_is_null():
-    assert Vec2().is_null is True
-
-
-def test_bool():
-    assert bool(Vec2()) is False
+        Vec3('xyz', 'abc')
 
 
 if __name__ == '__main__':
