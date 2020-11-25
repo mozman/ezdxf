a2da5086b5bcc37253f55ef19a5850243e2b4594
mozman <me@mozman.at>
2020-11-25 17:17:31 +0100

    implement Vec2() as extension type (cython)

--- src/ezdxf/acc/fastmath.pyx
+++ src/ezdxf/acc/fastmath.pyx
@@ -1,13 +1,24 @@
 # cython: language_level=3
 # Copyright (c) 2020, Manfred Moitzi
 # License: MIT License
+from typing import Iterable, List, Sequence, TYPE_CHECKING
+from libc.math cimport fabs, sin, cos, M_PI, hypot, atan2, acos
+import cython
 
-from libc.math cimport fabs
+if TYPE_CHECKING:
+    from ezdxf.eztypes import VecXY, Vertex
 
 cdef double abs_tol = 1e-12
 
 cdef bint isclose(double a, double b):
     return fabs(a - b) < abs_tol
+
+cdef bint isclose_abs_tol(double a, double b, double tol):
+    return fabs(a - b) < tol
+
+cdef double RAD2DEG = 180.0 / M_PI
+cdef double DEG2RAD = M_PI / 180.0
+
 
 cdef class Vec2:
     """ Immutable 2D vector.
@@ -56,6 +67,34 @@ cdef class Vec2:
         self.x = args[0]
         self.y = args[1]
 
+    @property
+    def vec3(self) -> 'Vec3':
+        return Vec3(self)
+
+    def round(self, ndigits=None) -> 'Vec2':
+        # only used for testing
+        return Vec2(round(self.x, ndigits), round(self.y, ndigits))
+
+    @staticmethod
+    def list(items: Iterable['Vertex']) -> List['Vec2']:
+        return list(Vec2.generate(items))
+
+    @staticmethod
+    def tuple(items: Iterable['Vertex']) -> Sequence['Vec2']:
+        return tuple(Vec2.generate(items))
+
+    @staticmethod
+    def generate(items: Iterable['Vertex']) -> Iterable['Vec2']:
+        return (Vec2(item) for item in items)
+
+    @staticmethod
+    def from_angle(double angle, double length = 1.) -> 'Vec2':
+        return v2_from_angle(angle, length)
+
+    @staticmethod
+    def from_deg_angle(double angle, double length = 1.0) -> 'Vec2':
+        return v2_from_angle(angle * DEG2RAD, length)
+
     def __str__(self) -> str:
         return '({}, {})'.format(self.x, self.y)
 
@@ -68,10 +107,79 @@ cdef class Vec2:
     def __hash__(self) -> int:
         return hash((self.x, self.y))
 
+    def copy(self) -> 'Vec2':
+        return self  # immutable
+
+    def __copy__(self) -> 'Vec2':
+        return self  # immutable
+
+    def __deepcopy__(self, memodict: dict) -> 'Vec2':
+        try:
+            return memodict[id(self)]
+        except KeyError:
+            memodict[id(self)] = self
+            return self
+
+    def __getitem__(self, index: int) -> float:
+        return (self.x, self.y)[index]
+
+    def __iter__(self) -> Iterable[float]:
+        yield self.x
+        yield self.y
+
+    def __abs__(self) -> float:
+        return hypot(self.x, self.y)
+
+    @property
+    def magnitude(self) -> float:
+        return hypot(self.x, self.y)
+
+    @property
+    def is_null(self) -> bool:
+        return self.x == 0 and self.y == 0
+
+    @property
+    def angle(self) -> float:
+        return atan2(self.y, self.x)
+
+    @property
+    def angle_deg(self) -> float:
+        return atan2(self.y, self.x) * RAD2DEG
+
+    def orthogonal(self, ccw: bool = True) -> 'Vec2':
+        return v2_ortho(self, ccw)
+
+    def lerp(self, other: 'VecXY', double factor = 0.5) -> 'Vec2':
+        cdef Vec2 o = Vec2(other)
+        return v2_lerp(self, o, factor)
+
+    def normalize(self, double length = 1.) -> Vec2:
+        return v2_normalize(self, length)
+
+    def project(self, other: 'VecXY') -> 'Vec2':
+        cdef Vec2 o = Vec2(other)
+        return v2_project(self, o)
+
+    def __neg__(self) -> 'Vec2':
+        cdef Vec2 res = Vec2()
+        res.x = -self.x
+        res.y = -self.y
+        return res
+
+    reversed = __neg__
+
+    def __bool__(self) -> bool:
+        return self.x != 0 or self.y != 0
+
+    def isclose(self, other: 'VecXY', double abs_tol = 1e-12) -> bool:
+        cdef Vec2 o = Vec2(other)
+        return isclose_abs_tol(self.x, o.x, abs_tol) and \
+               isclose_abs_tol(self.y, o.y, abs_tol)
+
     def __eq__(self, other) -> bool:
         if not isinstance(other, Vec2):
             other = Vec2(other)
-        return bool(v2isclose(self, other))
+        return bool(v2_isclose(self, other))
 
     def __lt__(self, other) -> bool:
         cdef Vec2 o = Vec2(other)
@@ -80,12 +188,135 @@ cdef class Vec2:
         else:
             return self.x < o.x
 
-    def __bool__(self) -> bool:
-        return self.x != 0 or self.y != 0
+    def __add__(self, other: 'VecXY') -> 'Vec2':
+        cdef Vec2 o = Vec2(other)
+        return v2_add(self, o)
 
-    @property
-    def is_null(self) -> bool:
-        return self.x == 0 and self.y == 0
+    __iadd__ = __add__  # immutable
+
+    def __sub__(self, other: 'VecXY') -> 'Vec2':
+        cdef Vec2 o = Vec2(other)
+        return v2_sub(self, o)
+
+    def __rsub__(self, other: 'VecXY') -> 'Vec2':
+        cdef Vec2 o = Vec2(other)
+        return v2_sub(o, self)
+
+    __isub__ = __sub__  # immutable
+
+    def __mul__(self, factor) -> 'Vec2':
+        if isinstance(self, Vec2):
+            return v2_mul(self, factor)
+        elif isinstance(factor, Vec2):
+            return v2_mul(<Vec2> factor, <double> self)
+        else:
+            return NotImplemented
+
+    __imul__ = __mul__  # immutable
+
+    def __truediv__(self, double factor) -> 'Vec2':
+        return v2_mul(self, 1.0 / factor)
+
+
+    def dot(self, other: 'VecXY') -> float:
+        cdef Vec2 o = Vec2(other)
+        return v2_dot(self, o)
+
+    def det(self, other: 'VecXY') -> float:
+        cdef Vec2 o = Vec2(other)
+        return v2_det(self, o)
+
+    def distance(self, other: 'VecXY') -> float:
+        cdef Vec2 o = Vec2(other)
+        return v2_dist(self, o)
+
+    def angle_between(self, other: 'VecXY') -> float:
+        cdef Vec2 o = Vec2(other)
+        return v2_angle_between(self, o)
+
+    cpdef rotate(self, double angle: float):
+        cdef double self_angle = atan2(self.y, self.x)
+        cdef double magnitude = hypot(self.x, self.y)
+        return v2_from_angle(self_angle + angle, magnitude)
+
+    def rotate_deg(self, double angle) -> 'Vec2':
+        return self.rotate(angle * DEG2RAD)
+
+    @staticmethod
+    def sum(items: Iterable['Vec2']) -> 'Vec2':
+        cdef Vec2 res = Vec2()
+        cdef Vec2 tmp
+        res.x = 0.0
+        res.y = 0.0
+        for v in items:
+            tmp = v
+            res.x += tmp.x
+            res.y += tmp.y
+        return res
+
+
+cdef Vec2 v2_add(Vec2 a, Vec2 b):
+    res = Vec2()
+    res.x = a.x + b.x
+    res.y = a.y + b.y
+    return res
+
+cdef Vec2 v2_sub(Vec2 a, Vec2 b):
+    res = Vec2()
+    res.x = a.x - b.x
+    res.y = a.y - b.y
+    return res
+
+cdef Vec2 v2_mul(Vec2 a, double factor):
+    res = Vec2()
+    res.x = a.x * factor
+    res.y = a.y * factor
+    return res
+
+cdef double v2_dot(Vec2 a, Vec2 b):
+    return a.x * b.x + a.y * b.y
+
+cdef double v2_det(Vec2 a, Vec2 b):
+    return a.x * b.y - a.y * b.x
+
+cdef double v2_dist(Vec2 a, Vec2 b):
+    return hypot(a.x - b.x, a.y - b.y)
+
+cdef Vec2 v2_from_angle(double angle, double length = 1.0):
+    return Vec2(cos(angle) * length, sin(angle) * length)
+
+cdef double v2_angle_between(Vec2 a, Vec2 b):
+    return acos(v2_dot(v2_normalize(a), v2_normalize(b)))
+
+cdef Vec2 v2_normalize(Vec2 a, double length = 1.0):
+    cdef double factor = length / hypot(a.x, a.y)
+    cdef Vec2 res = Vec2()
+    res.x = a.x * factor
+    res.y = a.y * factor
+    return res
+
+cdef Vec2 v2_lerp(Vec2 a, Vec2 b, double factor = 0.5):
+    cdef Vec2 res = Vec2()
+    res.x = a.x + (b.x - a.x) * factor
+    res.y = a.y + (b.y - a.y) * factor
+    return res
+
+cdef Vec2 v2_ortho(Vec2 a, bint ccw):
+    cdef Vec2 res = Vec2()
+    if ccw:
+        res.x = -a.y
+        res.y = a.x
+    else:
+        res.x = a.y
+        res.y = -a.x
+    return res
+
+cdef Vec2 v2_project(Vec2 a, Vec2 b):
+    cdef Vec2 uv = v2_normalize(a)
+    return v2_mul(uv, v2_dot(uv, b))
+
+cdef bint v2_isclose(Vec2 a, Vec2 b):
+    return isclose(a.x, b.x) and isclose(a.y, b.y)
 
 cdef class Vec3:
     """ Immutable 3D vector.
@@ -139,48 +370,62 @@ cdef class Vec3:
         else:
             self.z = 0.0
 
+    def __len__(self) -> int:
+        return 3
+
+    def __eq__(self, other) -> bool:
+        if not isinstance(other, Vec3):
+            other = Vec3(other)
+        return bool(v3_isclose(self, other))
+
+    def __lt__(self, other) -> bool:
+        cdef Vec3 o = Vec3(other)
+        if self.x == o.x:
+            return self.y < o.y
+        else:
+            return self.x < o.x
+
     def __repr__(self)-> str:
         return f"Vec3({self.x}, {self.y}, {self.z})"
 
     def __add__(self, other) -> 'Vec3':
         if not isinstance(other, Vec3):
             other = Vec3(other)
-        return v3add(self, other)
+        return v3_add(self, other)
 
     def __radd__(self, other) -> 'Vec3':
         if not isinstance(other, Vec3):
             other = Vec3(other)
-        return v3add(other, self)
+        return v3_add(other, self)
 
     def __sub__(self, other) -> 'Vec3':
         if not isinstance(other, Vec3):
             other = Vec3(other)
-        return v3sub(self, other)
+        return v3_sub(self, other)
 
     def __rsub__(self, other) -> 'Vec3':
         if not isinstance(other, Vec3):
             other = Vec3(other)
-        return v3sub(other, self)
+        return v3_sub(other, self)
 
-cdef Vec3 v3add(Vec3 a, Vec3 b):
+X_AXIS = Vec3(1, 0, 0)
+Y_AXIS = Vec3(0, 1, 0)
+Z_AXIS = Vec3(0, 0, 1)
+NULLVEC = Vec3(0, 0, 0)
+
+cdef Vec3 v3_add(Vec3 a, Vec3 b):
     res = Vec3()
     res.x = a.x + b.x
     res.y = a.y + b.y
     res.z = a.z + b.z
     return res
 
-cdef Vec3 v3sub(Vec3 a, Vec3 b):
+cdef Vec3 v3_sub(Vec3 a, Vec3 b):
     res = Vec3()
     res.x = a.x - b.x
     res.y = a.y - b.y
     res.z = a.z - b.z
     return res
 
-cdef Vec2 v2add(Vec2 a, Vec2 b):
-    res = Vec2()
-    res.x = a.x + b.x
-    res.y = a.y + b.y
-    return res
-
-cdef bint v2isclose(Vec2 a, Vec2 b):
-    return isclose(a.x, b.x) and isclose(a.y, b.y)
+cdef bint v3_isclose(Vec3 a, Vec3 b):
+    return isclose(a.x, b.x) and isclose(a.y, b.y) and isclose(a.z, b.z)
--- tests/test_06_math/test_603_vec2.py
+++ tests/test_06_math/test_603_vec2.py
@@ -5,11 +5,25 @@ import math
 # Import from 'ezdxf.math.vector' to test Python implementation
 from ezdxf.math.vector import Vec2, Vec3
 
+all_vec_classes = [Vec2, Vec3]
+vec2_only = [Vec2]
+try:
+    from ezdxf.acc.fastmath import Vec2 as CVec2
+    all_vec_classes.append(CVec2)
+    vec2_only.append(CVec2)
+except ImportError:
+    pass
+
 
 # Vec2 is a sub set of Vec3, Vec3 can do everything Vec2 can do, but not every
 # operation has the same result for 2D and 3D.
-@pytest.fixture(params=[Vec2, Vec3])
+@pytest.fixture(params=all_vec_classes)
 def vcls(request):
+    return request.param
+
+
+@pytest.fixture(params=vec2_only)
+def vec2(request):
     return request.param
 
 
@@ -20,13 +34,13 @@ def test_init_tuple(vcls):
 
 
 def test_init_vec2(vcls):
-    v = Vec2(vcls((2, 3)))
+    v = Vec2(vcls(2, 3))
     assert v.x == 2
     assert v.y == 3
 
 
 def test_compatible_to_vector():
-    v = Vec3(Vec2((1, 2)))
+    v = Vec3(Vec2(1, 2))
     assert v == (1, 2, 0)
 
     v = Vec2(Vec3(1, 2, 3))
@@ -34,16 +48,16 @@ def test_compatible_to_vector():
     assert v.y == 2
 
 
-def test_vec3():
-    v = Vec2((1, 2))
+def test_vec3(vec2):
+    v = vec2(1, 2)
     assert len(v) == 2
     v3 = v.vec3
     assert len(v3) == 3
     assert v3 == (1, 2, 0)
 
 
-def test_round():
-    v = Vec2((1.123, 2.123))
+def test_round(vec2):
+    v = vec2(1.123, 2.123)
     v2 = v.round(1)
     assert v2 == (1.1, 2.1)
 
@@ -55,7 +69,7 @@ def test_from_angle(vcls):
 
 
 def test_vector_as_tuple(vcls):
-    v = vcls((1, 2))
+    v = vcls(1, 2)
     assert v[0] == 1
     assert v[1] == 2
 
@@ -71,19 +85,20 @@ def test_vector_as_tuple(vcls):
 
 
 def test_iter(vcls):
-    assert sum(vcls((1, 2))) == 3
+    assert sum(vcls(1, 2)) == 3
 
 
-def test_deep_copy(vcls):
+def test_deep_copy():
     import copy
 
-    v = vcls((1, 2))
+    v = Vec2(1, 2)
     l1 = [v, v, v]
     l2 = copy.copy(l1)
     assert l2[0] is l2[1]
     assert l2[1] is l2[2]
     assert l2[0] is v
 
+    # Vec3, CVec2 and CVec3 are immutable and do not create copies of itself!
     l3 = copy.deepcopy(l1)
     assert l3[0] is l3[1]
     assert l3[1] is l3[2]
@@ -91,26 +106,26 @@ def test_deep_copy(vcls):
 
 
 def test_get_angle(vcls):
-    v = vcls((3, 3))
+    v = vcls(3, 3)
     assert math.isclose(v.angle_deg, 45)
     assert math.isclose(v.angle, math.radians(45))
 
 
 def test_compare_vectors(vcls):
-    v1 = vcls((1, 2))
+    v1 = vcls(1, 2)
     assert v1 == v1
 
-    v2 = vcls((2, 3))
+    v2 = vcls(2, 3)
     assert v2 > v1
     assert v1 < v2
 
 
 def test_is_null(vcls):
-    v = vcls((0, 0))
-    assert v.is_null
+    v = vcls(0, 0)
+    assert v.is_null is True
 
-    v1 = vcls((23.56678, 56678.56778)) * (1.0 / 14.5667)
-    v2 = vcls((23.56678, 56678.56778)) / 14.5667
+    v1 = vcls(23.56678, 56678.56778) * (1.0 / 14.5667)
+    v2 = vcls(23.56678, 56678.56778) / 14.5667
     assert (v2 - v1).is_null
 
 
@@ -118,42 +133,42 @@ def test_bool(vcls):
     v = vcls((0, 0))
     assert bool(v) is False
 
-    v1 = vcls((23.56678, 56678.56778)) * (1.0 / 14.5667)
-    v2 = vcls((23.56678, 56678.56778)) / 14.5667
+    v1 = vcls(23.56678, 56678.56778) * (1.0 / 14.5667)
+    v2 = vcls(23.56678, 56678.56778) / 14.5667
     result = v2 - v1
     assert bool(result) is False
     # actual precision is abs_tol=1e-9
-    assert not vcls((1e-8, 0)).is_null
+    assert not vcls(1e-8, 0).is_null
 
 
 def test_magnitude(vcls):
-    v = vcls((3, 4))
+    v = vcls(3, 4)
     assert math.isclose(abs(v), 5)
     assert math.isclose(v.magnitude, 5)
 
 
 def test_normalize(vcls):
-    v = vcls((2, 0))
+    v = vcls(2, 0)
     assert v.normalize() == (1, 0)
 
 
 def test_normalize_to_length(vcls):
-    v = vcls((2, 0))
+    v = vcls(2, 0)
     assert v.normalize(4) == (4, 0)
 
 
 def test_orthogonal_ccw(vcls):
-    v = vcls((3, 4))
+    v = vcls(3, 4)
     assert v.orthogonal() == (-4, 3)
 
 
 def test_orthogonal_cw(vcls):
-    v = vcls((3, 4))
+    v = vcls(3, 4)
     assert v.orthogonal(False) == (4, -3)
 
 
 def test_negative(vcls):
-    v = vcls((2, 3))
+    v = vcls(2, 3)
     assert -v == (-2, -3)
 
 
@@ -224,34 +239,14 @@ def test_mul_scalar(vcls):
     assert v * 2 == (4, 6)
 
 
-def test_imul_scalar():
-    # Vec3 supports this operation but is immutable
-    v = Vec2((2, 3))
-    v1 = v
-    v *= 2
-    assert v == (4, 6)
-    assert v1 == (4, 6)
-    assert v1 is v
-
-
 def test_rmul_scalar(vcls):
-    v = vcls((2, 3))
+    v = vcls(2, 3)
     assert 2 * v == (4, 6)
 
 
 def test_div_scalar(vcls):
-    v = vcls((2, 3))
+    v = vcls(2, 3)
     assert v / 2 == (1, 1.5)
-
-
-def test_idiv_scalar():
-    # Vec3 supports this operation but is immutable
-    v = Vec2(2, 3)
-    v1 = v
-    v /= 2
-    assert v == (1, 1.5)
-    assert v1 == (1, 1.5)
-    assert v1 is v
 
 
 def test_rdiv_scalar():
@@ -273,8 +268,8 @@ def test_angle_deg(vcls):
 
 
 def test_angle_between(vcls):
-    v1 = vcls((0, 1))
-    v2 = vcls((1, 1))
+    v1 = vcls(0, 1)
+    v2 = vcls(1, 1)
     angle = v1.angle_between(v2)
     assert math.isclose(angle, math.pi / 4)
     # reverse order, same result
@@ -293,30 +288,30 @@ def test_angle_between_outside_domain():
 
 
 def test_rotate(vcls):
-    assert vcls((2, 2)).rotate_deg(90) == (-2, 2)
+    assert vcls(2, 2).rotate_deg(90) == (-2, 2)
 
 
 def test_lerp(vcls):
-    v1 = vcls((1, 1))
-    v2 = vcls((4, 4))
+    v1 = vcls(1, 1)
+    v2 = vcls(4, 4)
     assert v1.lerp(v2, .5) == (2.5, 2.5)
     assert v1.lerp(v2, 0) == (1, 1)
     assert v1.lerp(v2, 1) == (4, 4)
 
 
 def test_project(vcls):
-    v = vcls((10, 0))
-    assert v.project(vcls((5, 0))) == (5, 0)
-    assert v.project(vcls((5, 5))) == (5, 0)
-    assert v.project(vcls((5, 5))) == (5, 0)
+    v = vcls(10, 0)
+    assert v.project(vcls(5, 0)) == (5, 0)
+    assert v.project(vcls(5, 5)) == (5, 0)
+    assert v.project(vcls(5, 5)) == (5, 0)
 
-    v = vcls((10, 10))
-    assert v.project(vcls((10, 0))) == (5, 5)
+    v = vcls(10, 10)
+    assert v.project(vcls(10, 0)) == (5, 5)
 
 
-def test_det():
-    assert Vec2((1, 0)).det(Vec2((0, 1))) == 1
-    assert Vec2((0, 1)).det(Vec2((1, 0))) == -1
+def test_det(vec2):
+    assert vec2(1, 0).det(vec2(0, 1)) == 1
+    assert vec2(0, 1).det(vec2(1, 0)) == -1
 
 
 def test_sum(vcls):
