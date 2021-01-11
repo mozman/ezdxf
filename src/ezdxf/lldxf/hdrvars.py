cf76cd39320497e78dc23faffdf76112bec44612
mozman <me@mozman.at>
2021-01-11 06:04:57 +0100

    reformat code

--- src/ezdxf/lldxf/hdrvars.py
+++ src/ezdxf/lldxf/hdrvars.py
@@ -1,6 +1,5 @@
 # Purpose: header variables factory
-# Created: 20.11.2010
-# Copyright (c) 2010-2018, Manfred Moitzi
+# Copyright (c) 2010-2021, Manfred Moitzi
 # License: MIT License
 from typing import Sequence, Union
 from .types import DXFVertex, DXFTag, cast_tag_value
@@ -11,7 +10,6 @@ def SingleValue(value: Union[str, float], code: int = 1) -> DXFTag:
 
 
 def Point2D(value: Sequence[float]) -> DXFVertex:
-
     return DXFVertex(10, (value[0], value[1]))
 
 
@@ -20,7 +18,8 @@ def Point3D(value: Sequence[float]) -> DXFVertex:
 
 
 class HeaderVarDef:
-    def __init__(self, name, code, factory, mindxf, maxdxf, priority, default=None):
+    def __init__(self, name, code, factory, mindxf, maxdxf, priority,
+                 default=None):
         self.name = name
         self.code = code
         self.factory = factory
