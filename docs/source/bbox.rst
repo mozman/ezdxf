7bfe379335c544a6a71d6daea12dd7b150b1abd1
mozman <me@mozman.at>
2021-01-26 18:47:00 +0100

    add optional caching to the bbox module (untested)

--- docs/source/bbox.rst
+++ docs/source/bbox.rst
@@ -15,9 +15,32 @@ from the module :mod:`ezdxf.math`.
 The `entities` iterable as input can be the whole modelspace, an entity
 query or any iterable container of DXF entities.
 
-.. autofunction:: extends(entities: Iterable[DXFEntity]) -> BoundingBox
+The optional caching object :class:`Cache` has to be instantiated by the user,
+this is only useful if the same entities will be processed multiple times.
+This cache works only for entities which have a handle, virtual entities
+( e.g block reference content) are created on the fly and do not have a handle
+nor a fixed id, therefore caching is not possible.
 
-.. autofunction:: multi_flat(entities: Iterable[DXFEntity]) -> Iterable[BoundingBox]
+Example usage with caching:
 
-.. autofunction:: multi_recursive(entities: Iterable[DXFEntity]) -> Iterable[BoundingBox]
+.. code-block:: Python
 
+    from ezdxf import bbox
+
+    msp = doc.modelspace()
+    cache = bbox.Cache()
+    # get overall bounding box
+    first_bbox = bbox.extends(msp, cache)
+    # bounding box of all LINE entities
+    second_bbox = bbox.extend(msp.query("LINE"), cache)
+
+
+.. autofunction:: extends(entities: Iterable[DXFEntity], cache: Cache=None) -> BoundingBox
+
+.. autofunction:: multi_flat(entities: Iterable[DXFEntity] cache: Cache=None) -> Iterable[BoundingBox]
+
+.. autofunction:: multi_recursive(entities: Iterable[DXFEntity] cache: Cache=None) -> Iterable[BoundingBox]
+
+.. class:: Cache
+
+    Caching object
--- src/ezdxf/bbox.py
+++ src/ezdxf/bbox.py
@@ -1,6 +1,6 @@
 #  Copyright (c) 2021, Manfred Moitzi
 #  License: MIT License
-from typing import TYPE_CHECKING, Iterable
+from typing import TYPE_CHECKING, Iterable, Dict, Optional
 from ezdxf import disassemble
 from ezdxf.math import BoundingBox
 
@@ -8,8 +8,42 @@ if TYPE_CHECKING:
     from ezdxf.eztypes import DXFEntity
 
 
+class Cache:
+    def __init__(self):
+        self._boxes: Dict[str, BoundingBox] = dict()
+        self.hits: int = 0
+        self.misses: int = 0
+
+    def get(self, entity: 'DXFEntity') -> Optional[BoundingBox]:
+        assert entity is not None
+        handle = entity.dxf.handle
+        if handle is None or handle == '0':
+            self.misses += 1
+            return None
+        box = self._boxes.get(handle)
+        if box is None:
+            self.misses += 1
+        else:
+            self.hits += 1
+        return box
+
+    def store(self, entity: 'DXFEntity', box: BoundingBox) -> None:
+        assert entity is not None
+        handle = entity.dxf.handle
+        if handle is None or handle == '0':
+            return
+        if entity.dxftype() == 'HATCH':
+            # Special treatment for multiple primitives for the same
+            # HATCH entity - all have the same handle:
+            # Do not store boundary path they are not distinguishable,
+            # which boundary path should be returned for the handle?
+            return
+        self._boxes[handle] = box
+
+
 def multi_recursive(
-        entities: Iterable['DXFEntity']) -> Iterable[BoundingBox]:
+        entities: Iterable['DXFEntity'],
+        cache: Cache = None) -> Iterable[BoundingBox]:
     """ Yields all bounding boxes for the given `entities` and all bounding
     boxes for their sub entities.
 
@@ -17,30 +51,43 @@ def multi_recursive(
     flat_entities = disassemble.recursive_decompose(entities)
     primitives = disassemble.to_primitives(flat_entities)
     for primitive in primitives:
-        if not primitive.is_empty:
+        if primitive.is_empty:
+            continue
+
+        entity = primitive.entity
+        if cache is not None:
+            box = cache.get(entity)
+            if box is None:
+                box = BoundingBox(primitive.vertices())
+                if box.has_data:
+                    cache.store(entity, box)
+        else:
             box = BoundingBox(primitive.vertices())
-            if box.has_data:
-                yield box
+
+        if box.has_data:
+            yield box
 
 
-def extends(entities: Iterable['DXFEntity']) -> BoundingBox:
+def extends(entities: Iterable['DXFEntity'],
+            cache: Cache = None) -> BoundingBox:
     """ Returns a single bounding box for the given `entities` and their sub
     entities.
 
     """
     _extends = BoundingBox()
-    for box in multi_recursive(entities):
+    for box in multi_recursive(entities, cache):
         _extends.extend(box)
     return _extends
 
 
-def multi_flat(entities: Iterable['DXFEntity']) -> Iterable[BoundingBox]:
+def multi_flat(entities: Iterable['DXFEntity'],
+               cache: Cache = None) -> Iterable[BoundingBox]:
     """ Yields all bounding boxes for the given `entities` at the top level,
     the sub entity extends are included, but they do not yield their own
     bounding boxes.
 
     """
     for entity in entities:
-        box = extends([entity])
+        box = extends([entity], cache)
         if box.has_data:
             yield box
