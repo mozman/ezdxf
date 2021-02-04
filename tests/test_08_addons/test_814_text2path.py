4bcc17bfa4861e367173593cb865a5b284fb8a46
mozman <me@mozman.at>
2021-02-04 06:20:08 +0100

    add path metrics tests for text2path

--- tests/test_08_addons/test_814_text2path.py
+++ tests/test_08_addons/test_814_text2path.py
@@ -13,6 +13,7 @@ noto_sans_sc_not_found = 'Noto' not in findfont(
 from ezdxf.tools.fonts import FontFace
 from ezdxf.addons import text2path
 from ezdxf.render import Path
+from ezdxf.render import path
 
 
 def _to_paths(s, f='Arial'):
@@ -87,12 +88,64 @@ def test_group_percent_sign():
 @pytest.mark.parametrize('s,c', [
     ["中", 1], ["国", 1], ["文", 2], ["字", 2]
 ])
-def test_group_chinese_chars_and_ignore_holes(s,c):
+def test_group_chinese_chars_and_ignore_holes(s, c):
     paths = _to_paths(s, f=NOTO_SANS_SC)
     result = list(text2path.group_contour_and_holes(paths))
     assert len(result) == c
     contour, holes = result[0]
     assert isinstance(contour, Path)
+
+
+class TestStringToPathMetrics:
+    @pytest.fixture(scope='class')
+    def ff(self):
+        return FontFace(family="Arial")
+
+    # Surprise - even 0 and negative values work without any exceptions!
+    @pytest.mark.parametrize('size', [0, 0.05, 1, 2, 100, -1, -2, -100])
+    def test_text_path_height_for_exact_drawing_units(self, size, ff):
+        paths = text2path.make_paths_from_str("X", font=ff, size=size)
+        bbox = path.bbox(paths)
+        assert bbox.size.y == pytest.approx(abs(size))
+
+    @pytest.mark.parametrize('size', [0.05, 1, 2, 100])
+    def test_path_coordinates_for_positive_size(self, size, ff):
+        paths = text2path.make_paths_from_str("X", font=ff, size=size)
+        bbox = path.bbox(paths)
+        assert bbox.extmax.y == pytest.approx(size)
+        assert bbox.extmin.y == pytest.approx(0)
+
+    @pytest.mark.parametrize('size', [-0.05, -1, -2, -100])
+    def test_path_coordinates_for_negative_size(self, size, ff):
+        # Negative text height mirrors text about the x-axis!
+        paths = text2path.make_paths_from_str("X", font=ff, size=size)
+        bbox = path.bbox(paths)
+        assert bbox.extmax.y == pytest.approx(0)
+        assert bbox.extmin.y == pytest.approx(size)
+
+    @pytest.mark.parametrize('size', [0.05, 1, 2, 100])
+    def test_length_for_fit_alignment(self, size, ff):
+        length = 3
+        paths = text2path.make_paths_from_str(
+            "XXX", font=ff, size=size, align="FIT", length=length)
+        bbox = path.bbox(paths)
+        assert bbox.size.x == pytest.approx(length), "expect exact length"
+        assert bbox.size.y == pytest.approx(size), \
+            "text height should be unscaled"
+
+    @pytest.mark.parametrize('size', [0.05, 1, 2, 100])
+    def test_scaled_height_and_length_for_aligned_text(self, size, ff):
+        length = 3
+        paths = text2path.make_paths_from_str("XXX", font=ff, size=size,
+                                              align="LEFT")
+        default = path.bbox(paths)
+        paths = text2path.make_paths_from_str(
+            "XXX", font=ff, size=size, align="ALIGNED", length=length)
+        bbox = path.bbox(paths)
+        scale = bbox.size.x / default.size.x
+        assert bbox.size.x == pytest.approx(length), "expect exact length"
+        assert bbox.size.y == pytest.approx(size * scale), \
+            "text height should be scaled"
 
 
 if __name__ == '__main__':
