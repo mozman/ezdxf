5f55e34f4d8edd715ccf1038c442933cec9077df
mozman <me@mozman.at>
2021-02-23 08:14:56 +0100

    refactor launcher commands

--- src/ezdxf/__main__.py
+++ src/ezdxf/__main__.py
@@ -3,11 +3,13 @@
 import sys
 import argparse
 from pathlib import Path
-from ezdxf import __version__
+from ezdxf import __version__, options
 from ezdxf.acc import USE_C_EXT
-from ezdxf import options
+from ezdxf import commands
+from ezdxf.tools import fonts
 
 YES_NO = {True: 'yes', False: 'no'}
+options.load_proxy_graphics = True
 
 
 def add_common_arguments(parser):
@@ -25,127 +27,6 @@ def add_common_arguments(parser):
         '--log',
         action='store',
         help="path to a verbose appending log",
-    )
-
-
-def add_pp_parser(subparser):
-    parser = subparser.add_parser(
-        "pp",
-        help="pretty print DXF files as HTML file"
-    )
-    parser.add_argument(
-        'files',
-        metavar='FILE',
-        nargs='+',
-        help='DXF files pretty print',
-    )
-    parser.add_argument(
-        '-o', '--open',
-        action='store_true',
-        help='open generated HTML file with the default web browser',
-    )
-    parser.add_argument(
-        '-r', '--raw',
-        action='store_true',
-        help='raw mode - just print tags, no DXF structure interpretation',
-    )
-    parser.add_argument(
-        '-x', '--nocompile',
-        action='store_true',
-        help="don't compile points coordinates into single tags "
-             "(only in raw mode)",
-    )
-    parser.add_argument(
-        '-l', '--legacy',
-        action='store_true',
-        help="legacy mode - reorders DXF point coordinates",
-    )
-    parser.add_argument(
-        '-s', '--sections',
-        action='store',
-        default='hctbeo',
-        help="choose sections to include and their order, h=HEADER, c=CLASSES, "
-             "t=TABLES, b=BLOCKS, e=ENTITIES, o=OBJECTS",
-    )
-
-
-def add_audit_parser(subparsers):
-    parser = subparsers.add_parser(
-        "audit",
-        help="audit and repair DXF files"
-    )
-    parser.add_argument(
-        'files',
-        metavar='FILE',
-        nargs='+',
-        help='audit DXF files',
-    )
-    parser.add_argument(
-        '-s', '--save',
-        action='store_true',
-        help="save recovered files with extension \".rec.dxf\" "
-    )
-
-
-def add_draw_parser(subparsers):
-    parser = subparsers.add_parser(
-        "draw",
-        help="draw and convert DXF files by Matplotlib"
-    )
-    parser.add_argument(
-        'file',
-        metavar='FILE',
-        nargs='?',
-        help='DXF file to view or convert',
-    )
-    parser.add_argument(
-        '--formats',
-        action='store_true',
-        help="show all supported export formats and exit"
-    )
-    parser.add_argument(
-        '-o', '--out',
-        required=False,
-        help="output filename for export"
-    )
-    parser.add_argument(
-        '--dpi',
-        type=int,
-        default=300,
-        help="target render resolution, default is 300",
-    )
-    parser.add_argument(
-        '--ltype',
-        default='internal',
-        choices=['internal', 'ezdxf'],
-        help="select the line type rendering engine, default is internal",
-    )
-
-
-def add_view_parser(subparsers):
-    parser = subparsers.add_parser(
-        "view",
-        help="view DXF files by PyQt viewer"
-    )
-    parser.add_argument(
-        'file',
-        metavar='FILE',
-        nargs='?',
-        help='DXF file to view',
-    )
-    parser.add_argument(
-        '--ltype',
-        default='internal',
-        choices=['internal', 'ezdxf'],
-        help="select the line type rendering engine, default is internal",
-    )
-    # disable lineweight at all by default:
-    parser.add_argument(
-        '--lwscale',
-        type=float,
-        default=0,
-        help="set custom line weight scaling, default is 0 to disable "
-             "line weights at all",
     )
 
 
@@ -174,8 +55,7 @@ def setup_log(args):
 
 
 DESCRIPTION = """
-This launcher is part of the ezdxf Python package (https://pypi.org/project/ezdxf/) 
-and provides some tools to work with DXF files.
+Command launcher for the Python package "ezdxf": https://pypi.org/project/ezdxf/
 
 """
 
@@ -187,30 +67,23 @@ def main():
     )
     add_common_arguments(parser)
     subparsers = parser.add_subparsers(dest="command")
-    add_pp_parser(subparsers)
-    add_audit_parser(subparsers)
-    add_draw_parser(subparsers)
-    add_view_parser(subparsers)
+    commands.add_parsers(subparsers)
 
     args = parser.parse_args(sys.argv[1:])
+    help_ = True
     options.log_unprocessed_tags = args.verbose
     if args.log:
         setup_log(args)
     if args.version:
         print_version()
-    elif args.command == "pp":
-        from ezdxf.pp import run
+        help_ = False
+
+    run = commands.get(args.command)
+    if run:
+        # For the case automatic font loading is disabled:
+        fonts.load()
         run(args)
-    elif args.command == "audit":
-        from ezdxf.commands import audit
-        audit(args)
-    elif args.command == "draw":
-        from ezdxf.commands import draw
-        draw(args)
-    elif args.command == "view":
-        from ezdxf.commands import view
-        view(args)
-    else:
+    elif help_:
         parser.print_help()
 
 
--- src/ezdxf/commands.py
+++ src/ezdxf/commands.py
@@ -1,5 +1,7 @@
 #  Copyright (c) 2021, Manfred Moitzi
 #  License: MIT License
+from typing import Callable, Optional, Dict
+import abc
 import sys
 import os
 import glob
@@ -7,86 +9,194 @@ import signal
 import logging
 from pathlib import Path
 
-import ezdxf
 from ezdxf import recover
 from ezdxf.lldxf import const
 from ezdxf.lldxf.validator import is_dxf_file
-from ezdxf.tools import fonts
 
-# Load and draw proxy graphic:
-ezdxf.options.load_proxy_graphics = True
+__all__ = ['get', 'add_parsers']
+
 logger = logging.getLogger('ezdxf')
 
 
-def audit(args):
+def get(cmd: str) -> Optional[Callable]:
+    cls = _commands.get(cmd)
+    if cls:
+        return cls.run
+    return None
+
+
+def add_parsers(subparsers) -> None:
+    for cmd in _commands.values():  # in order of registration
+        cmd.add_parser(subparsers)
+
+
+class Command:
+    """ abstract base class for launcher commands """
+    NAME = "command"
+
+    @staticmethod
+    @abc.abstractmethod
+    def add_parser(subparsers) -> None:
+        pass
+
+    @staticmethod
+    @abc.abstractmethod
+    def run(args) -> None:
+        pass
+
+
+_commands: Dict[str, Command] = dict()
+
+
+def register(cls: Command):
+    """ Register a launcher sub-command. """
+    _commands[cls.NAME] = cls
+    return cls
+
+
+@register
+class PrettyPrint(Command):
+    """ Launcher sub-command: pp """
+    NAME = 'pp'
+
+    @staticmethod
+    def add_parser(subparsers):
+        parser = subparsers.add_parser(
+            PrettyPrint.NAME,
+            help="pretty print DXF files as HTML file"
+        )
+        parser.add_argument(
+            'files',
+            metavar='FILE',
+            nargs='+',
+            help='DXF files pretty print',
+        )
+        parser.add_argument(
+            '-o', '--open',
+            action='store_true',
+            help='open generated HTML file by the default web browser',
+        )
+        parser.add_argument(
+            '-r', '--raw',
+            action='store_true',
+            help='raw mode, no DXF structure interpretation',
+        )
+        parser.add_argument(
+            '-x', '--nocompile',
+            action='store_true',
+            help="don't compile points coordinates into single tags "
+                 "(only in raw mode)",
+        )
+        parser.add_argument(
+            '-l', '--legacy',
+            action='store_true',
+            help="legacy mode, reorder DXF point coordinates",
+        )
+        parser.add_argument(
+            '-s', '--sections',
+            action='store',
+            default='hctbeo',
+            help="choose sections to include and their order, h=HEADER, c=CLASSES, "
+                 "t=TABLES, b=BLOCKS, e=ENTITIES, o=OBJECTS",
+        )
+
+    @staticmethod
+    def run(args):
+        from ezdxf.pp import run
+        run(args)
+
+
+@register
+class Audit(Command):
     """ Launcher sub-command: audit """
+    NAME = 'audit'
 
-    def build_outname(name: str) -> str:
-        p = Path(name)
-        return p.parent / (p.stem + ".rec.dxf")
+    @staticmethod
+    def add_parser(subparsers):
+        parser = subparsers.add_parser(
+            Audit.NAME,
+            help="audit and repair DXF files"
+        )
+        parser.add_argument(
+            'files',
+            metavar='FILE',
+            nargs='+',
+            help='audit DXF files',
+        )
+        parser.add_argument(
+            '-s', '--save',
+            action='store_true',
+            help="save recovered files with extension \".rec.dxf\" "
+        )
 
-    def log_fixes(auditor):
-        for error in auditor.fixes:
-            logger.info('fixed:' + error.message)
+    @staticmethod
+    def run(args):
+        def build_outname(name: str) -> str:
+            p = Path(name)
+            return p.parent / (p.stem + ".rec.dxf")
 
-    def log_errors(auditor):
-        for error in auditor.errors:
-            logger.error(error.message)
+        def log_fixes(auditor):
+            for error in auditor.fixes:
+                logger.info('fixed:' + error.message)
 
-    def _audit(filename: str) -> None:
-        msg = f"auditing file: {filename}"
-        print(msg)
-        logger.info(msg)
-        try:
-            doc, auditor = recover.readfile(filename)
-        except IOError:
-            msg = 'Not a DXF file or a generic I/O error.'
+        def log_errors(auditor):
+            for error in auditor.errors:
+                logger.error(error.message)
+
+        def _audit(filename: str) -> None:
+            msg = f"auditing file: {filename}"
             print(msg)
-            logger.error(msg)
-            return  # keep on processing additional files
-        except const.DXFStructureError:
-            msg = 'Invalid or corrupted DXF file.'
-            print(msg)
-            logger.error(msg)
-            return  # keep on processing additional files
+            logger.info(msg)
+            try:
+                doc, auditor = recover.readfile(filename)
+            except IOError:
+                msg = 'Not a DXF file or a generic I/O error.'
+                print(msg)
+                logger.error(msg)
+                return  # keep on processing additional files
+            except const.DXFStructureError:
+                msg = 'Invalid or corrupted DXF file.'
+                print(msg)
+                logger.error(msg)
+                return  # keep on processing additional files
 
-        if auditor.has_errors:
-            auditor.print_error_report()
-            log_errors(auditor)
-        if auditor.has_fixes:
-            auditor.print_fixed_errors()
-            log_fixes(auditor)
+            if auditor.has_errors:
+                auditor.print_error_report()
+                log_errors(auditor)
+            if auditor.has_fixes:
+                auditor.print_fixed_errors()
+                log_fixes(auditor)
 
-        if auditor.has_errors is False and auditor.has_fixes is False:
-            print('No errors found.')
-        else:
-            print(f'Found {len(auditor.errors)} errors, '
-                  f'applied {len(auditor.fixes)} fixes')
+            if auditor.has_errors is False and auditor.has_fixes is False:
+                print('No errors found.')
+            else:
+                print(f'Found {len(auditor.errors)} errors, '
+                      f'applied {len(auditor.fixes)} fixes')
 
-        if args.save:
-            outname = build_outname(filename)
-            doc.saveas(outname)
-            print(f"Saved recovered file as: {outname}")
+            if args.save:
+                outname = build_outname(filename)
+                doc.saveas(outname)
+                print(f"Saved recovered file as: {outname}")
 
-    for pattern in args.files:
-        names = list(glob.glob(pattern))
-        if len(names) == 0:
-            msg = f"File(s) '{pattern}' not found."
-            print(msg)
-            logger.error(msg)
-            continue
-        for filename in names:
-            if not os.path.exists(filename):
-                msg = f"File '{filename}' not found."
+        for pattern in args.files:
+            names = list(glob.glob(pattern))
+            if len(names) == 0:
+                msg = f"File(s) '{pattern}' not found."
                 print(msg)
                 logger.error(msg)
                 continue
-            if not is_dxf_file(filename):
-                msg = f"File '{filename}' is not a DXF file."
-                print(msg)
-                logger.error(msg)
-                continue
-            _audit(filename)
+            for filename in names:
+                if not os.path.exists(filename):
+                    msg = f"File '{filename}' not found."
+                    print(msg)
+                    logger.error(msg)
+                    continue
+                if not is_dxf_file(filename):
+                    msg = f"File '{filename}' is not a DXF file."
+                    print(msg)
+                    logger.error(msg)
+                    continue
+                _audit(filename)
 
 
 def load_document(filename: str):
@@ -97,7 +207,7 @@ def load_document(filename: str):
         print(msg)
         logger.error(msg)
         sys.exit(2)
-    except ezdxf.DXFStructureError:
+    except const.DXFStructureError:
         msg = f'Invalid or corrupted DXF file: {filename}'
         print(msg)
         logger.error(msg)
@@ -115,61 +225,136 @@ def load_document(filename: str):
     return doc, auditor
 
 
-def draw(args):
-    # Import on demand for a quicker startup:
-    import matplotlib.pyplot as plt
-    from ezdxf.addons.drawing import RenderContext, Frontend
-    from ezdxf.addons.drawing.matplotlib import MatplotlibBackend
+@register
+class Draw(Command):
+    """ Launcher sub-command: draw """
+    NAME = 'draw'
 
-    ezdxf.options.load_proxy_graphics = True
-    # For the case automatic font loading is disabled:
-    fonts.load()
+    @staticmethod
+    def add_parser(subparsers):
+        parser = subparsers.add_parser(
+            Draw.NAME,
+            help="draw and convert DXF files by Matplotlib"
+        )
+        parser.add_argument(
+            'file',
+            metavar='FILE',
+            nargs='?',
+            help='DXF file to view or convert',
+        )
+        parser.add_argument(
+            '--formats',
+            action='store_true',
+            help="show all supported export formats and exit"
+        )
+        parser.add_argument(
+            '-o', '--out',
+            required=False,
+            help="output filename for export"
+        )
+        parser.add_argument(
+            '--dpi',
+            type=int,
+            default=300,
+            help="target render resolution, default is 300",
+        )
+        parser.add_argument(
+            '--ltype',
+            default='internal',
+            choices=['internal', 'ezdxf'],
+            help="select the line type rendering engine, default is internal",
+        )
 
-    # Print all supported export formats:
-    if args.formats:
+    @staticmethod
+    def run(args):
+        # Import on demand for a quicker startup:
+        try:
+            import matplotlib.pyplot as plt
+        except ImportError:
+            print('Matplotlib package not found.')
+            sys.exit(2)
+
+        from ezdxf.addons.drawing import RenderContext, Frontend
+        from ezdxf.addons.drawing.matplotlib import MatplotlibBackend
+
+        # Print all supported export formats:
+        if args.formats:
+            fig = plt.figure()
+            for extension, description in fig.canvas.get_supported_filetypes().items():
+                print(f'{extension}: {description}')
+            sys.exit(0)
+
+        if args.file:
+            filename = args.file
+        else:
+            print('argument FILE is required')
+            sys.exit(1)
+
+        doc, _ = load_document(filename)
         fig = plt.figure()
-        for extension, description in fig.canvas.get_supported_filetypes().items():
-            print(f'{extension}: {description}')
-        sys.exit()
+        ax = fig.add_axes([0, 0, 1, 1])
+        ctx = RenderContext(doc)
+        out = MatplotlibBackend(ax, params={'linetype_renderer': args.ltype})
+        Frontend(ctx, out).draw_layout(doc.modelspace(), finalize=True)
+        if args.out is not None:
+            print(f'exporting to "{args.out}"')
+            fig.savefig(args.out, dpi=args.dpi)
+            plt.close(fig)
+        else:
+            plt.show()
 
-    if args.file:
+
+@register
+class View(Command):
+    """ Launcher sub-command: view """
+    NAME = 'view'
+
+    @staticmethod
+    def add_parser(subparsers):
+        parser = subparsers.add_parser(
+            View.NAME,
+            help="view DXF files by the PyQt viewer"
+        )
+        parser.add_argument(
+            'file',
+            metavar='FILE',
+            nargs='?',
+            help='DXF file to view',
+        )
+        parser.add_argument(
+            '--ltype',
+            default='internal',
+            choices=['internal', 'ezdxf'],
+            help="select the line type rendering engine, default is internal",
+        )
+        # disable lineweight at all by default:
+        parser.add_argument(
+            '--lwscale',
+            type=float,
+            default=0,
+            help="set custom line weight scaling, default is 0 to disable "
+                 "line weights at all",
+        )
+
+    @staticmethod
+    def run(args):
+        # Import on demand for a quicker startup:
+        try:
+            from PyQt5 import QtWidgets
+        except ImportError:
+            print('PyQt5 package not found.')
+            sys.exit(1)
+        from ezdxf.addons.drawing.qtviewer import CadViewer
+
+        signal.signal(signal.SIGINT, signal.SIG_DFL)  # handle Ctrl+C properly
+        app = QtWidgets.QApplication(sys.argv)
+        viewer = CadViewer(params={
+            'linetype_renderer': args.ltype,
+            'lineweight_scaling': args.lwscale,
+        })
         filename = args.file
-    else:
-        print(f'argument FILE is required')
-        sys.exit()
-
-    doc, _ = load_document(filename)
-    fig = plt.figure()
-    ax = fig.add_axes([0, 0, 1, 1])
-    ctx = RenderContext(doc)
-    out = MatplotlibBackend(ax, params={'linetype_renderer': args.ltype})
-    Frontend(ctx, out).draw_layout(doc.modelspace(), finalize=True)
-    if args.out is not None:
-        print(f'exporting to "{args.out}"')
-        fig.savefig(args.out, dpi=args.dpi)
-        plt.close(fig)
-    else:
-        plt.show()
-
-
-def view(args):
-    # Import on demand for a quicker startup:
-    from PyQt5 import QtWidgets
-    from ezdxf.addons.drawing.qtviewer import CadViewer
-
-    ezdxf.options.load_proxy_graphics = True
-    # For the case automatic font loading is disabled:
-    fonts.load()
-
-    signal.signal(signal.SIGINT, signal.SIG_DFL)  # handle Ctrl+C properly
-    app = QtWidgets.QApplication(sys.argv)
-    viewer = CadViewer(params={
-        'linetype_renderer': args.ltype,
-        'lineweight_scaling': args.lwscale,
-    })
-    filename = args.file
-    if filename:
-        doc, auditor = load_document(filename)
-        viewer.set_document(doc, auditor)
-        viewer.draw_layout('Model')
-    sys.exit(app.exec_())
+        if filename:
+            doc, auditor = load_document(filename)
+            viewer.set_document(doc, auditor)
+            viewer.draw_layout('Model')
+        sys.exit(app.exec_())
