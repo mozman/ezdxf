508b4ee1361c51ebae87e2de6cd3f81ffc8fdfa5
johnthagen <johnthagen@users.noreply.github.com>
2021-03-23 12:07:56 -0400

    Fix is_closed usage in solid_hatch_polyline_path_with_bulge.py

--- docs/source/tutorials/src/hatch/solid_hatch_polyline_path_with_bulge.py
+++ docs/source/tutorials/src/hatch/solid_hatch_polyline_path_with_bulge.py
@@ -10,6 +10,6 @@ hatch = msp.add_hatch(color=2)  # by default a solid fill hatch with fill color=
 # bulge value 1 = an arc with diameter=10 (= distance to next vertex * bulge value)
 # bulge value > 0 ... arc is right of line
 # bulge value < 0 ... arc is left of line
-hatch.paths.add_polyline_path([(0, 0, 1), (10, 0), (10, 10, -0.5), (0, 10)], is_closed=1)
+hatch.paths.add_polyline_path([(0, 0, 1), (10, 0), (10, 10, -0.5), (0, 10)], is_closed=True)
 
 doc.saveas("solid_hatch_polyline_path_with_bulge.dxf")
