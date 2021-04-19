65a7d6531244315d1a05a5727a5158598c05d9d9
mozman <me@mozman.at>
2021-04-19 16:33:49 +0200

    catch runtime error of Matplotlib #402
    This does not solve the real problem, but at
    least ezdxf does not crash.

--- src/ezdxf/tools/fonts.py
+++ src/ezdxf/tools/fonts.py
@@ -428,7 +428,10 @@ class MatplotlibFont(AbstractFont):
         if not text.strip():
             return 0
         path = self._support_lib.get_text_path(text, self._font_properties)
-        return max(path.vertices[:, 0].tolist()) * self._width_factor
+        try:
+            return max(path.vertices[:, 0].tolist()) * self._width_factor
+        except RuntimeError:
+            return 0
 
 
 class MonospaceFont(AbstractFont):
