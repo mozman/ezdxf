57ad6f608b19826f8de693b0891ca8b55ed512fa
mozman <me@mozman.at>
2021-06-26 12:40:32 +0200

    reformat code by black

--- src/ezdxf/entities/objectcollection.py
+++ src/ezdxf/entities/objectcollection.py
@@ -1,35 +1,40 @@
-# Copyright (c) 2018-2020 Manfred Moitzi
+# Copyright (c) 2018-2021 Manfred Moitzi
 # License: MIT License
 from typing import TYPE_CHECKING, Iterable, cast, Optional
-from ezdxf.lldxf.const import DXFValueError, DXFKeyError
+from ezdxf.lldxf.const import DXFValueError
 
 if TYPE_CHECKING:
     from ezdxf.eztypes import (
-        DXFObject, Dictionary, Drawing, ObjectsSection, EntityDB,
+        DXFObject,
+        Dictionary,
+        Drawing,
+        ObjectsSection,
+        EntityDB,
     )
 
 
 class ObjectCollection:
     def __init__(
         self,
-        doc: 'Drawing',
-        dict_name: str = 'ACAD_MATERIAL',
-        object_type: str = 'MATERIAL'
+        doc: "Drawing",
+        dict_name: str = "ACAD_MATERIAL",
+        object_type: str = "MATERIAL",
     ):
-        self.doc: 'Drawing' = doc
+        self.doc: "Drawing" = doc
         self.object_type: str = object_type
-        self.object_dict: 'Dictionary' = doc.rootdict.get_required_dict(
-            dict_name)
+        self.object_dict: "Dictionary" = doc.rootdict.get_required_dict(
+            dict_name
+        )
 
     @property
-    def objects(self) -> 'ObjectsSection':
+    def objects(self) -> "ObjectsSection":
         return self.doc.objects
 
     @property
-    def entitydb(self) -> 'EntityDB':
+    def entitydb(self) -> "EntityDB":
         return self.doc.entitydb
 
-    def __iter__(self) -> Iterable['DXFObject']:
+    def __iter__(self) -> Iterable["DXFObject"]:
         return self.object_dict.items()
 
     def __len__(self) -> int:
@@ -38,15 +43,13 @@ class ObjectCollection:
     def __contains__(self, name: str) -> bool:
         return name in self.object_dict
 
-    def __getitem__(self, name: str) -> 'DXFObject':
-        return cast('DXFObject', self.object_dict.__getitem__(name))
+    def __getitem__(self, name: str) -> "DXFObject":
+        return cast("DXFObject", self.object_dict.__getitem__(name))
 
     def get(
-        self,
-        name: str,
-        default: 'DXFObject' = None
-    ) -> Optional['DXFObject']:
-        """ Get object by name.
+        self, name: str, default: "DXFObject" = None
+    ) -> Optional["DXFObject"]:
+        """Get object by name.
 
         Args:
             name: object name as string
@@ -55,8 +58,8 @@ class ObjectCollection:
         """
         return self.object_dict.get(name, default)
 
-    def new(self, name: str) -> 'DXFObject':
-        """  Create a new object of type `self.object_type` and store its handle
+    def new(self, name: str) -> "DXFObject":
+        """Create a new object of type `self.object_type` and store its handle
         in the object manager dictionary.
 
         Args:
@@ -73,24 +76,26 @@ class ObjectCollection:
         """
         if name in self.object_dict:
             raise DXFValueError(
-                f'{self.object_type} entry {name} already exists.')
-        return self._new(name, dxfattribs={'name': name})
+                f"{self.object_type} entry {name} already exists."
+            )
+        return self._new(name, dxfattribs={"name": name})
 
-    def _new(self, name: str, dxfattribs: dict) -> 'DXFObject':
+    def _new(self, name: str, dxfattribs: dict) -> "DXFObject":
         owner = self.object_dict.dxf.handle
-        dxfattribs['owner'] = owner
+        dxfattribs["owner"] = owner
         obj = self.objects.add_dxf_object_with_reactor(
-            self.object_type, dxfattribs=dxfattribs)
+            self.object_type, dxfattribs=dxfattribs
+        )
         self.object_dict.add(name, obj)
-        return cast('DXFObject', obj)
+        return cast("DXFObject", obj)
 
     def delete(self, name: str) -> None:
         obj = self.object_dict.get(name)
         if obj is not None:
-            obj = cast('DXFObject', obj)
+            obj = cast("DXFObject", obj)
             self.object_dict.discard(name)
             self.objects.delete_entity(obj)
 
     def clear(self) -> None:
-        """ Delete all entries. """
+        """Delete all entries."""
         self.object_dict.clear()
