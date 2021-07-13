b080a1eddc511daa58e932b0836585a3be089498
mozman <me@mozman.at>
2021-07-13 06:44:39 +0200

    refactor upright module, added tests

--- src/ezdxf/upright.py
+++ src/ezdxf/upright.py
@@ -1,43 +1,10 @@
 # Copyright (c) 2021, Manfred Moitzi
 # License: MIT License
 
-from ezdxf.math import Z_AXIS, Vec3, OCS, OCSTransform, Matrix44
+from ezdxf.math import Z_AXIS, Vec3, OCS, Vertex
 from ezdxf.entities import DXFGraphic, DXFNamespace
 
 __all__ = ["upright"]
-
-FLIPPED_Z_AXIS = -Z_AXIS
-TRANSFORMER = OCSTransform.from_ocs(
-    OCS(FLIPPED_Z_AXIS),
-    OCS(Z_AXIS),
-    Matrix44(),
-)
-
-
-def _flip_thickness(dxf: DXFNamespace) -> None:
-    if dxf.hasattr("thickness"):
-        dxf.thickness = TRANSFORMER.transform_thickness(dxf.thickness)
-
-
-def _flip_circle(dxf: DXFNamespace) -> None:
-    dxf.center = TRANSFORMER.transform_vertex(dxf.center)
-    _flip_thickness(dxf)
-    dxf.discard("extrusion")
-
-
-def _flip_arc(dxf: DXFNamespace) -> None:
-    _flip_circle(dxf)
-    s, e = TRANSFORMER.transform_ccw_arc_angles_deg(
-        dxf.start_angle, dxf.end_angle
-    )
-    dxf.start_angle = s
-    dxf.end_angle = e
-
-
-UPRIGHT_TOOLS = {
-    "CIRCLE": _flip_circle,
-    "ARC": _flip_arc,
-}
 
 
 def upright(entity: DXFGraphic) -> None:
@@ -46,17 +13,89 @@ def upright(entity: DXFGraphic) -> None:
     DXF entities with other extrusion vectors and unsupported DXF entities will
     be silently ignored.
 
+    .. warning::
+
+        The WCS representation as :class:`~ezdxf.path.Path` objects is the same
+        overall but not always 100% identical, the orientation or the starting
+        points of curves can be different.
+
+        E.g. arc angles are always counter-clockwise oriented around the
+        extrusion vector, therefore flipping the extrusion vector creates a
+        similar but not a 100% identical arc.
+
     Supported DXF entities:
 
     - CIRCLE
     - ARC
 
     """
+    # A mirrored text represented by an extrusion vector (0, 0, -1) cannot
+    # represented by an extrusion vector (0, 0, 1), therefore this CANNOT work
+    # for text entities or entities including text:
+    # TEXT, ATTRIB, ATTDEF, MTEXT, DIMENSION, LEADER, MLEADER
+
     if not entity.dxf.hasattr("extrusion"):
         return
     extrusion = Vec3(entity.dxf.extrusion).normalize()
     if not extrusion.isclose(FLIPPED_Z_AXIS):
         return
-    tool = UPRIGHT_TOOLS.get(entity.dxftype())
-    if tool:
-        tool(entity.dxf)
+    simple_tool = SIMPLE_UPRIGHT_TOOLS.get(entity.dxftype())
+    if simple_tool:
+        simple_tool(entity.dxf)
+    else:
+        complex_tool = COMPLEX_UPRIGHT_TOOLS.get(entity.dxftype())
+        if complex_tool:
+            complex_tool(entity)
+
+
+FLIPPED_Z_AXIS = -Z_AXIS
+FLIPPED_OCS = OCS(FLIPPED_Z_AXIS)
+
+
+def _flip_deg_angle(angle: float) -> float:
+    return 180.0 - angle if angle >= 0.0 else -180.0 - angle
+
+
+def _flip_vertex(vertex: Vertex) -> Vertex:
+    return FLIPPED_OCS.to_wcs(vertex)
+
+
+def _flip_thickness(dxf: DXFNamespace) -> None:
+    if dxf.hasattr("thickness"):
+        dxf.thickness = -dxf.thickness
+
+
+def _flip_circle(dxf: DXFNamespace) -> None:
+    dxf.center = _flip_vertex(dxf.center)
+    _flip_thickness(dxf)
+    dxf.discard("extrusion")
+
+
+def _flip_arc(dxf: DXFNamespace) -> None:
+    _flip_circle(dxf)
+    end_angle = dxf.end_angle
+    dxf.end_angle = _flip_deg_angle(dxf.start_angle)
+    dxf.start_angle = _flip_deg_angle(end_angle)
+
+
+# All properties stored as DXF attributes
+SIMPLE_UPRIGHT_TOOLS = {
+    "CIRCLE": _flip_circle,
+    "ARC": _flip_arc,
+    "SOLID": None,
+    "TRACE": None,
+    "ELLIPSE": None,  # WCS entity!
+}
+
+
+def _flip_complex_entity(entity: DXFGraphic) -> None:
+    pass
+
+
+# Additional vertices or paths to transform
+COMPLEX_UPRIGHT_TOOLS = {
+    "LWPOLYLINE": _flip_complex_entity,
+    "POLYLINE": None,  # only 2D POLYLINE
+    "POLYGON": None,  # HATCH, MPOLYGON
+    "INSERT": None,
+}
---
+++ tests/test_02_dxf_graphics/test_251_upright.py
@@ -0,0 +1,95 @@
+#  Copyright (c) 2021, Manfred Moitzi
+#  License: MIT License
+
+import pytest
+import math
+from ezdxf.upright import upright, _flip_deg_angle
+from ezdxf import path
+from ezdxf.entities import Circle, Arc
+from ezdxf.math import Z_AXIS, Matrix44, OCS, OCSTransform
+
+
+@pytest.mark.parametrize(
+    "angle",
+    [
+        0.0,
+        30.0,
+        60.0,
+        90.0,
+        180.0,
+        270.0,
+        -30.0,
+        -60.0,
+        -90.0,
+        -180.0,
+        -270.0,
+    ],
+)
+def test_flip_deg_angle(angle):
+    t = OCSTransform.from_ocs(
+        OCS(-Z_AXIS),
+        OCS(Z_AXIS),
+        Matrix44(),
+    )
+    control_value = t.transform_deg_angle(angle)
+    assert _flip_deg_angle(angle) == pytest.approx(control_value)
+
+
+@pytest.fixture
+def circle():
+    return Circle.new(
+        dxfattribs={"center": (3, 4), "radius": 2.0, "extrusion": (0, 0, -1)}
+    )
+
+
+def test_upright_circle_params(circle):
+    upright(circle)
+    assert circle.dxf.extrusion.isclose(Z_AXIS)
+    assert circle.dxf.center.isclose((-3, 4))
+    assert circle.dxf.radius == 2.0
+
+
+def test_upright_circle_geometry(circle):
+    circle.dxf.center = (0, 0)  # required for rotation!
+    p0 = path.make_path(circle)
+    upright(circle)
+    # IMPORTANT: Circle has a different WCS representation as Path object
+    # Rotated around the z-axis by 180 degrees AND reversed order, because
+    # the start point is always at 0 degrees, determined by the OCS x-axis!
+    p1 = path.make_path(circle).transform(Matrix44.z_rotate(math.pi))
+    assert path.have_close_control_vertices(p0, p1.reversed())
+
+
+@pytest.fixture
+def arc():
+    return Arc.new(
+        dxfattribs={
+            "center": (3, 4),
+            "radius": 2.0,
+            "start_angle": 15,
+            "end_angle": 75,
+            "extrusion": (0, 0, -1),
+        }
+    )
+
+
+def test_upright_arc(arc):
+    upright(arc)
+    assert arc.dxf.extrusion.isclose(Z_AXIS)
+    assert arc.dxf.center.isclose((-3, 4))
+    assert arc.dxf.radius == 2.0
+    assert arc.dxf.start_angle == pytest.approx(105.0)
+    assert arc.dxf.end_angle == pytest.approx(165.0)
+
+
+def test_upright_arc_geometry(arc):
+    p0 = path.make_path(arc)
+    upright(arc)
+    # ARC angles are always in counter-clockwise orientation around the
+    # extrusion vector, therefore a reversed path vertex order:
+    p1 = path.make_path(arc).reversed()
+    assert path.have_close_control_vertices(p0, p1)
+
+
+if __name__ == "__main__":
+    pytest.main([__file__])
