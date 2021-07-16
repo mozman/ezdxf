c0eda655452fa6a91fe1c1596206b1cba7743054
mozman <me@mozman.at>
2021-07-16 06:30:24 +0200

    add upright support for HATCH and MPOLYGON

--- src/ezdxf/upright.py
+++ src/ezdxf/upright.py
@@ -16,10 +16,15 @@
 # work for text entities or entities including text:
 # TEXT, ATTRIB, ATTDEF, MTEXT, DIMENSION, LEADER, MLEADER
 
-from typing import Iterable, List, Sequence
+from typing import Iterable, List, Sequence, Tuple
+from functools import singledispatch
 import math
-from ezdxf.math import Z_AXIS, Vec3
+from ezdxf.math import Z_AXIS, Vec3, Vec2
 from ezdxf.entities import DXFGraphic, DXFNamespace, LWPolyline, Polyline
+from ezdxf.entities.polygon import BasePolygon
+from ezdxf.entities.boundary_paths import (
+    PolylinePath, EdgePath, LineEdge, ArcEdge, EllipseEdge, SplineEdge,
+)
 from ezdxf.lldxf import const
 
 __all__ = ["upright", "upright_all"]
@@ -50,6 +55,8 @@ def upright(entity: DXFGraphic) -> None:
     - TRACE
     - LWPOLYLINE
     - POLYLINE (only 2D entities)
+    - HATCH
+    - MPOLYGON
 
     """
     if not (
@@ -95,6 +102,10 @@ def _flip_rad_angle(angle: float) -> float:
 
 def _flip_vertex(vertex: Vec3) -> Vec3:
     return Vec3(-vertex.x, vertex.y, -vertex.z)
+
+
+def _flip_2d_vertex(vertex: Vec2) -> Vec2:
+    return Vec2(-vertex.x, vertex.y)
 
 
 def _flip_existing_vertex(dxf: DXFNamespace, name: str) -> None:
@@ -150,6 +161,7 @@ SIMPLE_UPRIGHT_TOOLS = {
     "SOLID": _flip_solid,
     "TRACE": _flip_solid,
     "ELLIPSE": _flip_ellipse,
+    "INSERT": None,
 }
 
 
@@ -158,7 +170,7 @@ def _flip_lwpolyline(entity: DXFGraphic) -> None:
     flipped_points: List[Sequence[float]] = []
     for x, y, start_width, end_width, bulge in entity.lwpoints:
         bulge = -bulge
-        v = _flip_vertex(Vec3(x, y))
+        v = _flip_2d_vertex(Vec2(x, y))
         flipped_points.append((v.x, v.y, start_width, end_width, bulge))
     entity.set_points(flipped_points, format="xyseb")
     _finalize_polyline(entity.dxf)
@@ -182,11 +194,79 @@ def _flip_polyline2d(entity: DXFGraphic) -> None:
     _finalize_polyline(entity.dxf)
 
 
+def _flip_polygon(entity: DXFGraphic) -> None:
+    assert isinstance(entity, BasePolygon)
+    for p in entity.paths:
+        _flip_boundary_path(p)
+    _flip_elevation(entity.dxf)
+    entity.dxf.discard("extrusion")
+
+
+@singledispatch
+def _flip_boundary_path(path) -> None:
+    raise TypeError(f"unsupported path type: {path!r}")
+
+
+@_flip_boundary_path.register(PolylinePath)
+def _flip_polyline_path(polyline: PolylinePath) -> None:
+    flipped_vertices: List[Tuple[float, float, float]] = []
+    for x, y, bulge in polyline.vertices:
+        bulge = -bulge
+        v = _flip_2d_vertex(Vec2(x, y))
+        flipped_vertices.append((v.x, v.y, bulge))
+    polyline.vertices = flipped_vertices
+
+
+@_flip_boundary_path.register(EdgePath)
+def _flip_edge_path(edges: EdgePath) -> None:
+    for edge in edges:
+        _flip_edge(edge)
+
+
+@singledispatch
+def _flip_edge(edge) -> None:
+    raise TypeError(f"unsupported edge type: {edge!r}")
+
+
+@_flip_edge.register(LineEdge)
+def _flip_line_edge(edge: LineEdge) -> None:
+    edge.start = _flip_2d_vertex(edge.start)
+    edge.end = _flip_2d_vertex(edge.end)
+
+
+@_flip_edge.register(ArcEdge)
+def _flip_arc_edge(edge: ArcEdge) -> None:
+    edge.center = _flip_2d_vertex(edge.center)
+    edge.start_angle = _flip_deg_angle(edge.start_angle)
+    edge.end_angle = _flip_deg_angle(edge.end_angle)
+    edge.ccw = not edge.ccw
+
+
+@_flip_edge.register(EllipseEdge)
+def _flip_ellipse_edge(edge: EllipseEdge) -> None:
+    edge.center = _flip_2d_vertex(edge.center)
+    edge.major_axis = _flip_2d_vertex(edge.major_axis)
+    # ellipse params as angles in degrees - not radians!
+    edge.start_angle = _flip_deg_angle(edge.start_angle)
+    edge.end_angle = _flip_deg_angle(edge.end_angle)
+    edge.ccw = not edge.ccw
+
+
+@_flip_edge.register(SplineEdge)
+def _flip_spline_edge(edge: SplineEdge) -> None:
+    flip_vertex = _flip_2d_vertex
+    if edge.start_tangent is not None:
+        edge.start_tangent = flip_vertex(edge.start_tangent)
+    if edge.end_tangent is not None:
+        edge.end_tangent = flip_vertex(edge.end_tangent)
+    edge.control_points = [flip_vertex(v) for v in edge.control_points]
+    edge.fit_points = [flip_vertex(v) for v in edge.fit_points]
+
+
 # Additional vertices or paths to transform
 COMPLEX_UPRIGHT_TOOLS = {
     "LWPOLYLINE": _flip_lwpolyline,
     "POLYLINE": _flip_polyline2d,
-    "HATCH": None,
-    "MPOLYGON": None,
-    "INSERT": None,
+    "HATCH": _flip_polygon,
+    "MPOLYGON": _flip_polygon,
 }
