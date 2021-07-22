9da7e9d4b43eaa5e35d97fc0a85f59d544794b92
mozman <me@mozman.at>
2021-07-22 07:31:28 +0200

    add protocols.py
    Goal: clean up the virtual_entities() mess of mixing REQUIREMENT
    and optional DECONSTRUCTION by one method.

---
+++ src/ezdxf/protocols.py
@@ -0,0 +1,43 @@
+#  Copyright (c) 2021, Manfred Moitzi
+#  License: MIT License
+from typing import Iterable, TYPE_CHECKING
+from typing_extensions import Protocol, runtime_checkable
+
+if TYPE_CHECKING:
+    from ezdxf.entities import DXFGraphic
+
+
+@runtime_checkable
+class SupportsVirtualEntities(Protocol):
+    """ The virtual entities protocol is used to disassemble complex entities
+    into DXF primitives like LINE, ARC, ... as REQUIREMENT to process this
+    entities.
+
+    Optional DECONSTRUCTION of entities into DXF primitives like LWPOLYLINE
+    into LINE and ARC entities is NOT the intended usage of this protocol!
+
+    """
+    # The current state of the virtual_entities() method is a mixed usage of
+    # these two use cases.
+    #
+    # Valid usage as REQUIREMENT:
+    # INSERT
+    # DIMENSION
+    # ACAD_PROXY_ENTITY
+    #
+    # Misused for DECONSTRUCTION:
+    # POINT
+    # POLYLINE (2D, 3D, Mesh, Polyface)
+    # LWPOLYLINE
+
+    def __virtual_entities__(self) -> Iterable["DXFGraphic"]:
+        ...
+
+
+def virtual_entities(entity: SupportsVirtualEntities) -> Iterable["DXFGraphic"]:
+    if isinstance(entity, SupportsVirtualEntities):
+        return entity.__virtual_entities__()
+    else:
+        raise TypeError(
+            f"{type(entity)!r} does not support the __virtual_entities__ protocol"
+        )
