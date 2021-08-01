9cf0460081ab84f357e773888e9222b800db0121
mozman <me@mozman.at>
2021-08-01 07:18:33 +0200

    reformat code by black

--- tests/test_02_dxf_graphics/test_208_attrib.py
+++ tests/test_02_dxf_graphics/test_208_attrib.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2019-2020 Manfred Moitzi
+# Copyright (c) 2019-2021 Manfred Moitzi
 # License: MIT License
 import pytest
 
@@ -8,7 +8,7 @@ from ezdxf.lldxf.const import DXF12, DXF2000
 from ezdxf.lldxf.tagwriter import TagCollector, basic_tags_from_text
 
 TEST_CLASS = Attrib
-TEST_TYPE = 'ATTRIB'
+TEST_TYPE = "ATTRIB"
 
 ENTITY_R12 = """0
 ATTRIB
@@ -105,7 +105,7 @@ TAG
 """
 
 
-@pytest.fixture(scope='module')
+@pytest.fixture(scope="module")
 def doc():
     return ezdxf.new()
 
@@ -117,6 +117,7 @@ def entity(request):
 
 def test_registered():
     from ezdxf.entities.factory import ENTITY_CLASSES
+
     assert TEST_TYPE in ENTITY_CLASSES
 
 
@@ -126,32 +127,37 @@ def test_default_init():
 
 
 def test_default_new():
-    entity = TEST_CLASS.new(handle='ABBA', owner='0', dxfattribs={
-        'color': '7',
-        'insert': (1, 2, 3),
-    })
-    assert entity.dxf.layer == '0'
+    entity = TEST_CLASS.new(
+        handle="ABBA",
+        owner="0",
+        dxfattribs={
+            "color": "7",
+            "insert": (1, 2, 3),
+        },
+    )
+    assert entity.dxf.layer == "0"
     assert entity.dxf.color == 7
-    assert entity.dxf.linetype == 'BYLAYER'
+    assert entity.dxf.linetype == "BYLAYER"
     assert entity.dxf.insert == (1, 2, 3)
-    assert entity.dxf.insert.x == 1, 'is not Vec3 compatible'
-    assert entity.dxf.insert.y == 2, 'is not Vec3 compatible'
-    assert entity.dxf.insert.z == 3, 'is not Vec3 compatible'
+    assert entity.dxf.insert.x == 1, "is not Vec3 compatible"
+    assert entity.dxf.insert.y == 2, "is not Vec3 compatible"
+    assert entity.dxf.insert.z == 3, "is not Vec3 compatible"
     # can set DXF R2007 value
     entity.dxf.shadow_mode = 1
     assert entity.dxf.shadow_mode == 1
     assert entity.dxf.extrusion == (0.0, 0.0, 1.0)
-    assert entity.dxf.hasattr('extrusion') is False, 'just the default value'
+    assert entity.dxf.hasattr("extrusion") is False, "just the default value"
 
 
 def test_load_from_text(entity):
-    assert entity.dxf.layer == '0'
-    assert entity.dxf.color == 256, 'default color is 256 (by layer)'
+    assert entity.dxf.layer == "0"
+    assert entity.dxf.color == 256, "default color is 256 (by layer)"
     assert entity.dxf.insert == (0, 0, 0)
 
 
-@pytest.mark.parametrize("txt,ver",
-    [(ENTITY_R2000, DXF2000), (ENTITY_R12, DXF12)])
+@pytest.mark.parametrize(
+    "txt,ver", [(ENTITY_R2000, DXF2000), (ENTITY_R12, DXF12)]
+)
 def test_write_dxf(txt, ver):
     expected = basic_tags_from_text(txt)
     attdef = TEST_CLASS.from_text(txt)
@@ -164,17 +170,20 @@ def test_write_dxf(txt, ver):
     assert collector.has_all_tags(collector2)
 
 
-@pytest.mark.parametrize('invalid_text', [
-    'test\ntext\r',
-    'test\r\ntext',
-    'testtext^',
-    'test\ntext^',
-    'test\ntext^\r',
-])
+@pytest.mark.parametrize(
+    "invalid_text",
+    [
+        "test\ntext\r",
+        "test\r\ntext",
+        "testtext^",
+        "test\ntext^",
+        "test\ntext^\r",
+    ],
+)
 def test_removing_invalid_chars_at_setting_content(invalid_text):
     txt = Attrib()
     txt.dxf.text = invalid_text
-    assert txt.dxf.text == 'testtext'
+    assert txt.dxf.text == "testtext"
 
 
 class TestEmbeddedMTextSupport:
