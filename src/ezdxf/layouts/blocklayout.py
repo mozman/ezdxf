2cf2067e7afaa8ab2d8c8357c2d6d2ec6b3e7d66
mozman <me@mozman.at>
2021-08-08 07:48:54 +0200

    refactor blocklayout.py

--- src/ezdxf/layouts/blocklayout.py
+++ src/ezdxf/layouts/blocklayout.py
@@ -1,21 +1,19 @@
-# Copyright (c) 2019-2020, Manfred Moitzi
+# Copyright (c) 2019-2021, Manfred Moitzi
 # License: MIT License
-from typing import TYPE_CHECKING, Iterable, Optional, Union
+from typing import Iterable, Optional
 from ezdxf.lldxf import const
 from .base import BaseLayout
-
-if TYPE_CHECKING:
-    from ezdxf.eztypes import DXFGraphic, AttDef
+from ezdxf.entities import DXFGraphic, AttDef, Block, EndBlk
 
 
 class BlockLayout(BaseLayout):
-    """ BlockLayout has the same factory-functions as Layout, but is managed
+    """BlockLayout has the same factory-functions as Layout, but is managed
     in the :class:`BlocksSection` class. It represents a DXF Block.
 
     """
 
-    def __contains__(self, entity: Union['DXFGraphic', str]) -> bool:
-        """ Returns ``True`` if block contains `entity`.
+    def __contains__(self, entity) -> bool:
+        """Returns ``True`` if block contains `entity`.
 
         Args:
              entity: :class:`DXFGraphic` object or handle as hex string
@@ -23,38 +21,39 @@ class BlockLayout(BaseLayout):
         """
         if isinstance(entity, str):
             entity = self.entitydb[entity]
-        return entity in self.entity_space
+        assert isinstance(entity, DXFGraphic)
+        return entity in self.entity_space  # type: ignore
 
     @property
-    def block(self):
-        """ the associated :class:`~ezdxf.entities.Block` entity. """
-        return self.block_record.block
+    def block(self) -> Block:
+        """the associated :class:`~ezdxf.entities.Block` entity."""
+        return self.block_record.block  # type: ignore
 
     @property
-    def endblk(self):
-        """ the associated :class:`~ezdxf.entities.EndBlk` entity. """
-        return self.block_record.endblk
+    def endblk(self) -> EndBlk:
+        """the associated :class:`~ezdxf.entities.EndBlk` entity."""
+        return self.block_record.endblk  # type: ignore
 
     @property
     def name(self) -> str:
-        """ Get block and block_record name """
+        """Get block and block_record name"""
         return self.block_record.dxf.name
 
     @name.setter
     def name(self, new_name) -> None:
-        """ Set block and block_record name """
+        """Set block and block_record name"""
         self.block_record.rename(new_name)
 
     @property
     def dxf(self):
-        """ DXF name space of associated :class:`~ezdxf.entities.BlockRecord`
+        """DXF name space of associated :class:`~ezdxf.entities.BlockRecord`
         table entry.
         """
         return self.block_record.dxf
 
     @property
     def can_explode(self) -> bool:
-        """ Set property to ``True`` to allow exploding block references of
+        """Set property to ``True`` to allow exploding block references of
         this block.
         """
         return bool(self.block_record.dxf.explode)
@@ -65,7 +64,7 @@ class BlockLayout(BaseLayout):
 
     @property
     def scale_uniformly(self) -> bool:
-        """ Set property to ``True`` to allow block references of this block
+        """Set property to ``True`` to allow block references of this block
         only scale uniformly.
         """
         return bool(self.block_record.dxf.scale)
@@ -74,28 +73,29 @@ class BlockLayout(BaseLayout):
     def scale_uniformly(self, value: bool):
         self.block_record.dxf.scale = int(value)
 
-    def attdefs(self) -> Iterable['AttDef']:
-        """ Returns iterable of all :class:`~ezdxf.entities.attrib.Attdef`
+    def attdefs(self) -> Iterable[AttDef]:
+        """Returns iterable of all :class:`~ezdxf.entities.attrib.Attdef`
         entities.
         """
-        return (entity for entity in self if entity.dxftype() == 'ATTDEF')
+        return (e for e in self if isinstance(e, AttDef))
 
     def has_attdef(self, tag: str) -> bool:
-        """ Returns ``True`` if an :class:`~ezdxf.entities.attrib.Attdef` for
+        """Returns ``True`` if an :class:`~ezdxf.entities.attrib.Attdef` for
         `tag` exist.
         """
         return self.get_attdef(tag) is not None
 
-    def get_attdef(self, tag: str) -> Optional['DXFGraphic']:
-        """ Returns attached :class:`~ezdxf.entities.attrib.Attdef` entity by
+    def get_attdef(self, tag: str) -> Optional[DXFGraphic]:
+        """Returns attached :class:`~ezdxf.entities.attrib.Attdef` entity by
         `tag` name.
         """
         for attdef in self.attdefs():
             if tag == attdef.dxf.tag:
                 return attdef
+        return None
 
-    def get_attdef_text(self, tag: str, default: str = None) -> str:
-        """ Returns text content for :class:`~ezdxf.entities.attrib.Attdef`
+    def get_attdef_text(self, tag: str, default: str = "") -> str:
+        """Returns text content for :class:`~ezdxf.entities.attrib.Attdef`
         `tag` as string or returns `default` if no :class:`Attdef` for `tag`
         exist.
 
@@ -109,13 +109,12 @@ class BlockLayout(BaseLayout):
             return default
         return attdef.dxf.text
 
-    def get_const_attdefs(self) -> Iterable['AttDef']:
-        """ Returns iterable for all constant ATTDEF entities. (internal API)
-        """
+    def get_const_attdefs(self) -> Iterable[AttDef]:
+        """Returns iterable for all constant ATTDEF entities. (internal API)"""
         return (attdef for attdef in self.attdefs() if attdef.is_const)
 
     def has_non_const_attdef(self) -> bool:
-        """ Returns ``True`` if the block has a non constant attribute
+        """Returns ``True`` if the block has a non constant attribute
         definition.
         """
         return any(not attdef.is_const for attdef in self.attdefs())
