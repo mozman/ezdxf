df9b5f926f6414f5e963615fc66766931c0263f9
mozman <me@mozman.at>
2021-08-20 09:55:01 +0200

    add integration test for the MTextExplode add-on

--- integration_tests/test_mtext_explode_addon.py
+++ integration_tests/test_mtext_explode_addon.py
@@ -1,11 +1,7 @@
 #  Copyright (c) 2021, Manfred Moitzi
 #  License: MIT License
-import pathlib
 import ezdxf
-from ezdxf import zoom
 from ezdxf.addons import MTextExplode
-
-DIR = pathlib.Path("~/Desktop/Outbox").expanduser()
 
 LOREM_IPSUM = (
     r"Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam "
@@ -35,46 +31,24 @@ def new_doc(content: str, width: float = 30):
         },
     )
     mtext.set_bg_color(None, text_frame=True)
-    zoom.extents(msp)
     return doc
 
 
-def explode_mtext(doc, filename, destroy=False):
+def explode_mtext(doc, destroy=False):
     msp = doc.modelspace()
     with MTextExplode(msp) as xpl:
         for mtext in msp.query("MTEXT"):
             xpl.explode(mtext, destroy=destroy)
             if mtext.is_alive:
                 mtext.dxf.layer = "SOURCE"
-    zoom.extents(msp)
-    doc.saveas(DIR / filename)
 
 
-def explode_mtext_to_block(doc, filename, destroy=False):
-    msp = doc.modelspace()
-    blk = doc.blocks.new("EXPLODE")
-    with MTextExplode(blk) as xpl:
-        for mtext in msp.query("MTEXT"):
-            xpl.explode(mtext, destroy=destroy)
-            if mtext.is_alive:
-                mtext.dxf.layer = "SOURCE"
-    msp.add_blockref("EXPLODE", (0, 0))
-    zoom.extents(msp)
-    doc.saveas(DIR / filename)
-
-
-def create(filename):
+def test_addon_is_still_working():
+    # Testing only the basic functionality!
+    # The text layout engine has its own test suite in test file 517!
+    # The MTextParser has its own test suite in test file 521!
     doc = new_doc(LEFT + CENTER + RIGHT + JUSTIFIED)
-    doc.saveas(DIR / filename)
-    return doc
-
-
-def load(filename):
-    return ezdxf.readfile(DIR / filename)
-
-
-if __name__ == "__main__":
-    doc = create("mtext_source.dxf")
-    explode_mtext_to_block(doc, "mtext_xpl_blk.dxf", destroy=True)
-    # doc = load("mtext_tab_stops.dxf")
-    # explode_mtext(doc, "mtext_xpl_tabs.dxf", destroy=False)
+    explode_mtext(doc, destroy=True)
+    msp = doc.modelspace()
+    assert len(msp.query("MTEXT")) == 0
+    assert len(msp.query("TEXT")) == 208  # checked by BricsCAD
