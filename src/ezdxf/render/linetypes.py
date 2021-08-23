ebc4d58d218e815fde3baebc3c48ebf37b713612
mozman <me@mozman.at>
2021-08-23 08:56:40 +0200

    set mypy option "allow_redefinition" to True

--- src/ezdxf/render/linetypes.py
+++ src/ezdxf/render/linetypes.py
@@ -1,4 +1,4 @@
-#  Copyright (c) 2020, Manfred Moitzi
+#  Copyright (c) 2020-2021, Manfred Moitzi
 #  License: MIT License
 from typing import Tuple, Iterable
 import math
@@ -13,18 +13,17 @@ class LineTypeRenderer:
         # Dash pattern should end with a gap (even count).
         # Dash length in drawing units.
 
-        self._dashes = tuple(dashes)
-        self._dash_count = len(self._dashes)
-        self.is_solid = True
-        self._current_dash = 0
-        self._current_dash_length = 0
+        self._dashes: Tuple[float] = tuple(dashes)  # type: ignore  # why Tuple[float, ...]?
+        self._dash_count: int = len(self._dashes)
+        self.is_solid: bool = True
+        self._current_dash: int = 0
+        self._current_dash_length: float = 0.0
         if self._dash_count > 1:
             self.is_solid = False
             self._current_dash_length = self._dashes[0]
             self._is_dash = True
 
-    def line_segment(
-            self, start: Vertex, end: Vertex) -> Iterable[LineSegment]:
+    def line_segment(self, start: Vertex, end: Vertex) -> Iterable[LineSegment]:
         start = Vec3(start)
         end = Vec3(end)
         if self.is_solid or start.isclose(end):
@@ -42,14 +41,15 @@ class LineTypeRenderer:
             start = end
 
     def line_segments(
-            self, vertices: Iterable[Vertex]) -> Iterable[LineSegment]:
+        self, vertices: Iterable[Vertex]
+    ) -> Iterable[LineSegment]:
         last = None
         for vertex in vertices:
             if last is not None:
                 yield from self.line_segment(last, vertex)
             last = vertex
 
-    def _render_dashes(self, length: float) -> Tuple[bool, float]:
+    def _render_dashes(self, length: float) -> Iterable[Tuple[bool, float]]:
         if length <= self._current_dash_length:
             self._current_dash_length -= length
             yield self._is_dash, length
