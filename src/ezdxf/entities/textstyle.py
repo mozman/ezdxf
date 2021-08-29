d68b0eeee81de558e2cf266a0facefa66335f706
mozman <me@mozman.at>
2021-08-29 11:20:27 +0200

    reformat code by black

--- src/ezdxf/entities/textstyle.py
+++ src/ezdxf/entities/textstyle.py
@@ -1,68 +1,73 @@
-# Copyright (c) 2019-2020, Manfred Moitzi
+# Copyright (c) 2019-2021, Manfred Moitzi
 # License: MIT License
 from typing import TYPE_CHECKING, Tuple
 import logging
 from ezdxf.lldxf import validator, const
 from ezdxf.lldxf.attributes import (
-    DXFAttr, DXFAttributes, DefSubclass, RETURN_DEFAULT, group_code_mapping,
+    DXFAttr,
+    DXFAttributes,
+    DefSubclass,
+    RETURN_DEFAULT,
+    group_code_mapping,
 )
 from ezdxf.lldxf.const import DXF12, SUBCLASS_MARKER
 from ezdxf.entities.dxfentity import base_class, SubclassProcessor, DXFEntity
 from ezdxf.entities.layer import acdb_symbol_table_record
 from .factory import register_entity
 
-logger = logging.getLogger('ezdxf')
+logger = logging.getLogger("ezdxf")
 
 if TYPE_CHECKING:
     from ezdxf.eztypes import TagWriter, DXFNamespace
 
-__all__ = ['Textstyle']
+__all__ = ["Textstyle"]
 
-acdb_style = DefSubclass('AcDbTextStyleTableRecord', {
-    'name': DXFAttr(
-        2, default='Standard',
-        validator=validator.is_valid_table_name,
-    ),
-    # Flags: Standard flag values (bit-coded values):
-    # 1 = If set, this entry describes a shape
-    # 4 = Vertical text
-    # 16 = If set, table entry is externally dependent on an xref
-    # 32 = If both this bit and bit 16 are set, the externally dependent xref ...
-    # 64 = If set, the table entry was referenced by at least one entity in ...
-    # Vertical text works only for SHX fonts in AutoCAD and BricsCAD
-    'flags': DXFAttr(70, default=0),
-
-    # Fixed height, 0 if not fixed
-    'height': DXFAttr(
-        40, default=0,
-        validator=validator.is_greater_or_equal_zero,
-        fixer=RETURN_DEFAULT,
-    ),
-    # Width factor:  a.k.a. "Stretch"
-    'width': DXFAttr(
-        41, default=1,
-        validator=validator.is_greater_zero,
-        fixer=RETURN_DEFAULT,
-    ),
-    # Oblique angle in degree, 0 = vertical
-    'oblique': DXFAttr(50, default=0),
-
-    # Generation flags:
-    # 2 = backward
-    # 4 = mirrored in Y
-    'generation_flags': DXFAttr(71, default=0),
-
-    # Last height used:
-    'last_height': DXFAttr(42, default=2.5),
-
-    # Primary font file name:
-    # ATTENTION: The font file name can be an empty string and the font family
-    # may be stored in XDATA! See also posts at the (unrelated) issue #380.
-    'font': DXFAttr(3, default='txt'),
-
-    # Big font name, blank if none
-    'bigfont': DXFAttr(4, default=''),
-})
+acdb_style = DefSubclass(
+    "AcDbTextStyleTableRecord",
+    {
+        "name": DXFAttr(
+            2,
+            default="Standard",
+            validator=validator.is_valid_table_name,
+        ),
+        # Flags: Standard flag values (bit-coded values):
+        # 1 = If set, this entry describes a shape
+        # 4 = Vertical text
+        # 16 = If set, table entry is externally dependent on an xref
+        # 32 = If both this bit and bit 16 are set, the externally dependent xref ...
+        # 64 = If set, the table entry was referenced by at least one entity in ...
+        # Vertical text works only for SHX fonts in AutoCAD and BricsCAD
+        "flags": DXFAttr(70, default=0),
+        # Fixed height, 0 if not fixed
+        "height": DXFAttr(
+            40,
+            default=0,
+            validator=validator.is_greater_or_equal_zero,
+            fixer=RETURN_DEFAULT,
+        ),
+        # Width factor:  a.k.a. "Stretch"
+        "width": DXFAttr(
+            41,
+            default=1,
+            validator=validator.is_greater_zero,
+            fixer=RETURN_DEFAULT,
+        ),
+        # Oblique angle in degree, 0 = vertical
+        "oblique": DXFAttr(50, default=0),
+        # Generation flags:
+        # 2 = backward
+        # 4 = mirrored in Y
+        "generation_flags": DXFAttr(71, default=0),
+        # Last height used:
+        "last_height": DXFAttr(42, default=2.5),
+        # Primary font file name:
+        # ATTENTION: The font file name can be an empty string and the font family
+        # may be stored in XDATA! See also posts at the (unrelated) issue #380.
+        "font": DXFAttr(3, default="txt"),
+        # Big font name, blank if none
+        "bigfont": DXFAttr(4, default=""),
+    },
+)
 acdb_style_group_codes = group_code_mapping(acdb_style)
 
 
@@ -79,37 +84,49 @@ acdb_style_group_codes = group_code_mapping(acdb_style)
 
 @register_entity
 class Textstyle(DXFEntity):
-    """ DXF STYLE entity """
-    DXFTYPE = 'STYLE'
+    """DXF STYLE entity"""
+
+    DXFTYPE = "STYLE"
     DXFATTRIBS = DXFAttributes(base_class, acdb_symbol_table_record, acdb_style)
     BOLD = 0b01000000000000000000000000
     ITALIC = 0b10000000000000000000000000
 
     def load_dxf_attribs(
-            self, processor: SubclassProcessor = None) -> 'DXFNamespace':
+        self, processor: SubclassProcessor = None
+    ) -> "DXFNamespace":
         dxf = super().load_dxf_attribs(processor)
         if processor:
             processor.fast_load_dxfattribs(dxf, acdb_style_group_codes, 2)
         return dxf
 
-    def export_entity(self, tagwriter: 'TagWriter') -> None:
+    def export_entity(self, tagwriter: "TagWriter") -> None:
         super().export_entity(tagwriter)
         if tagwriter.dxfversion > DXF12:
             tagwriter.write_tag2(SUBCLASS_MARKER, acdb_symbol_table_record.name)
             tagwriter.write_tag2(SUBCLASS_MARKER, acdb_style.name)
 
-        self.dxf.export_dxf_attribs(tagwriter, [
-            'name', 'flags', 'height', 'width', 'oblique', 'generation_flags',
-            'last_height', 'font', 'bigfont'
-        ])
+        self.dxf.export_dxf_attribs(
+            tagwriter,
+            [
+                "name",
+                "flags",
+                "height",
+                "width",
+                "oblique",
+                "generation_flags",
+                "last_height",
+                "font",
+                "bigfont",
+            ],
+        )
 
     @property
     def has_extended_font_data(self) -> bool:
-        """ Returns ``True`` if extended font data is present. """
-        return self.has_xdata('ACAD')
+        """Returns ``True`` if extended font data is present."""
+        return self.has_xdata("ACAD")
 
     def get_extended_font_data(self) -> Tuple[str, bool, bool]:
-        """ Returns extended font data as tuple (font-family, italic-flag,
+        """Returns extended font data as tuple (font-family, italic-flag,
         bold-flag).
 
         The extended font data is optional and not reliable! Returns
@@ -120,7 +137,7 @@ class Textstyle(DXFEntity):
         italic = False
         bold = False
         try:
-            xdata = self.get_xdata('ACAD')
+            xdata = self.get_xdata("ACAD")
         except const.DXFValueError:
             pass
         else:
@@ -134,55 +151,54 @@ class Textstyle(DXFEntity):
                     bold = bool(self.BOLD & value)
         return family, italic, bold
 
-    def set_extended_font_data(self, family: str = "", *,
-                               italic=False,
-                               bold=False) -> None:
-        """ Set extended font data, the font-family name `family` is not
+    def set_extended_font_data(
+        self, family: str = "", *, italic=False, bold=False
+    ) -> None:
+        """Set extended font data, the font-family name `family` is not
         validated by `ezdxf`. Overwrites existing data.
         """
-        if self.has_xdata('ACAD'):
-            self.discard_xdata('ACAD')
+        if self.has_xdata("ACAD"):
+            self.discard_xdata("ACAD")
 
         flags = 34  # unknown default flags
         if italic:
             flags += self.ITALIC
         if bold:
             flags += self.BOLD
-        self.set_xdata('ACAD', [(1000, family), (1071, flags)])
+        self.set_xdata("ACAD", [(1000, family), (1071, flags)])
 
     def discard_extended_font_data(self):
-        """ Discard extended font data. """
-        self.discard_xdata('ACAD')
+        """Discard extended font data."""
+        self.discard_xdata("ACAD")
 
     @property
     def is_backward(self) -> bool:
-        """ Get/set text generation flag BACKWARDS, for mirrored text along the
+        """Get/set text generation flag BACKWARDS, for mirrored text along the
         x-axis.
         """
-        return self.get_flag_state(const.BACKWARD, 'generation_flags')
+        return self.get_flag_state(const.BACKWARD, "generation_flags")
 
     @is_backward.setter
     def is_backward(self, state) -> None:
-        self.set_flag_state(const.BACKWARD, state, 'generation_flags')
+        self.set_flag_state(const.BACKWARD, state, "generation_flags")
 
     @property
     def is_upside_down(self) -> bool:
-        """ Get/set text generation flag UPSIDE_DOWN, for mirrored text along
+        """Get/set text generation flag UPSIDE_DOWN, for mirrored text along
         the y-axis.
 
         """
-        return self.get_flag_state(const.UPSIDE_DOWN, 'generation_flags')
+        return self.get_flag_state(const.UPSIDE_DOWN, "generation_flags")
 
     @is_upside_down.setter
     def is_upside_down(self, state) -> None:
-        self.set_flag_state(const.UPSIDE_DOWN, state, 'generation_flags')
+        self.set_flag_state(const.UPSIDE_DOWN, state, "generation_flags")
 
     @property
     def is_vertical_stacked(self) -> bool:
-        """ Get/set style flag VERTICAL_STACKED, for vertical stacked text.
-        """
-        return self.get_flag_state(const.VERTICAL_STACKED, 'flags')
+        """Get/set style flag VERTICAL_STACKED, for vertical stacked text."""
+        return self.get_flag_state(const.VERTICAL_STACKED, "flags")
 
     @is_vertical_stacked.setter
     def is_vertical_stacked(self, state) -> None:
-        self.set_flag_state(const.VERTICAL_STACKED, state, 'flags')
+        self.set_flag_state(const.VERTICAL_STACKED, state, "flags")
--- src/ezdxf/entities/tolerance.py
+++ src/ezdxf/entities/tolerance.py
@@ -1,9 +1,13 @@
-# Copyright (c) 2019-2020 Manfred Moitzi
+# Copyright (c) 2019-2021 Manfred Moitzi
 # License: MIT License
 from typing import TYPE_CHECKING
 from ezdxf.lldxf import validator
 from ezdxf.lldxf.attributes import (
-    DXFAttr, DXFAttributes, DefSubclass, XType, RETURN_DEFAULT,
+    DXFAttr,
+    DXFAttributes,
+    DefSubclass,
+    XType,
+    RETURN_DEFAULT,
     group_code_mapping,
 )
 from ezdxf.lldxf.const import SUBCLASS_MARKER, DXF2000
@@ -16,60 +20,70 @@ from .factory import register_entity
 if TYPE_CHECKING:
     from ezdxf.eztypes import TagWriter, DXFNamespace, Matrix44
 
-__all__ = ['Tolerance']
+__all__ = ["Tolerance"]
 
-acdb_tolerance = DefSubclass('AcDbFcf', {
-    'dimstyle': DXFAttr(
-        3, default='Standard',
-        validator=validator.is_valid_table_name,
-    ),
-    # Insertion point (in WCS):
-    'insert': DXFAttr(10, xtype=XType.point3d, default=NULLVEC),
-
-    # String representing the visual representation of the tolerance:
-    'content': DXFAttr(1, default=""),
-    'extrusion': DXFAttr(
-        210, xtype=XType.point3d, default=Z_AXIS, optional=True,
-        validator=validator.is_not_null_vector,
-        fixer=RETURN_DEFAULT,
-    ),
-    # X-axis direction vector (in WCS):
-    'x_axis_vector': DXFAttr(
-        11, xtype=XType.point3d, default=X_AXIS,
-        validator=validator.is_not_null_vector,
-        fixer=RETURN_DEFAULT,
-    ),
-
-})
+acdb_tolerance = DefSubclass(
+    "AcDbFcf",
+    {
+        "dimstyle": DXFAttr(
+            3,
+            default="Standard",
+            validator=validator.is_valid_table_name,
+        ),
+        # Insertion point (in WCS):
+        "insert": DXFAttr(10, xtype=XType.point3d, default=NULLVEC),
+        # String representing the visual representation of the tolerance:
+        "content": DXFAttr(1, default=""),
+        "extrusion": DXFAttr(
+            210,
+            xtype=XType.point3d,
+            default=Z_AXIS,
+            optional=True,
+            validator=validator.is_not_null_vector,
+            fixer=RETURN_DEFAULT,
+        ),
+        # X-axis direction vector (in WCS):
+        "x_axis_vector": DXFAttr(
+            11,
+            xtype=XType.point3d,
+            default=X_AXIS,
+            validator=validator.is_not_null_vector,
+            fixer=RETURN_DEFAULT,
+        ),
+    },
+)
 acdb_tolerance_group_codes = group_code_mapping(acdb_tolerance)
 
 
 @register_entity
 class Tolerance(DXFGraphic):
-    """ DXF TOLERANCE entity """
-    DXFTYPE = 'TOLERANCE'
+    """DXF TOLERANCE entity"""
+
+    DXFTYPE = "TOLERANCE"
     DXFATTRIBS = DXFAttributes(base_class, acdb_entity, acdb_tolerance)
     MIN_DXF_VERSION_FOR_EXPORT = DXF2000
 
     def load_dxf_attribs(
-            self, processor: SubclassProcessor = None) -> 'DXFNamespace':
+        self, processor: SubclassProcessor = None
+    ) -> "DXFNamespace":
         dxf = super().load_dxf_attribs(processor)
         if processor:
             processor.fast_load_dxfattribs(
-                dxf, acdb_tolerance_group_codes, subclass=2, recover=True)
+                dxf, acdb_tolerance_group_codes, subclass=2, recover=True
+            )
         return dxf
 
-    def export_entity(self, tagwriter: 'TagWriter') -> None:
-        """ Export entity specific data as DXF tags. """
+    def export_entity(self, tagwriter: "TagWriter") -> None:
+        """Export entity specific data as DXF tags."""
         super().export_entity(tagwriter)
         tagwriter.write_tag2(SUBCLASS_MARKER, acdb_tolerance.name)
-        self.dxf.export_dxf_attribs(tagwriter, [
-            'dimstyle', 'insert', 'content', 'extrusion', 'x_axis_vector'
-        ])
+        self.dxf.export_dxf_attribs(
+            tagwriter,
+            ["dimstyle", "insert", "content", "extrusion", "x_axis_vector"],
+        )
 
-    def transform(self, m: 'Matrix44') -> 'Tolerance':
-        """ Transform the TOLERANCE entity by transformation matrix `m` inplace.
-        """
+    def transform(self, m: "Matrix44") -> "Tolerance":
+        """Transform the TOLERANCE entity by transformation matrix `m` inplace."""
         self.dxf.insert = m.transform(self.dxf.insert)
         self.dxf.x_axis_vector = m.transform_direction(self.dxf.x_axis_vector)
         self.dxf.extrusion, _ = transform_extrusion(self.dxf.extrusion, m)
