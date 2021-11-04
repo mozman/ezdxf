d4cd6398c1832fd0c8604190f85ef56e7346a153
mozman <me@mozman.at>
2021-11-04 05:44:54 +0100

    add add_angular_dim_3p() example

--- examples/render/dimension_angular.py
+++ examples/render/dimension_angular.py
@@ -49,51 +49,6 @@ def locations():
         location(Vec3(10, 0), -1),
     ]
 
-
-def angular_2l_default_above(dxfversion="R2000"):
-    doc = ezdxf.new(dxfversion, setup=True)
-    msp = doc.modelspace()
-    for base, line1, line2 in locations():
-        msp.add_line(line1[0], line1[1])
-        msp.add_line(line2[0], line2[1])
-
-        # Default DimStyle EZ_CURVED:
-        # - angle units = degree
-        # - scale 1: 100
-        # - closed filled arrow, size = 0.25
-        # - text location above dimension line
-        #
-        # base:
-        #   location of the dimension line
-        # line1:
-        #   first line defining the angle
-        # line2:
-        #   second line defining the angle
-        dim = msp.add_angular_dim_2l(
-            base=base, line1=line1, line2=line2, dimstyle="EZ_CURVED"
-        )
-        # Necessary second step, to create the BLOCK entity with the DIMENSION
-        # geometry. Ezdxf supports DXF R2000 attributes for DXF R12 rendering,
-        # but they have to be applied by the DIMSTYLE override feature, this
-        # additional attributes are not stored in the XDATA section of the
-        # DIMENSION entity, they are just used to render the DIMENSION entity.
-        # The return value `dim` is not a DIMENSION entity, instead a
-        # DimStyleOverride object is returned, the DIMENSION entity is stored
-        # as dim.dimension, see also ezdxf.override.DimStyleOverride class.
-        dim.render(discard=BRICSCAD)
-    doc.set_modelspace_vport(height=30)
-    doc.saveas(OUTDIR / f"dim_angular_2l_{dxfversion}_default_above.dxf")
-
-
-def add_lines(
-    msp, center: Vec3, radius: float, start_angle: float, end_angle: float
-):
-    start_point = center + Vec3.from_deg_angle(start_angle) * radius
-    end_point = center + Vec3.from_deg_angle(end_angle) * radius
-    msp.add_line(center, start_point)
-    msp.add_line(center, end_point)
-
-
 def angular_cra_default(dxfversion="R2013"):
     doc = ezdxf.new(dxfversion, setup=True)
     msp = doc.modelspace()
@@ -153,6 +108,66 @@ def angular_cra_default(dxfversion="R2013"):
     doc.saveas(OUTDIR / f"dim_angular_cra_{dxfversion}_default.dxf")
 
 
+def angular_3p_default(dxfversion="R2013"):
+    doc = ezdxf.new(dxfversion, setup=True)
+    msp = doc.modelspace()
+    radius = 5
+    distance = 2
+    data = [
+        [Vec3(0, 0), 60, 120],
+        [Vec3(10, 0), 300, 240],
+        [Vec3(20, 0), 240, 300],
+    ]
+    for name, dimtad, offset in [
+        ["above", 1, Vec3(0, 20)],
+        ["center", 0, Vec3(0, 0)],
+        ["below", 4, Vec3(0, -20)],
+    ]:
+        for center, start_angle, end_angle in data:
+            center += offset
+            dir1 = Vec3.from_deg_angle(start_angle)
+            dir2 = Vec3.from_deg_angle(end_angle)
+
+            # calculate defpoints from parameters of the "cra" example:
+            p1 = center + dir1 * radius
+            p2 = center + dir2 * radius
+            base = center + dir1.lerp(dir2) * (radius + distance)
+
+            add_lines(msp, center, radius, start_angle, end_angle)
+            msp.add_angular_dim_3p(
+                base,
+                center,
+                p1,
+                p2,
+                override={"dimtad": dimtad},
+            ).render(discard=BRICSCAD)
+
+    doc.set_modelspace_vport(height=70)
+    doc.saveas(OUTDIR / f"dim_angular_3p_{dxfversion}_default.dxf")
+
+
+def angular_2l_default(dxfversion="R2013"):
+    doc = ezdxf.new(dxfversion, setup=True)
+    msp = doc.modelspace()
+    for base, line1, line2 in locations():
+        msp.add_line(line1[0], line1[1])
+        msp.add_line(line2[0], line2[1])
+        msp.add_angular_dim_2l(
+            base=base, line1=line1, line2=line2, dimstyle="EZ_CURVED"
+        ).render(discard=BRICSCAD)
+    doc.set_modelspace_vport(height=30)
+    doc.saveas(OUTDIR / f"dim_angular_2l_{dxfversion}_default.dxf")
+
+
+def add_lines(
+    msp, center: Vec3, radius: float, start_angle: float, end_angle: float
+):
+    start_point = center + Vec3.from_deg_angle(start_angle) * radius
+    end_point = center + Vec3.from_deg_angle(end_angle) * radius
+    msp.add_line(center, start_point)
+    msp.add_line(center, end_point)
+
+
 def angular_3d(dxfversion="R2000"):
     doc = ezdxf.new(dxfversion, setup=True)
     msp = doc.modelspace()
@@ -174,4 +189,6 @@ def angular_3d(dxfversion="R2000"):
 
 if __name__ == "__main__":
     angular_cra_default()
+    angular_3p_default()
+    angular_2l_default()
     angular_3d()
