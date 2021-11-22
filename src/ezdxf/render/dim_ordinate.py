03514f14573e6a5ef46baaf2afca6422167ba0fb
mozman <me@mozman.at>
2021-11-22 08:05:16 +0100

    setup OrdinateDimension

---
+++ src/ezdxf/render/dim_ordinate.py
@@ -0,0 +1,109 @@
+#  Copyright (c) 2021, Manfred Moitzi
+#  License: MIT License
+
+from typing import TYPE_CHECKING, List
+import logging
+
+from ezdxf.math import Vec2, UCS, NULLVEC
+from ezdxf.lldxf import const
+from ezdxf.entities import DimStyleOverride, Dimension
+from .dim_base import (
+    BaseDimensionRenderer,
+    get_required_defpoint,
+    compile_mtext,
+)
+
+if TYPE_CHECKING:
+    from ezdxf.eztypes import GenericLayoutType
+
+__all__ = ["OrdinateDimension"]
+
+logger = logging.getLogger("ezdxf")
+
+
+class OrdinateDimension(BaseDimensionRenderer):
+    # Required defpoints:
+    # defpoint = origin (group code 10)
+    # defpoint2 = feature location (group code 13)
+    # defpoint3 = end of leader (group code 14)
+    def __init__(
+        self,
+        dimension: Dimension,
+        ucs: "UCS" = None,
+        override: DimStyleOverride = None,
+    ):
+        self.origin: Vec2 = get_required_defpoint(dimension, "defpoint")
+        self.feature_location: Vec2 = get_required_defpoint(
+            dimension, "defpoint2"
+        )
+        self.end_of_leader: Vec2 = get_required_defpoint(dimension, "defpoint3")
+        # x_type = not(y_type)
+        self.y_type = bool(
+            dimension.dxf.get("dimtype", 0) & const.DIM_ORDINATE_TYPE
+        )
+        super().__init__(dimension, ucs, override)
+
+    def update_measurement(self) -> None:
+        distance: Vec2 = self.feature_location - self.origin
+        self.measurement.update(distance.y if self.y_type else distance.x)
+
+    def get_defpoints(self) -> List[Vec2]:
+        return [
+            self.origin,
+            self.feature_location,
+            self.end_of_leader,
+        ]
+
+    def transform_ucs_to_wcs(self) -> None:
+        """Transforms dimension definition points into WCS or if required into
+        OCS.
+        """
+
+        def from_ucs(attr, func):
+            point = dxf.get(attr, NULLVEC)
+            dxf.set(attr, func(point))
+
+        dxf = self.dimension.dxf
+        ucs = self.geometry.ucs
+        from_ucs("defpoint", ucs.to_wcs)
+        from_ucs("defpoint2", ucs.to_wcs)
+        from_ucs("defpoint3", ucs.to_wcs)
+        from_ucs("text_midpoint", ucs.to_ocs)
+
+    def render(self, block: "GenericLayoutType") -> None:
+        """Main method to create dimension geometry of basic DXF entities in the
+        associated BLOCK layout.
+
+        Args:
+            block: target BLOCK for rendering
+
+        """
+        super().render(block)
+        self.add_extension_line()
+        measurement = self.measurement
+        if measurement.text:
+            if self.geometry.supports_dxf_r2000:
+                text = compile_mtext(measurement, self.tol)
+            else:
+                text = measurement.text
+            self.add_measurement_text(
+                text, measurement.text_location, measurement.text_rotation
+            )
+        self.geometry.add_defpoints(self.get_defpoints())
+
+    def add_extension_line(self) -> None:
+        pass
+
+    def add_measurement_text(
+        self, dim_text: str, pos: Vec2, rotation: float
+    ) -> None:
+        """Add measurement text to dimension BLOCK.
+
+        Args:
+            dim_text: dimension text
+            pos: text location
+            rotation: text rotation in degrees
+
+        """
+        attribs = self.measurement.dxfattribs()
+        self.add_text(dim_text, pos=pos, rotation=rotation, dxfattribs=attribs)
