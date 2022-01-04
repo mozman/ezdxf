49d04380144552d830f7aa1bf2fe56c4787f9c07
mozman <me@mozman.at>
2022-01-04 13:26:44 +0100

    add more tests for intersect_polylines_2d()

--- tests/test_06_math/test_653_polyline_intersection.py
+++ tests/test_06_math/test_653_polyline_intersection.py
@@ -10,17 +10,18 @@ from ezdxf.math import (
     ConstructionEllipse,
     BSpline,
 )
+from ezdxf.render import forms
 
 
 class TestIntersectPolylines2d:
-    def test_intersecting_one_liners(self):
+    def test_intersecting_single_segments(self):
         pline1 = Vec2.list([(0, 1), (2, 1)])
         pline2 = Vec2.list([(1, 0), (1, 2)])
         res = intersect_polylines_2d(pline1, pline2)
         assert len(res) == 1
         assert res[0].isclose(Vec2(1, 1))
 
-    def test_none_intersecting_one_liners(self):
+    def test_none_intersecting_single_segments(self):
         pline1 = Vec2.list([(0, 0), (2, 0)])
         pline2 = Vec2.list([(0, 1), (2, 1)])
         res = intersect_polylines_2d(pline1, pline2)
@@ -40,7 +41,7 @@ class TestIntersectPolylines2d:
         assert len(res) == 1
         assert res[0].isclose(Vec2(1, 1))
 
-    def test_intersecting_zig_zag(self):
+    def test_intersecting_zig_zag_lines(self):
         pline1 = Vec2.list([(0, 0), (2, 2), (4, 0), (6, 2), (8, 0)])
         pline2 = Vec2.list([(0, 2), (2, 0), (4, 2), (6, 0), (8, 2)])
         res = intersect_polylines_2d(pline1, pline2)
@@ -51,7 +52,7 @@ class TestIntersectPolylines2d:
         assert res[2].isclose(Vec2(5, 1))
         assert res[3].isclose(Vec2(7, 1))
 
-    def test_touching_zig_zag(self):
+    def test_zig_zag_lines_with_common_vertices(self):
         pline1 = Vec2.list([(0, 0), (2, 2), (4, 0), (6, 2), (8, 0)])
         pline2 = Vec2.list([(0, 4), (2, 2), (4, 4), (6, 2), (8, 4)])
         res = intersect_polylines_2d(pline1, pline2)
@@ -60,13 +61,60 @@ class TestIntersectPolylines2d:
         assert res[0].isclose(Vec2(2, 2))
         assert res[1].isclose(Vec2(6, 2))
 
-    def test_complex_ellipse_spline_intersection(self):
+    def test_complex_ellipse_with_spline_intersection(self):
         ellipse = ConstructionEllipse(center=(0, 0), major_axis=(3, 0), ratio=0.5)
         bspline = BSpline([(-4, -4), (-2, -1), (2, 1), (4, 4)])
         p1 = ellipse.flattening(distance=0.01)
         p2 = bspline.flattening(distance=0.01)
         res = intersect_polylines_2d(Vec2.list(p1), Vec2.list(p2))
         assert len(res) == 2
+
+    def test_intersecting_squares(self):
+        square1 = forms.close_polygon(forms.square(2.0))
+        square2 = forms.translate(square1, (1, 1))
+        res = intersect_polylines_2d(Vec2.list(square1), Vec2.list(square2))
+        assert len(res) == 2
+        res.sort()
+        assert res[0].isclose(Vec2(1, 2))
+        assert res[1].isclose(Vec2(2, 1))
+
+    def test_squares_with_common_corner_vertex(self):
+        square1 = forms.close_polygon(forms.square(2.0))
+        square2 = forms.translate(square1, (2, 2))
+        res = intersect_polylines_2d(Vec2.list(square1), Vec2.list(square2))
+        assert len(res) == 1
+        assert res[0].isclose(Vec2(2, 2))
+
+    def test_coincident_common_segment(self):
+        """ The common segment does not create intersection points.
+        Same as intersection of coincident lines!
+        """
+        pline1 = Vec2.list([(1, 1), (2, 1)])
+        pline2 = Vec2.list([(1, 1), (2, 1)])
+        res = intersect_polylines_2d(pline1, pline2)
+        assert len(res) == 0
+
+    def test_coincident_common_last_segment(self):
+        """ The common segment does not create intersection points, but the
+        preceding segment does.
+        """
+        pline1 = Vec2.list([(0, 0), (1, 1), (2, 1)])
+        pline2 = Vec2.list([(0, 2), (1, 1), (2, 1)])
+        res = intersect_polylines_2d(pline1, pline2)
+        assert len(res) == 1
+        assert res[0].isclose(Vec2(1, 1))
+
+    def test_coincident_common_intermediate_segment(self):
+        """ The common segment does not create intersection points, but the
+        preceding and the following segment does.
+        """
+        pline1 = Vec2.list([(0, 0), (1, 1), (2, 1), (3, 0)])
+        pline2 = Vec2.list([(0, 2), (1, 1), (2, 1), (3, 2)])
+        res = intersect_polylines_2d(pline1, pline2)
+        assert len(res) == 2
+        res.sort()
+        assert res[0].isclose(Vec2(1, 1))
+        assert res[1].isclose(Vec2(2, 1))
 
 
 class TestIntersectPolylines3d:
@@ -78,14 +126,14 @@ class TestIntersectPolylines3d:
     This class tests only if the function intersect_polylines_3d() is implemented.
 
     """
-    def test_intersecting_one_liners(self):
+    def test_intersecting_single_segments(self):
         pline1 = Vec3.list([(0, 1), (2, 1)])
         pline2 = Vec3.list([(1, 0), (1, 2)])
         res = intersect_polylines_3d(pline1, pline2)
         assert len(res) == 1
         assert res[0].isclose(Vec3(1, 1))
 
-    def test_none_intersecting_one_liners(self):
+    def test_none_intersecting_single_segments(self):
         pline1 = Vec3.list([(0, 0), (2, 0)])
         pline2 = Vec3.list([(0, 1), (2, 1)])
         res = intersect_polylines_3d(pline1, pline2)
