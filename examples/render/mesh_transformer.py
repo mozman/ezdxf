ffc5c0377aad33b0f488361bdca404ee297c1db8
mozman <me@mozman.at>
2022-01-19 08:12:35 +0100

    refactor example script to use new idioms

--- examples/render/mesh_transformer.py
+++ examples/render/mesh_transformer.py
@@ -1,28 +1,34 @@
-# Copyright (c) 2020, Manfred Moitzi
+# Copyright (c) 2020-2022, Manfred Moitzi
 # License: MIT License
 from pathlib import Path
 import ezdxf
-from ezdxf.render.forms import cube, cylinder
+from ezdxf import colors
+from ezdxf.gfxattribs import GfxAttribs
+from ezdxf.render import forms
 
 DIR = Path("~/Desktop/Outbox").expanduser()
 
-mycube = cube().scale_uniform(10).subdivide(2)
-mycylinder = cylinder(12, radius=5, top_center=(0, 0, 10)).translate(0, 20)
+cube = forms.cube().scale_uniform(10).subdivide(2)
+cylinder = forms.cylinder(12, radius=5, top_center=(0, 0, 10)).translate(0, 20)
 
 doc = ezdxf.new()
 msp = doc.modelspace()
 
-mycube.render(msp, dxfattribs={"color": 1})
-mycube.translate(20)
-mycube.render_polyface(msp, dxfattribs={"color": 3})
-mycube.translate(20)
-mycube.render_3dfaces(msp, dxfattribs={"color": 5})
+red = GfxAttribs(color=colors.RED)
+green = GfxAttribs(color=colors.GREEN)
+blue = GfxAttribs(color=colors.BLUE)
 
-mycylinder.render(msp, dxfattribs={"color": 1})
-mycylinder.translate(20)
-mycylinder.render_polyface(msp, dxfattribs={"color": 3})
-mycylinder.translate(20)
-mycylinder.render_3dfaces(msp, dxfattribs={"color": 5})
+cube.render(msp, dxfattribs=red)
+cube.translate(20)
+cube.render_polyface(msp, dxfattribs=green)
+cube.translate(20)
+cube.render_3dfaces(msp, dxfattribs=blue)
+
+cylinder.render(msp, dxfattribs=red)
+cylinder.translate(20)
+cylinder.render_polyface(msp, dxfattribs=green)
+cylinder.translate(20)
+cylinder.render_3dfaces(msp, dxfattribs=blue)
 
 doc.set_modelspace_vport(30, center=(30, 20))
 doc.saveas(DIR / "meshes.dxf")
