2b638c63cc5d8b2ddd92b4f0b8337fc9576a93a3
mozman <me@mozman.at>
2022-02-09 17:37:04 +0100

    update example

--- examples/viewports_in_paperspace.py
+++ examples/viewports_in_paperspace.py
@@ -1,11 +1,15 @@
-# Copyright (c) 2015-2021, Manfred Moitzi
+# Copyright (c) 2015-2022, Manfred Moitzi
 # License: MIT License
 import math
 from pathlib import Path
 import ezdxf
+from ezdxf.layouts import Paperspace
+from ezdxf.enums import TextEntityAlignment
 
 MESH_SIZE = 20
 DIR = Path("~/Desktop/Outbox").expanduser()
+if not DIR.exists():
+    DIR = Path(".")
 
 
 def build_cos_sin_mesh(mesh):
@@ -44,11 +48,15 @@ def create_3d_modelspace_content(modelspace):
     build_cos_sin_mesh(mesh)
 
 
-def create_viewports(paperspace, dxfversion):
+def create_viewports(paperspace: Paperspace, dxfversion):
     # Define viewports in paper space:
     # center, size=(width, height) defines the viewport in paper space.
     # view_center_point and view_height defines the area in model space
     # which is displayed in the viewport.
+    txt_attribs = dict(
+        style="OpenSans-Bold",
+        color=5,
+    )
     paperspace.add_viewport(
         center=(2.5, 2.5),
         size=(5, 5),
@@ -57,13 +65,8 @@ def create_viewports(paperspace, dxfversion):
     )
     # scale is calculated by: height of model space (view_height=10) / height of viewport (height=5)
     paperspace.add_text(
-        "View of Rectangle Scale=1:2",
-        dxfattribs={
-            "insert": (0, 5.2),
-            "height": 0.18,
-            "color": 1,
-        },
-    )
+        "View of Rectangle Scale=1:2", height=0.18, dxfattribs=txt_attribs
+    ).set_placement((0, 5.2))
 
     paperspace.add_viewport(
         center=(8.5, 2.5),
@@ -72,13 +75,8 @@ def create_viewports(paperspace, dxfversion):
         view_height=25,
     )
     paperspace.add_text(
-        "View of Circle Scale=1:5",
-        dxfattribs={
-            "insert": (6, 5.2),
-            "height": 0.18,
-            "color": 1,
-        },
-    )
+        "View of Circle Scale=1:5", height=0.18, dxfattribs=txt_attribs
+    ).set_placement((6, 5.2))
 
     paperspace.add_viewport(
         center=(14.5, 2.5),
@@ -87,13 +85,8 @@ def create_viewports(paperspace, dxfversion):
         view_height=5,
     )
     paperspace.add_text(
-        "View of Triangle Scale=1:1",
-        dxfattribs={
-            "insert": (12, 5.2),
-            "height": 0.18,
-            "color": 1,
-        },
-    )
+        "View of Triangle Scale=1:1", height=0.18, dxfattribs=txt_attribs
+    ).set_placement((12, 5.2))
 
     paperspace.add_viewport(
         center=(7.5, 10),
@@ -102,9 +95,8 @@ def create_viewports(paperspace, dxfversion):
         view_height=7.5,
     )
     paperspace.add_text(
-        "Overall View Scale=1:1",
-        dxfattribs={"insert": (0, 14), "height": 0.18, "color": 1},
-    )
+        "Overall View Scale=1:1", height=0.18, dxfattribs=txt_attribs
+    ).set_placement((0, 14))
 
     paperspace.add_viewport(
         center=(16, 13.5),
@@ -114,12 +106,8 @@ def create_viewports(paperspace, dxfversion):
     )
     # scale = 7.5/0.15 = 50
     paperspace.add_text(
-        "Scale=1:50",
-        dxfattribs={
-            "height": 0.18,
-            "color": 1,
-        },
-    ).set_pos((16, 14), align="CENTER")
+        "Scale=1:50", height=0.18, dxfattribs=txt_attribs
+    ).set_placement((16, 14), align=TextEntityAlignment.CENTER)
 
     vp = paperspace.add_viewport(
         center=(16, 10), size=(4, 4), view_center_point=(0, 0), view_height=30
@@ -128,13 +116,13 @@ def create_viewports(paperspace, dxfversion):
     vp.dxf.view_direction_vector = (-1, -1, 1)
 
     paperspace.add_text(
-        "Viewport to 3D Mesh", dxfattribs={"height": 0.18, "color": 1}
-    ).set_pos((16, 12.5), align="CENTER")
+        "Viewport to 3D Mesh", height=0.18, dxfattribs=txt_attribs
+    ).set_placement((16, 10), align=TextEntityAlignment.CENTER)
 
 
 def main():
     def make(dxfversion, filename):
-        doc = ezdxf.new(dxfversion)
+        doc = ezdxf.new(dxfversion, setup=True)
         if "VIEWPORTS" not in doc.layers:
             vp_layer = doc.layers.add("VIEWPORTS")
         else:
@@ -145,7 +133,7 @@ def main():
         create_2d_modelspace_content(doc.modelspace())
         create_3d_modelspace_content(doc.modelspace())
         # IMPORTANT: DXF R12 supports only one paper space aka layout, every layout name returns the same layout
-        layout = doc.layout("Layout1")  # default layout
+        layout: Paperspace = doc.layout("Layout1")  # type: ignore
         layout.page_setup(size=(22, 17), margins=(1, 1, 1, 1), units="inch")
         create_viewports(layout, dxfversion)
 
