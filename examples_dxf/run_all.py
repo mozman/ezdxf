758113d9ffeff460c5ff32531e5d152eadc068c1
mozman <me@mozman.at>
2022-02-20 08:12:06 +0100

    make run_all.py posix compatible

--- examples_dxf/run_all.py
+++ examples_dxf/run_all.py
@@ -2,8 +2,14 @@
 #  License: MIT License
 from pathlib import Path
 import subprocess
+import shlex
+import shutil
+import sys
 
 PYTHON3 = "python3"
+POSIX = sys.platform != "win32"
+if POSIX:
+    PYTHON3 = shutil.which(PYTHON3)
 
 
 def main():
@@ -11,7 +17,8 @@ def main():
     for script in filepath.parent.glob("create_*.py"):
         cmd = f"{PYTHON3} {script}"
         print(f'executing: "{cmd}"')
-        subprocess.run(cmd)
+        args = shlex.split(cmd, posix=POSIX)
+        subprocess.run(args)
 
 
 if __name__ == "__main__":
