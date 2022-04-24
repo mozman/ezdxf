35704b1539a99b2ad8ec04c5ac484fa5fcb38229
mozman <me@mozman.at>
2022-04-24 13:48:07 +0200

    extend sat-analyzer.py

--- exploration/sat-analyzer.py
+++ exploration/sat-analyzer.py
@@ -1,7 +1,9 @@
 #  Copyright (c) 2022, Manfred Moitzi
 #  License: MIT License
 from typing import List
-DATA = "prism.sat"
+CUBE = "cube777.sat"
+PRISM = "prism.sat"
+DATA = PRISM
 
 
 def parse_record(record: str) -> List[str]:
@@ -29,50 +31,69 @@ reversed/forward/forward_v ... Parameter defining the sense direction.
 ... @7 unknown
 @=text 7=chars: "unknown"
 
+SAT/SAB seem to support only triangles for polygon face meshes.
+
 The content is version dependent!
 
 ===============================================================================
 
-0= body ~0 id() ~1 <lump-1> ~2 ~3
+0= body ~0 ID ~1 <lump-1> ~2 ~3
 >>> ~0 ENTITY attribute 0-ptr 
 >>> ~1 another attribute 0-ptr ???
 >>> next-ptr <lump-1> BODY data ptr
 >>> ~2 BODY wire 0-ptr
 >>> ~3 BODY transform 0-ptr
 
-1= lump ~ id() ~ ~ <shell-2> <body-0>
+1= lump ~ ID ~ ~ <shell-2> <body-0>
 >>> next-ptr=<shell-2>, parent-ptr=<body-0>
 
-2= shell ~ id() ~ ~ ~ <face-3> ~ <lump-1>
+2= shell ~ ID ~ ~ ~ <face-3> ~ <lump-1>
 >>> next-ptr=<face-3>, parent-ptr=<lump-1>
 
-3= face ~ id() ~ <face-4> <loop-5> <shell-2> ~ <plane-surface-6> forward single
+3= face ~ ID ~ <face-4> <loop-5> <shell-2> ~ <plane-surface-6> forward single
 >>> next-ptr=<face-4>, parent-ptr=<shell-2>
 >>> <loop-5>
 >>> <plane-surface-6>
 
-5= loop ~ id() ~ ~ <coedge-10> <face-3>
+5= loop ~ ID ~ ~ <coedge-10> <face-3>
 >>> next-ptr=<coedge-10>, parent-ptr <face-3>
+>>> loop-5 begins at coedge-10
 
-10= coedge ~ id() ~ <coedge-15> <coedge-16> <coedge-17> <edge-18> reversed <loop-5> ~
->>> 3x coedge, 1x edge-18, parent-ptr=<loop-5>
+10= coedge ~ ID ~ <coedge-15> <coedge-16> <coedge-17> <edge-18> reversed <loop-5> ~
+>>> triangle: <coedge-10> -> <coedge-15> -> <coedge-16> => <coedge-10>
+>>> <coedge-17> is the same edge in <loop-20>
+>>> <edge-18> defines the vertices, same as in <coedge-17>
+>>> reversed: edge orientation reversed cw/ccw?
+>>> parent-ptr=<loop-5>
 
-18= edge ~ id() ~ <vertex-32> <v0> <vertex-33> <v1> <coedge-17> <straight-curve-34> forward @7 unknown
->>> vertex-32 -> vertex-33, <coedge-17>?, <straight-curve-34>
+15= coedge ~ ID ~ <coedge-16> <coedge-10> <coedge-27> <edge-28> forward <loop-5> ~
+16= coedge ~ ID ~ <coedge-10> <coedge-15> <coedge-29> <edge-30> reversed <loop-5> ~
 
-32= vertex ~ id() ~ <edge-26> <point-55>
->>> <edge-26>?, <point-55>
+17= coedge ~ ID ~ <coedge-31> <coedge-25> <coedge-10> <edge-18> forward <loop-20> ~
+>>> <coedge-10> is the same edge in <loop-5>
+>>> triangle: <coedge-17> -> <coedge-31> -> <coedge-25> => <coedge-17>
+>>> <edge-18> defines the vertices, same as in <coedge-10>
+>>> forward: edge orientation forward, must be contraire to <coedge-10> to have
+    consistent face normals
+>>> parent-ptr=<loop-20>
 
-55= point ~ id() ~ <v0> <v1> <v2>
+18= edge ~ ID ~ <vertex-32> [0] <vertex-33> [1] <coedge-17> <straight-curve-34> forward @7 unknown
+>>> vertex-32 -> vertex-33
+>>> is also used by <coedge-17> 
+>>> <straight-curve-34> defines the kind of edge
+
+32= vertex ~ ID ~ <edge-26> <point-55>
+>>> <edge-26> also uses <vertex-32>
+>>> <point-55> finally some coordinates
+
+55= point ~ id() ~ [0] [1] [2]
 >>> coordinates
 
-34= straight-curve ~ id() ~ <v0> <v1> <v2> <v3> <v4> <v5> I I
->>> Unbounded ray (v0, v1, v2) -> (v3, v4, v5)
+34= straight-curve ~ ID ~ [0] [1] [2] [3] [4] [5] I I
+>>> Unbounded ray ([0], [1], [2]) -> ([3], [4], [5])
 
-6= plane-surface ~ id() ~ <v0> <v1> <v2> <v3> <v4> <v5> <v6> <v7> <v8> forward_v I I I I
->>> unbounded plane by 3 points (v0, v1, v2) -> (v3, v4, v5) -> (v6, v7, v8)
-
-   
+6= plane-surface ~ ID ~ [0] [1] [2] [3] [4] [5] [6] [7] [8] forward_v I I I I
+>>> unbounded plane by 3 points ([0], [1], [2]) -> ([3], [4], [5]) -> ([6], [7], [8])
 """
 
 
