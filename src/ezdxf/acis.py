f97723b208cbfc383bc2b6a178b774bf054f9273
mozman <me@mozman.at>
2022-04-25 10:24:17 +0200

    AcisHeader returns proper string repr

--- src/ezdxf/acis.py
+++ src/ezdxf/acis.py
@@ -1,13 +1,58 @@
 #  Copyright (c) 2022, Manfred Moitzi
 #  License: MIT License
 from typing import List, Tuple, Union, Sequence
-
+from datetime import datetime
+from dataclasses import dataclass, field
 
 __all__ = ["parse_sat", "new_tree"]
 
+# ACIS versions exported by BricsCAD:
+# R2000/AC1015: 400, "ACIS 4.00 NT", text length has no prefix "@"
+# R2004/AC1018: 20800 @ "ACIS 208.00 NT", text length has "@" prefix
+# R2007/AC1021: 20800 @ "ACIS 208.00 NT", text length has "@" prefix
+# R2010/AC1024: 20800 @ "ACIS 208.00 NT", text length has "@" prefix
 
+ACIS_VERSION = {
+    400: "ACIS 4.00 NT",
+    20800: "ACIS 208.00 NT",
+}
+
+
+@dataclass
 class AcisHeader:
-    pass
+    version: int = 400
+    n_records: int = 0  # can be 0
+    n_entities: int = 0
+    history_flag: int = 0  # 1 if history has been saved
+    product_id: str = "ezdxf ACIS Builder"
+    acis_version: str = ACIS_VERSION[400]
+    creation_date: datetime = field(default_factory=datetime.now)
+    units_in_mm: float = 1.0
+
+    def dumps(self) -> str:
+        return "\n".join(
+            [
+                f"{self.version} {self.n_records} {self.n_entities} {self.history_flag} ",
+                self._header_str(),
+                f"{int(self.units_in_mm)} 9.9999999999999995e-007 1e-010",
+            ]
+        )
+
+    def _header_str(self) -> str:
+        p_len = len(self.product_id)
+        a_len = len(self.acis_version)
+        date = self.creation_date.ctime()
+        if self.version > 400:
+            return f"@{p_len} {self.product_id} @{a_len} {self.acis_version} @{len(date)} {date} "
+        else:
+            return f"{p_len} {self.product_id} {a_len} {self.acis_version} {len(date)} {date} "
+
+    def set_version(self, version: int) -> None:
+        try:
+            self.acis_version = ACIS_VERSION[version]
+            self.version = version
+        except KeyError:
+            raise ValueError(f"invalid ACIS version number {version}")
 
 
 class AcisNode:
--- tests/test_05_tools/test_529_acis.py
+++ tests/test_05_tools/test_529_acis.py
@@ -2,12 +2,46 @@
 #  License: MIT License
 
 import pytest
+from datetime import datetime
 from ezdxf import acis
 
 
 def test_new_acis_tree():
     atree = acis.new_tree()
     assert atree.header is not None
+
+
+def test_default_header():
+    header = acis.AcisHeader()
+    assert header.version == 400
+    assert header.acis_version == "ACIS 4.00 NT"
+    assert header.n_entities == 0
+    assert header.n_records == 0
+    assert isinstance(header.creation_date, datetime)
+
+
+HEADER_400 = """400 0 1 0 
+18 ezdxf ACIS Builder 12 ACIS 4.00 NT 24 Sat Jan  1 10:00:00 2022 
+1 9.9999999999999995e-007 1e-010"""
+
+HEADER_20800 = """20800 0 1 0 
+@18 ezdxf ACIS Builder @14 ACIS 208.00 NT @24 Sat Jan  1 10:00:00 2022 
+1 9.9999999999999995e-007 1e-010"""
+
+
+def test_dump_header_string_400():
+    header = acis.AcisHeader()
+    header.creation_date = datetime(2022, 1, 1, 10, 00)
+    header.n_entities = 1
+    assert header.dumps() == HEADER_400
+
+
+def test_dump_header_string_20800():
+    header = acis.AcisHeader()
+    header.set_version(20800)
+    header.creation_date = datetime(2022, 1, 1, 10, 00)
+    header.n_entities = 1
+    assert header.dumps() == HEADER_20800
 
 
 if __name__ == "__main__":
