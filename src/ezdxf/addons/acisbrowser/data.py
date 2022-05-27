20594502d8a678bac2596764f4501f3a59210d0e
mozman <me@mozman.at>
2022-05-27 09:01:22 +0200

    add raw SAB viewer

--- src/ezdxf/addons/acisbrowser/data.py
+++ src/ezdxf/addons/acisbrowser/data.py
@@ -2,7 +2,6 @@
 #  License: MIT License
 from typing import List, Iterator
 
-from ezdxf.acis.sat import parse_sat
 from ezdxf.acis.sab import parse_sab
 
 
@@ -16,9 +15,21 @@ class AcisData:
 class BinaryAcisData(AcisData):
     def __init__(self, data: bytes, name: str, handle: str):
         super().__init__(name, handle)
+        self.lines = list(make_sab_records(data))
 
 
 class TextAcisData(AcisData):
     def __init__(self, data: List[str], name: str, handle: str):
         super().__init__(name, handle)
         self.lines = list(data)
+
+
+def make_sab_records(data: bytes) -> Iterator[str]:
+    builder = parse_sab(data)
+    builder.reset_ids()
+    for entity in builder.entities:
+        content = [str(entity)]
+        content.append(str(entity.attributes))
+        for tag in entity.data:
+            content.append(str(tag.value))
+        yield " ".join(content)
