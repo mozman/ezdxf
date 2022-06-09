b661af792e693d2bda856e7e83a4c49cb19d5cc3
mozman <me@mozman.at>
2022-06-09 05:55:38 +0200

    add Script.add_multmatrix()

--- src/ezdxf/addons/openscad.py
+++ src/ezdxf/addons/openscad.py
@@ -9,6 +9,7 @@ import subprocess
 from uuid import uuid4
 import tempfile
 
+from ezdxf.math import Matrix44
 from ezdxf.render import MeshBuilder, MeshTransformer
 from ezdxf.addons import meshex
 
@@ -85,6 +86,12 @@ def run(script: str, exec_path: str = None) -> MeshTransformer:
     return new_mesh
 
 
+def str_matrix44(m: Matrix44) -> str:
+    # OpenSCAD uses column major order!
+    s = ", ".join([str(list(c)) for c in m.columns()])
+    return f"[{s}]"
+
+
 class Script:
     def __init__(self):
         self.data: List[str] = []
@@ -96,6 +103,11 @@ class Script:
     def add_polyhedron(self, mesh: MeshBuilder) -> None:
         """Add `mesh` as polyhedron() entity."""
         self.add(meshex.scad_dumps(mesh))
+
+    def add_multmatrix(self, m: Matrix44):
+        """Add a transformation matrix of type :class:`~ezdxf.math.Matrix44` as
+        ``multmatrix()`` operation."""
+        self.add(f"multmatrix(m = {str_matrix44(m)})")  # no pending ";"
 
     def get_string(self) -> str:
         """Returns the OpenSCAD build script. """
--- tests/test_08_addons/test_820_openscad.py
+++ tests/test_08_addons/test_820_openscad.py
@@ -4,6 +4,17 @@
 import pytest
 from ezdxf.addons import openscad
 from ezdxf.render import MeshBuilder
+from ezdxf.math import Matrix44
+
+
+def test_matrix44_to_multmatrix_str():
+    s = openscad.str_matrix44(Matrix44.translate(20, 30, 40))
+    assert (
+        s == "[[1.0, 0.0, 0.0, 20.0],"
+        " [0.0, 1.0, 0.0, 30.0],"
+        " [0.0, 0.0, 1.0, 40.0],"
+        " [0.0, 0.0, 0.0, 1.0]]"
+    )
 
 
 class TestScriptBuilder:
@@ -23,14 +34,24 @@ class TestScriptBuilder:
             "}"
         )
 
-    @pytest.mark.parametrize("op,func", [
-        (openscad.UNION, "union()"),
-        (openscad.DIFFERENCE, "difference()"),
-        (openscad.INTERSECTION, "intersection()"),
-    ])
+    @pytest.mark.parametrize(
+        "op,func",
+        [
+            (openscad.UNION, "union()"),
+            (openscad.DIFFERENCE, "difference()"),
+            (openscad.INTERSECTION, "intersection()"),
+        ],
+    )
     def test_build_all_operations(self, op, func):
         m = MeshBuilder()
         assert openscad.boolean_operation(op, m, m).startswith(func)
+
+    def test_adding_multmatrix_operation(self):
+        script = openscad.Script()
+        script.add_multmatrix(Matrix44.translate(20, 30, 40))
+        result = script.get_string()
+        assert result.startswith("multmatrix(m = [[")
+        assert result.endswith("]])"), "operation without a pending ';'"
 
 
 if __name__ == "__main__":
