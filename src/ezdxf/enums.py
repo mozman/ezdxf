7ec3616e44af7257e429228728ea2b7ddc3ca6e1
mozman <me@mozman.at>
2022-07-02 09:38:40 +0200

    add PyCharm inspection hint

--- src/ezdxf/enums.py
+++ src/ezdxf/enums.py
@@ -24,6 +24,7 @@ class TextVAlign(IntEnum):
     TOP = const.TOP
 
 
+# noinspection PyArgumentList
 class TextEntityAlignment(Enum):
     """Text alignment enum for the :class:`~ezdxf.entities.Text`,
     :class:`~ezdxf.entities.Attrib` and :class:`~ezdxf.entities.AttDef`
