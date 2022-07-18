3cec9a85d9c97727faf848449d9eef47acff1871
mozman <me@mozman.at>
2022-07-18 19:41:55 +0200

    add PillowDelayedDraw() backend
    This backend does not need to know the layout
    extents beforehand. It collects all drawing
    commands while calculating the layout extends
    and executes the drawing commands at export.

--- src/ezdxf/addons/drawing/pillow.py
+++ src/ezdxf/addons/drawing/pillow.py
@@ -3,9 +3,10 @@
 from __future__ import annotations
 
 import sys
-from typing import Iterable, Tuple
+import enum
+from typing import Iterable, Tuple, List, Any
 import math
-from ezdxf.math import Vec3, Vec2, Matrix44, AbstractBoundingBox
+from ezdxf.math import Vec3, Vec2, Matrix44, AbstractBoundingBox, BoundingBox
 from ezdxf.addons.drawing.backend import Backend
 from ezdxf.addons.drawing.properties import Properties
 from ezdxf.addons.drawing.type_hints import Color
@@ -207,3 +208,171 @@ SUPPORT_TRANSPARENCY = [".png", ".tif", ".tiff"]
 def supports_transparency(filename: str) -> bool:
     filename = filename.lower()
     return any(filename.endswith(ftype) for ftype in SUPPORT_TRANSPARENCY)
+
+
+# noinspection PyArgumentList
+class Commands(enum.Enum):
+    POINT = enum.auto()
+    LINE = enum.auto()
+    POLYGON = enum.auto()
+
+
+class PillowDelayedDraw(Backend):
+    def __init__(
+        self,
+        image_size: Tuple[int, int] = (0, 0),
+        resolution: float = 1.0,
+        margin: int = 10,
+        dpi: int = 300,
+        oversampling: int = 1,
+        text_placeholder=True,
+    ):
+        super().__init__()
+        self.image_size = Vec2(image_size)
+        self.margin_x = float(margin)
+        self.margin_y = float(margin)
+        self.dpi = int(dpi)
+        self.oversampling = max(int(oversampling), 1)
+        self.text_placeholder = text_placeholder
+        # The lineweight is stored im mm,
+        # line_pixel_factor * lineweight is the width in pixels
+        self.line_pixel_factor = self.dpi / INCH_TO_MM  # pixel per mm
+        # resolution: pixels per DXF drawing units, same resolution in all
+        # directions
+        self.resolution = float(resolution)
+        self.bg_color: Color = "#000000"
+        self.commands: List[Tuple[Any, ...]] = list()
+        self.extents = BoundingBox()
+
+    def clear(self) -> None:
+        self.commands = list()
+        self.extents = BoundingBox()
+
+    def configure(self, config: Configuration) -> None:
+        super().configure(config)
+        self.line_pixel_factor *= self.config.lineweight_scaling
+
+    def set_background(self, color: Color) -> None:
+        self.bg_color = color
+
+    def width(self, lineweight: float) -> int:
+        return max(int(lineweight * self.line_pixel_factor), 1)
+
+    def draw_point(self, pos: Vec3, properties: Properties) -> None:
+        self.extents.extend((pos, ))
+        self.commands.append((Commands.POINT, pos, properties.color))
+
+    def draw_line(self, start: Vec3, end: Vec3, properties: Properties) -> None:
+        self.extents.extend((start, end))
+        self.commands.append(
+            (
+                Commands.LINE,
+                start,
+                end,
+                properties.color,
+                self.width(properties.lineweight)
+            )
+        )
+
+    def draw_filled_polygon(
+        self, points: Iterable[Vec3], properties: Properties
+    ) -> None:
+        vertices = tuple(points)
+        if len(vertices) > 2:
+            self.extents.extend(points)
+            self.commands.append((Commands.POLYGON, points, properties.color))
+
+    def draw_text(
+        self,
+        text: str,
+        transform: Matrix44,
+        properties: Properties,
+        cap_height: float,
+    ) -> None:
+        if not self.text_placeholder:
+            return
+        # draws a placeholder rectangle as text
+        width = self.get_text_line_width(text, cap_height, properties.font)
+        height = cap_height
+        points = Vec3.generate([(0, 0), (width, 0), (width, height), (0, height)])
+        points = transform.transform_vertices(points)
+        self.draw_filled_polygon(points, properties)
+
+    def get_font_measurements(
+        self, cap_height: float, font: FontFace = None
+    ) -> FontMeasurements:
+        return MonospaceFont(cap_height).measurements
+
+    def get_text_line_width(
+        self, text: str, cap_height: float, font: FontFace = None
+    ) -> float:
+        return MonospaceFont(cap_height).text_width(text) * 0.8
+
+    def execute(self) -> Image:
+        self._init_canvas_parameters()
+        image = self._make_image()
+        draw = ImageDraw.Draw(image)
+        for data in self.commands:
+            cmd = data[0]
+            if cmd == Commands.POINT:
+                _, pos, color = cmd
+                draw.point([self.pixel_loc(pos)], fill=color)
+            elif cmd == Commands.LINE:
+                _, start, end, color, width = cmd
+                draw.line(
+                    [self.pixel_loc(start), self.pixel_loc(end)],
+                    fill=color,
+                    width=width,
+                )
+            else:
+                _, points, color = data
+                draw.polygon(
+                    [self.pixel_loc(p) for p in points],
+                    fill=color,
+                    outline=color,
+                )
+        return image
+
+    def pixel_loc(self, point: Vec3) -> Tuple[float, float]:
+        ex, ey = self.extents.extmin  # type: ignore
+        x = (point.x - ex) * self.resolution + self.margin_x
+        y = (point.y - ey) * self.resolution + self.margin_y
+        return (
+            x * self.oversampling,
+            (self.image_size.y - y) * self.oversampling,
+        )
+
+    def _make_image(self):
+        x = int(self.image_size.x) * self.oversampling
+        y = int(self.image_size.y) * self.oversampling
+        return Image.new("RGBA", (x, y), color=self.bg_color)
+
+    def _init_canvas_parameters(self):
+        canvas_size = self.extents.size
+        if self.image_size == Vec2(0, 0):
+            self.image_size = Vec2(
+                math.ceil(canvas_size.x * self.resolution + 2.0 * self.margin_x),
+                math.ceil(canvas_size.y * self.resolution + 2.0 * self.margin_y),
+            )
+        else:
+            img_x, img_y = self.image_size
+            if img_y < 1:
+                raise ValueError(f"invalid image size: {self.image_size}")
+            img_ratio = img_x / img_y
+            region_ratio = canvas_size.x / canvas_size.y
+            if img_ratio >= region_ratio:  # image fills the height
+                self.resolution = (img_y - 2.0 * self.margin_y) / canvas_size.y
+                self.margin_x = (img_x - self.resolution * canvas_size.x) * 0.5
+            else:  # image fills the width
+                self.resolution = (img_x - 2.0 * self.margin_x) / canvas_size.x
+                self.margin_y = (img_y - self.resolution * canvas_size.y) * 0.5
+
+    def export(self, filename: str, **kwargs) -> None:
+        if not self.extents.has_data:
+            return  # empty drawing
+        image = self.execute()
+        if not supports_transparency(filename):
+            # remove alpha channel if not supported
+            image = image.convert("RGB")
+        dpi = kwargs.pop("dpi", self.dpi)
+        image.save(filename, dpi=(dpi, dpi), **kwargs)
