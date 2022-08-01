ef060ae517f181fbc49369a2518b995a94e75b15
mozman <me@mozman.at>
2022-08-01 13:54:23 +0200

    refactor hatching exploration script

--- exploration/hatching.py
+++ exploration/hatching.py
@@ -13,6 +13,7 @@ if not CWD.exists():
 
 def polygon_hatching(filename: str):
     doc = ezdxf.new()
+    setup(doc)
     msp = doc.modelspace()
     polygon = Vec2.list(
         forms.gear(16, top_width=1, bottom_width=3, height=2, outside_radius=10)
@@ -20,18 +21,14 @@ def polygon_hatching(filename: str):
     hole = list(forms.circle(16, radius=4))
     hole1 = Vec2.list(forms.translate(hole, (-2, -2)))
     hole2 = Vec2.list(forms.translate(hole, (2, 2)))
-    msp.add_lwpolyline(polygon, close=True)
-    msp.add_lwpolyline(hole1, close=True)
-    msp.add_lwpolyline(hole2, close=True)
     baseline = hatching.HatchBaseLine(
         Vec2(), direction=Vec2(1, 1), offset=Vec2(-1, 1)
     )
-    for line in hatching.hatch_polygons(baseline, [polygon, hole1, hole2]):
-        msp.add_line(line.start, line.end)
+    render_hatch(msp, baseline, polygon, [hole1, hole2])
     doc.saveas(CWD / filename)
 
 
-def draw(d:str) -> List[Vec2]:
+def draw(d: str) -> List[Vec2]:
     point = Vec2()
     points = [point]
     for cmd in d.split(","):
@@ -55,36 +52,70 @@ def draw(d:str) -> List[Vec2]:
     return points
 
 
-def collinear_hatching(filename: str):
-    doc = ezdxf.new()
-    msp = doc.modelspace()
-    polygons = [
-        draw("h+10, v+10, h-10"),
-        draw("h+2, v+2, h+2, v-2, h+6, v+10, h-2, v-2, h-2, v+2, h-6"),
-        draw("h+2, v+2, h+2, v+2, h+2, v-4, h+4, v+10, h-2, v-2, h-2, v-2, h-2, v+4, h-4"),
-        draw("h+2, v-2, h+2, v-2, h+2, v+4, h+4, v+10, h-2, v+2, h-2, v+2, h-2, v-4, h-4"),
-        draw("h+2, v+2, h+2, v-2, h+2, v+3, h+2, v-3, h+2, v+10, h-10"),
-        draw("h+3, q1+2, q4+2, h+3, v+10, q3+2, q2+2, h-2, q3+2, q2+2")
-    ]
-    for index, polygon in enumerate(polygons):
-        polygon = Vec2.list(forms.translate(polygon, Vec2(12 * index, 0)))
+SIZE = 0.1
+
+
+def setup(doc):
+    doc.layers.add("POLYGON", color=ezdxf.colors.BLUE)
+    doc.layers.add("MARKERS", color=ezdxf.colors.GREEN)
+    doc.layers.add("POINTS", color=ezdxf.colors.RED)
+    doc.layers.add("HATCH", color=ezdxf.colors.YELLOW)
+    marker = doc.blocks.new("MARKER")
+    marker.add_line((-SIZE, -SIZE), (SIZE, SIZE))
+    marker.add_line((-SIZE, SIZE), (SIZE, -SIZE))
+
+
+def render_hatch(msp, baseline, polygon, holes=None, offset=Vec2()):
+    polygons = [Vec2.list(forms.translate(polygon, offset))]
+    if holes:
+        for hole in holes:
+            polygons.append(Vec2.list(forms.translate(hole, offset)))
+    for polygon in polygons:
         msp.add_lwpolyline(
             polygon,
             close=True,
-            dxfattribs={"color": ezdxf.colors.BLUE, "layer": "POLYGON"},
+            dxfattribs={"layer": "POLYGON"},
         )
+        for p in polygon:
+            msp.add_circle(
+                p,
+                radius=SIZE,
+                dxfattribs={"layer": "POINTS"},
+            )
+    for line in hatching.hatch_polygons(baseline, polygons):
+        msp.add_line(
+            line.start,
+            line.end,
+            dxfattribs={"layer": "HATCH"},
+        )
+        msp.add_blockref("MARKER", line.start, dxfattribs={"layer": "MARKERS"})
+        msp.add_blockref("MARKER", line.end, dxfattribs={"layer": "MARKERS"})
+
+
+def collinear_hatching(filename: str):
+    doc = ezdxf.new()
+    msp = doc.modelspace()
+    setup(doc)
+    polygons = [
+        draw("h+10, v+10, h-10"),
+        draw("h+2, v+2, h+2, v-2, h+6, v+10, h-2, v-2, h-2, v+2, h-6"),
+        draw(
+            "h+2, v+2, h+2, v+2, h+2, v-4, h+4, v+10, h-2, v-2, h-2, v-2, h-2, v+4, h-4"
+        ),
+        draw(
+            "h+2, v-2, h+2, v-2, h+2, v+4, h+4, v+10, h-2, v+2, h-2, v+2, h-2, v-4, h-4"
+        ),
+        draw("h+2, v+2, h+2, v-2, h+2, v+3, h+2, v-3, h+2, v+10, h-10"),
+        draw("h+3, q1+2, q4+2, h+3, v+10, q3+2, q2+2, h-2, q3+2, q2+2"),
+        draw(
+            "h+3, q1+1, q1+1, q4+1, q4+1, h+3, v+10, q3+1, q3+1, q2+1, q2+1, h-2, q3+1, q3+1, q2+1, q2+1"
+        ),
+    ]
+    for index, polygon in enumerate(polygons):
         baseline = hatching.HatchBaseLine(
             Vec2(), direction=Vec2(1, 0), offset=Vec2(0, 1)
         )
-        for line in hatching.hatch_polygons(baseline, [polygon]):
-            msp.add_line(
-                line.start,
-                line.end,
-                dxfattribs={
-                    "color": ezdxf.colors.YELLOW,
-                    "layer": "HATCH",
-                },
-            )
+        render_hatch(msp, baseline, polygon, offset=Vec2(12 * index, 0))
     doc.saveas(CWD / filename)
 
 
