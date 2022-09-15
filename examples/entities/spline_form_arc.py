7dc78cf14783a177a6ea92c08a870908e73013d4
mozman <me@mozman.at>
2022-09-15 16:12:48 +0200

    refactor example spline_form_arc.py

--- examples/entities/spline_form_arc.py
+++ examples/entities/spline_form_arc.py
@@ -1,26 +1,37 @@
-# Copyright (c) 2020-2021, Manfred Moitzi
+# Copyright (c) 2020-2022, Manfred Moitzi
 # License: MIT License
-from pathlib import Path
+import pathlib
 import ezdxf
 from ezdxf import zoom
 
-DIR = Path("~/Desktop/outbox").expanduser()
+CWD = pathlib.Path("~/Desktop/Outbox").expanduser()
+if not CWD.exists():
+    CWD = pathlib.Path(".")
 
-doc = ezdxf.new()
-msp = doc.modelspace()
+# ------------------------------------------------------------------------------
+# This example shows how to convert an ARC into a SPLINE.
+# ------------------------------------------------------------------------------
 
 
-arc = msp.add_arc(
-    center=(0, 0),
-    radius=1.0,
-    start_angle=0,
-    end_angle=360,
-    dxfattribs={"layer": "arc"},
-)
+def main():
+    doc = ezdxf.new()
+    msp = doc.modelspace()
 
-spline = arc.to_spline(replace=False)
-spline.dxf.layer = "B-spline"
-spline.dxf.color = 1
+    arc = msp.add_arc(
+        center=(0, 0),
+        radius=1.0,
+        start_angle=0,
+        end_angle=360,
+        dxfattribs={"layer": "arc"},
+    )
 
-zoom.extents(msp)
-doc.saveas(DIR / "spline_from_arc.dxf")
+    spline = arc.to_spline(replace=False)
+    spline.dxf.layer = "B-spline"
+    spline.dxf.color = 1
+
+    zoom.extents(msp)
+    doc.saveas(CWD / "spline_from_arc.dxf")
+
+
+if __name__ == "__main__":
+    main()
