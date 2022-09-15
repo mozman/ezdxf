028807337bc7f330c294b0dfe6aeaa1b1800b83c
mozman <me@mozman.at>
2022-09-15 16:14:44 +0200

    refactor example spline_from_ellipse.py

--- examples/entities/spline_from_ellipse.py
+++ examples/entities/spline_from_ellipse.py
@@ -1,28 +1,39 @@
 # Copyright (c) 2020-2021, Manfred Moitzi
 # License: MIT License
-
-from pathlib import Path
+import pathlib
 import math
 import ezdxf
 from ezdxf import zoom
 
-DIR = Path("~/Desktop/Outbox").expanduser()
+CWD = pathlib.Path("~/Desktop/Outbox").expanduser()
+if not CWD.exists():
+    CWD = pathlib.Path(".")
 
-doc = ezdxf.new()
-msp = doc.modelspace()
+# ------------------------------------------------------------------------------
+# This example shows how to convert an ELLIPSE into a SPLINE.
+# ------------------------------------------------------------------------------
 
-ellipse = msp.add_ellipse(
-    center=(0, 0),
-    major_axis=(1, 0),
-    ratio=0.5,
-    start_param=0,
-    end_param=math.tau,
-    dxfattribs={"layer": "ellipse"},
-)
 
-spline = ellipse.to_spline(replace=False)
-spline.dxf.layer = "B-spline"
-spline.dxf.color = 1
+def main():
+    doc = ezdxf.new()
+    msp = doc.modelspace()
 
-zoom.extents(msp)
-doc.saveas(DIR / "spline_from_ellipse.dxf")
+    ellipse = msp.add_ellipse(
+        center=(0, 0),
+        major_axis=(1, 0),
+        ratio=0.5,
+        start_param=0,
+        end_param=math.tau,
+        dxfattribs={"layer": "ellipse"},
+    )
+
+    spline = ellipse.to_spline(replace=False)
+    spline.dxf.layer = "B-spline"
+    spline.dxf.color = 1
+
+    zoom.extents(msp)
+    doc.saveas(CWD / "spline_from_ellipse.dxf")
+
+
+if __name__ == "__main__":
+    main()
