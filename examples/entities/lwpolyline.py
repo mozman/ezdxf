46391ee382907192802ddf5bb323bca3212948c2
mozman <me@mozman.at>
2022-09-15 14:27:35 +0200

    refactor example lwpolyline.py

--- examples/entities/lwpolyline.py
+++ examples/entities/lwpolyline.py
@@ -1,51 +1,63 @@
-# Purpose: using lwpolyline
-# Copyright (c) 2014-2021, Manfred Moitzi
+# Copyright (c) 2014-2022, Manfred Moitzi
 # License: MIT License
+import pathlib
 import ezdxf
 
+CWD = pathlib.Path("~/Desktop/Outbox").expanduser()
+if not CWD.exists():
+    CWD = pathlib.Path(".")
 
-def tut_lwpolyline():
+
+# ------------------------------------------------------------------------------
+# This example shows the usage of the LWPOLYLINE entity, which is the default
+# 2D polyline entity (LW stands for LightWeight).
+#
+# docs: https://ezdxf.mozman.at/docs/dxfentities/lwpolyline.html
+# circular arc segments: https://ezdxf.mozman.at/docs/dxfentities/lwpolyline.html#bulge-value
+# tutorial: https://ezdxf.mozman.at/docs/tutorials/lwpolyline.html
+# ------------------------------------------------------------------------------
+
+
+def create_lwpolyline():
     doc = ezdxf.new("R2000")
     msp = doc.modelspace()
 
     points = [(0, 0), (3, 0), (6, 3), (6, 6)]
     msp.add_lwpolyline(points)
+    doc.saveas(CWD / "lwpolyline1.dxf")
 
-    doc.saveas("lwpolyline1.dxf")
 
-    # Append points to a polyline::
-
-    doc = ezdxf.readfile("lwpolyline1.dxf")
+def append_points_to_lwpolyline():
+    doc = ezdxf.readfile(CWD / "lwpolyline1.dxf")
     msp = doc.modelspace()
 
-    line = msp.query("LWPOLYLINE")[0]  # take first LWPolyline
-    line.append_points([(8, 7), (10, 7)])
+    pline = msp.query("LWPOLYLINE").first
+    pline.append_points([(8, 7), (10, 7)])
+    doc.saveas(CWD / "lwpolyline2.dxf")
 
-    doc.saveas("lwpolyline2.dxf")
 
-    # Use context manager to edit polyline::
-
-    doc = ezdxf.readfile("lwpolyline2.dxf")
+def edit_lwpolyline():
+    doc = ezdxf.readfile(CWD / "lwpolyline2.dxf")
     msp = doc.modelspace()
 
-    line = msp.query("LWPOLYLINE")[0]  # take first LWPolyline
-
-    with line.points() as points:  # points is a python standard list
+    pline = msp.query("LWPOLYLINE").first
+    # edit by context manager:
+    with pline.points() as points:  # points is a python standard list
         # del points[-2:]  # delete last 2 points
         # points.extend([(4, 7), (0, 7)])  # adding 2 other points
         # the same as one command
         points[-2:] = [(4, 7), (0, 7)]
     # implicit call of line.set_points(points) at context manager exit
+    doc.saveas(CWD / "lwpolyline3.dxf")
 
-    doc.saveas("lwpolyline3.dxf")
 
-    # Each line segment can have a different start/end width, if omitted start/end width = 0::
-
+def lwpolyline_width():
     doc = ezdxf.new("AC1015")
     msp = doc.modelspace()
 
+    # Each line segment can have a different start/end width, if omitted
+    # start/end width = 0.
     # point format = (x, y, [start_width, [end_width, [bulge]]])
-
     points = [
         (0, 0, 0.1, 0.15),
         (3, 0, 0.5, 0.75),
@@ -53,14 +65,14 @@ def tut_lwpolyline():
         (6, 6, 0.4, 0.45),
     ]
     msp.add_lwpolyline(points)
+    doc.saveas(CWD / "lwpolyline_width.dxf")
 
-    doc.saveas("lwpolyline4.dxf")
 
-    # LWPolyline can also have curved elements, they are defined by the bulge value::
-
+def lwpolyline_with_circular_arcs():
     doc = ezdxf.new("AC1015")
     msp = doc.modelspace()
 
+    # LWPolyline can also have curved elements, they are defined by the bulge value.
     # point format = (x, y, [start_width, [end_width, [bulge]]])
 
     points = [
@@ -70,8 +82,12 @@ def tut_lwpolyline():
         (9, 0),
     ]
     msp.add_lwpolyline(points)
+    doc.saveas(CWD / "lwpolyline_bulge.dxf")
 
-    doc.saveas("lwpolyline5.dxf")
 
-
-tut_lwpolyline()
+if __name__ == "__main__":
+    create_lwpolyline()
+    append_points_to_lwpolyline()
+    edit_lwpolyline()
+    lwpolyline_width()
+    lwpolyline_with_circular_arcs()
