bcc5cc3ae261e1169ad91348ea26ff06177691eb
mozman <me@mozman.at>
2022-09-25 09:20:29 +0200

    remove dummy module ezdxf.render.path
    This module was replaced by the
    ezdxf.path sub-package in v0.16

--- src/ezdxf/render/path.py
+++
@@ -1,8 +0,0 @@
-# Dummy file
-# features of v0.15.1
-from ezdxf.path import Path, Command
-import warnings
-
-warnings.warn(
-    'Import class Path() only from ezdxf.path, ezdxf.render.path.py will be '
-    'removed in v0.17.', DeprecationWarning)
