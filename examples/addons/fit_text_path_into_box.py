5763c341a1660e13b70780a37d822eed65b00255
mozman <me@mozman.at>
2022-09-26 07:30:20 +0200

    refactor example fit_text_path_into_box.py

--- examples/addons/fit_text_path_into_box.py
+++ examples/addons/fit_text_path_into_box.py
@@ -1,51 +1,64 @@
-#  Copyright (c) 2021, Manfred Moitzi
+#  Copyright (c) 2021-2022, Manfred Moitzi
 #  License: MIT License
-
-from pathlib import Path
+import pathlib
 import ezdxf
 from ezdxf import path, zoom
 from ezdxf.math import Matrix44
 from ezdxf.tools import fonts
 from ezdxf.addons import text2path
 
-DIR = Path("~/Desktop/Outbox").expanduser()
-fonts.load()
+CWD = pathlib.Path("~/Desktop/Outbox").expanduser()
+if not CWD.exists():
+    CWD = pathlib.Path(".")
 
-doc = ezdxf.new()
-doc.layers.new("OUTLINE")
-doc.layers.new("FILLING")
-msp = doc.modelspace()
+# ------------------------------------------------------------------------------
+# This example shows how create outline paths from a text and fit them into a
+# specified rectangle.
+# ------------------------------------------------------------------------------
 
-attr = {"color": 2}
-ff = fonts.FontFace(family="Arial")
-sx, sy = 4, 2
 
-# create the target box:
-msp.add_lwpolyline(
-    [(0, 0), (sx, 0), (sx, sy), (0, sy)], close=True, dxfattribs={"color": 1}
-)
+def main():
+    doc = ezdxf.new()
+    msp = doc.modelspace()
 
-# convert text string into path objects:
-text_as_paths = text2path.make_paths_from_str("Squeeze Me", ff)
+    ff = fonts.FontFace(family="Arial")
+    box_width, box_height = 4, 2
 
-# fit text paths into a given box size by scaling, does not move the path objects:
-# uniform=True, keeps the text aspect ratio
-# uniform=False, scales the text to touch all 4 sides of the box
-final_paths = path.fit_paths_into_box(
-    text_as_paths, size=(sx, sy, 0), uniform=False
-)
+    # Draw the target box:
+    msp.add_lwpolyline(
+        [(0, 0), (box_width, 0), (box_width, box_height), (0, box_height)],
+        close=True,
+        dxfattribs={"color": 1},
+    )
 
-# mirror text along x-axis
-final_paths = path.transform_paths(final_paths, Matrix44.scale(-1, 1, 1))
+    # Convert text string into path objects:
+    text_as_paths = text2path.make_paths_from_str("Squeeze Me", ff)
 
-# move bottom/left corner to (0, 0) if required:
-bbox = path.bbox(final_paths)
-dx, dy, dz = -bbox.extmin
-final_paths = path.transform_paths(final_paths, Matrix44.translate(dx, dy, dz))
+    # Fit text paths into a given box size by scaling, does not move the path
+    # objects:
+    # - uniform=True, keeps the text aspect ratio
+    # - uniform=False, scales the text to touch all 4 sides of the box
+    final_paths = path.fit_paths_into_box(
+        text_as_paths, size=(box_width, box_height, 0), uniform=False
+    )
 
-path.render_lwpolylines(
-    msp, final_paths, distance=0.01, dxfattribs={"color": 2}
-)
+    # Mirror text about the x-axis
+    final_paths = path.transform_paths(final_paths, Matrix44.scale(-1, 1, 1))
 
-zoom.extents(msp)
-doc.saveas(DIR / "SqeezeMe.dxf")
+    # Move bottom/left corner to (0, 0) if required:
+    bbox = path.bbox(final_paths)
+    dx, dy, dz = -bbox.extmin
+    final_paths = path.transform_paths(
+        final_paths, Matrix44.translate(dx, dy, dz)
+    )
+
+    path.render_lwpolylines(
+        msp, final_paths, distance=0.01, dxfattribs={"color": 2}
+    )
+
+    zoom.extents(msp)
+    doc.saveas(CWD / "SqueezeMe.dxf")
+
+
+if __name__ == "__main__":
+    main()
