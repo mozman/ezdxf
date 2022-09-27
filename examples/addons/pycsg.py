9fe99f746cfc0071e661e44c4386e7d4870083d2
mozman <me@mozman.at>
2022-09-27 06:44:45 +0200

    refactor pycsg examples

--- examples/addons/pycsg.py
+++ examples/addons/pycsg.py
@@ -1,36 +1,54 @@
 # Copyright (c) 2020-2022, Manfred Moitzi
 # License: MIT License
-from pathlib import Path
+import pathlib
 import ezdxf
 from ezdxf.addons.pycsg import CSG
 from ezdxf.render.forms import cube, cylinder_2p
 
-DIR = Path("~/Desktop/Outbox").expanduser()
-if not DIR.exists():
-    DIR = Path(".")
+CWD = pathlib.Path("~/Desktop/Outbox").expanduser()
+if not CWD.exists():
+    CWD = pathlib.Path(".")
 
-cube1 = cube()
-cylinder1 = cylinder_2p(
-    count=32, base_center=(0, -1, 0), top_center=(0, 1, 0), radius=0.25
-)
+# ------------------------------------------------------------------------------
+# This example shows how to use the pycsg add-on (Constructive Solid Geometry).
+#
+# docs: https://ezdxf.mozman.at/docs/addons/pycsg.html
+# ------------------------------------------------------------------------------
 
-doc = ezdxf.new()
-doc.set_modelspace_vport(6, center=(5, 0))
-msp = doc.modelspace()
 
-# build solid union
-union = CSG(cube1) + CSG(cylinder1)
-# convert to mesh and render mesh to modelspace
-union.mesh().merge_coplanar_faces().render_mesh(msp, dxfattribs={"color": 1})
+def main():
+    cube1 = cube()
+    cylinder1 = cylinder_2p(
+        count=32, base_center=(0, -1, 0), top_center=(0, 1, 0), radius=0.25
+    )
 
-# build solid difference
-difference = CSG(cube1) - CSG(cylinder1)
-# convert to mesh, translate mesh and render mesh to modelspace
-difference.mesh().merge_coplanar_faces().translate(1.5).render(msp, dxfattribs={"color": 3})
+    doc = ezdxf.new()
+    doc.set_modelspace_vport(6, center=(5, 0))
+    msp = doc.modelspace()
 
-# build solid intersection
-intersection = CSG(cube1) * CSG(cylinder1)
-# convert to mesh, translate mesh and render mesh to modelspace
-intersection.mesh().translate(2.75).render(msp, dxfattribs={"color": 5})
+    # build solid union
+    union = CSG(cube1) + CSG(cylinder1)
+    # convert to mesh and render mesh to modelspace
+    union.mesh().merge_coplanar_faces().render_mesh(
+        msp, dxfattribs={"color": 1}
+    )
 
-doc.saveas(DIR / "pycsg01.dxf")
+    # build solid difference
+    difference = CSG(cube1) - CSG(cylinder1)
+    # convert to mesh, translate mesh and render mesh to modelspace
+    difference.mesh().merge_coplanar_faces().translate(1.5).render_mesh(
+        msp, dxfattribs={"color": 3}
+    )
+
+    # build solid intersection
+    intersection = CSG(cube1) * CSG(cylinder1)
+    # convert to mesh, translate mesh and render mesh to modelspace
+    intersection.mesh().translate(2.75).render_mesh(
+        msp, dxfattribs={"color": 5}
+    )
+
+    doc.saveas(CWD / "pycsg01.dxf")
+
+
+if __name__ == "__main__":
+    main()
--- examples/addons/pycsg_sphere.py
+++ examples/addons/pycsg_sphere.py
@@ -1,44 +1,54 @@
 # Copyright (c) 2020-2022, Manfred Moitzi
 # License: MIT License
-
-from pathlib import Path
+import pathlib
 import ezdxf
-
 from ezdxf.render.forms import sphere, cube
 from ezdxf.addons.pycsg import CSG
 
-DIR = Path("~/Desktop/Outbox").expanduser()
-if not DIR.exists():
-    DIR = Path(".")
+CWD = pathlib.Path("~/Desktop/Outbox").expanduser()
+if not CWD.exists():
+    CWD = pathlib.Path(".")
+
+# ------------------------------------------------------------------------------
+# This example shows how to use the pycsg add-on (Constructive Solid Geometry).
+#
+# docs: https://ezdxf.mozman.at/docs/addons/pycsg.html
+# ------------------------------------------------------------------------------
 
 NLENGTH = 0.05
 
-doc = ezdxf.new()
-doc.layers.new("csg", dxfattribs={"color": 1})
-doc.layers.new("normals", dxfattribs={"color": 6})
 
-doc.set_modelspace_vport(6, center=(5, 0))
-msp = doc.modelspace()
+def main():
+    doc = ezdxf.new()
+    doc.layers.new("csg", dxfattribs={"color": 1})
+    doc.layers.new("normals", dxfattribs={"color": 6})
 
-cube1 = cube().translate(-0.5, -0.5, -0.5)
-sphere1 = sphere(count=32, stacks=16, radius=0.5, quads=True)
+    doc.set_modelspace_vport(6, center=(5, 0))
+    msp = doc.modelspace()
 
-union = (CSG(cube1) + CSG(sphere1)).mesh()
-union.render_mesh(msp, dxfattribs={"layer": "csg", "color": 1})
-union.render_normals(
-    msp, length=NLENGTH, relative=False, dxfattribs={"layer": "normals"}
-)
+    cube1 = cube().translate(-0.5, -0.5, -0.5)
+    sphere1 = sphere(count=32, stacks=16, radius=0.5, quads=True)
 
-subtract = (CSG(cube1) - CSG(sphere1)).mesh().translate(2.5)
-subtract.render_mesh(msp, dxfattribs={"layer": "csg", "color": 3})
-subtract.render_normals(
-    msp, length=NLENGTH, relative=False, dxfattribs={"layer": "normals"}
-)
+    union = (CSG(cube1) + CSG(sphere1)).mesh()
+    union.render_mesh(msp, dxfattribs={"layer": "csg", "color": 1})
+    union.render_normals(
+        msp, length=NLENGTH, relative=False, dxfattribs={"layer": "normals"}
+    )
 
-intersection = (CSG(cube1) * CSG(sphere1)).mesh().translate(4)
-intersection.render(msp, dxfattribs={"layer": "csg", "color": 5})
-intersection.render_normals(
-    msp, length=NLENGTH, relative=False, dxfattribs={"layer": "normals"}
-)
+    subtract = (CSG(cube1) - CSG(sphere1)).mesh().translate(2.5)
+    subtract.render_mesh(msp, dxfattribs={"layer": "csg", "color": 3})
+    subtract.render_normals(
+        msp, length=NLENGTH, relative=False, dxfattribs={"layer": "normals"}
+    )
 
-doc.saveas(DIR / "csg_sphere.dxf")
+    intersection = (CSG(cube1) * CSG(sphere1)).mesh().translate(4)
+    intersection.render_mesh(msp, dxfattribs={"layer": "csg", "color": 5})
+    intersection.render_normals(
+        msp, length=NLENGTH, relative=False, dxfattribs={"layer": "normals"}
+    )
+
+    doc.saveas(CWD / "csg_sphere.dxf")
+
+
+if __name__ == "__main__":
+    main()
