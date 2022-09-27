b32bc3cd04d06a3d83d487113010524f3620e430
mozman <me@mozman.at>
2022-09-27 06:33:43 +0200

    refactor odafc examples

--- examples/addons/odafc_read_dwg.py
+++ examples/addons/odafc_read_dwg.py
@@ -1,12 +1,20 @@
-# Copyright (c) 2020-2021, Manfred Moitzi
+# Copyright (c) 2020-2022, Manfred Moitzi
 # License: MIT License
-from pathlib import Path
-
+import pathlib
 import ezdxf
 from ezdxf.addons import odafc
 
+CWD = pathlib.Path("~/Desktop/Outbox").expanduser()
+if not CWD.exists():
+    CWD = pathlib.Path(".")
+
+# ------------------------------------------------------------------------------
+# This example shows how to read DWG files by the "Open Design Alliance File Converter" (odafc).
+#
+# docs: https://ezdxf.mozman.at/docs/addons/odafc.html
+# ------------------------------------------------------------------------------
+
 FILE = "colorwh.dwg"
-OUTDIR = Path("~/Desktop/outbox").expanduser()
 
 doc = odafc.readfile(ezdxf.options.test_files_path / "AutodeskSamples" / FILE)
 if doc:
@@ -14,4 +22,4 @@ if doc:
     print(f"Filename: {doc.filename}")
     print(f"DXF Version: {doc.dxfversion} - {doc.acad_release}")
     print(f"Modelspace has {len(msp)} entities.")
-    doc.saveas((OUTDIR / FILE).with_suffix(".dxf"))
+    doc.saveas((CWD / FILE).with_suffix(".dxf"))
