08f45cb126b1cf473c02cde31431792913ef4f40
mozman <me@mozman.at>
2022-10-16 10:07:36 +0200

    refactor example separate_layers.py

--- examples/separate_layers.py
+++ examples/separate_layers.py
@@ -3,7 +3,7 @@
 import pathlib
 import ezdxf
 from ezdxf.addons import Importer
-from ezdxf.entities import DXFEntity
+from ezdxf.query import EntityQuery
 
 CWD = pathlib.Path("~/Desktop/Outbox").expanduser()
 if not CWD.exists():
@@ -14,9 +14,7 @@ SOURCE_DXF = "test.dxf"
 # ------------------------------------------------------------------------------
 # This example shows how to separate entities by their layer attribute
 #
-# The example imports all entities from each layer defined in the layer table
-# into a new DXF document. The catch: a layer don't have to have an entry in the
-# layer table!
+# The example imports all entities from the modelspace into a new DXF document.
 # The Importer add-on is required to also import the required resources used by
 # the imported entities.
 #
@@ -30,25 +28,28 @@ SOURCE_DXF = "test.dxf"
 
 
 def main():
-    doc = ezdxf.readfile(CWD / SOURCE_DXF)
-    msp = doc.modelspace()
+    source_doc = ezdxf.readfile(CWD / SOURCE_DXF)
+    source_msp = source_doc.modelspace()
     # create an EntityQuery container with all entities from the modelspace
-    all_entities = msp.query()
-    # get all existing layers, filter unwanted layers if needed
-    all_layer_names = [layer.dxf.name for layer in doc.layers]
+    source_entities = source_msp.query()
+    # get all layer names from entities in the modelspace:
+    all_layer_names = [e.dxf.layer for e in source_entities]
+    # remove unwanted layers if needed
+
     for layer_name in all_layer_names:
-        # create a new document for each layer and the same DXF version as the
+        # create a new document for each layer with the same DXF version as the
         # source file
-        layer_doc = ezdxf.new(dxfversion=doc.dxfversion)
+        layer_doc = ezdxf.new(dxfversion=source_doc.dxfversion)
         layer_msp = layer_doc.modelspace()
-        importer = Importer(doc, layer_doc)
-        # select all entities from modelspace from this layer
-        entities: list[DXFEntity] = all_entities.layer == layer_name  # type: ignore
+        importer = Importer(source_doc, layer_doc)
+        # select all entities from modelspace from this layer (extended query
+        # feature of class EntityQuery)
+        entities: EntityQuery = source_entities.layer == layer_name  # type: ignore
         if len(entities):
             importer.import_entities(entities, layer_msp)
-        # create required resources
-        importer.finalize()
-        layer_doc.saveas(CWD / f"{layer_name}.dxf")
+            # create required resources
+            importer.finalize()
+            layer_doc.saveas(CWD / f"{layer_name}.dxf")
 
 
 if __name__ == "__main__":
