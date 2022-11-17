ff13dddcee529b13d1e0c4d8f5f65e6929c2f716
mozman <me@mozman.at>
2022-11-17 16:00:41 +0100

    refactor dim_curved.py type annotations

--- src/ezdxf/render/dim_curved.py
+++ src/ezdxf/render/dim_curved.py
@@ -2,7 +2,7 @@
 # License: MIT License
 from __future__ import annotations
 import math
-from typing import TYPE_CHECKING, Tuple, List, Optional, Dict, Any
+from typing import TYPE_CHECKING, Optional
 from abc import abstractmethod
 import logging
 
@@ -43,7 +43,7 @@ logger = logging.getLogger("ezdxf")
 ARC_PREFIX = "( "
 
 
-def has_required_attributes(entity: DXFEntity, attrib_names: List[str]):
+def has_required_attributes(entity: DXFEntity, attrib_names: list[str]):
     has = entity.dxf.hasattr
     return all(has(attrib_name) for attrib_name in attrib_names)
 
@@ -280,7 +280,7 @@ class _CurvedDimensionLine(BaseDimensionRenderer):
         ...
 
     @abstractmethod
-    def get_defpoints(self) -> List[Vec2]:
+    def get_defpoints(self) -> list[Vec2]:
         ...
 
     def transform_ucs_to_wcs(self) -> None:
@@ -449,7 +449,7 @@ class _CurvedDimensionLine(BaseDimensionRenderer):
                 rotation += 180.0  # apply to UCS rotation!
         measurement.text_rotation = rotation
 
-    def get_leader_points(self) -> Tuple[Vec2, Vec2]:
+    def get_leader_points(self) -> tuple[Vec2, Vec2]:
         # Leader location is defined by dimtad (text_valign):
         # "center":
         # - connects to the left or right vertical center of the text
@@ -469,7 +469,7 @@ class _CurvedDimensionLine(BaseDimensionRenderer):
                 p1, p2 = c0, c1
             return order_leader_points(self.dim_midpoint, p1, p2)
 
-    def render(self, block: "GenericLayoutType") -> None:
+    def render(self, block: GenericLayoutType) -> None:
         """Main method to create dimension geometry of basic DXF entities in the
         associated BLOCK layout.
 
@@ -508,7 +508,7 @@ class _CurvedDimensionLine(BaseDimensionRenderer):
             )
 
     def _add_ext_line(
-        self, start: Vec2, direction: Vec2, dxfattribs: Dict[str, Any]
+        self, start: Vec2, direction: Vec2, dxfattribs
     ) -> None:
         ext_lines = self.extension_lines
         center = self.center_of_arc
@@ -530,7 +530,7 @@ class _CurvedDimensionLine(BaseDimensionRenderer):
         end = center + direction * (radius + ext_above)
         self.add_line(start, end, dxfattribs=dxfattribs)
 
-    def add_arrows(self) -> Tuple[float, float]:
+    def add_arrows(self) -> tuple[float, float]:
         """Add arrows or ticks to dimension.
 
         Returns: dimension start- and end angle offsets to adjust the
@@ -763,7 +763,7 @@ class AngularDimension(_AngularCommonBase):
             p2=self.leg2_end,
         )
 
-    def get_defpoints(self) -> List[Vec2]:
+    def get_defpoints(self) -> list[Vec2]:
         return [
             self.leg1_start,
             self.leg1_end,
@@ -849,7 +849,7 @@ class Angular3PDimension(_AngularCommonBase):
         self.ext1_start = self.leg1_start
         self.ext2_start = self.leg2_start
 
-    def get_defpoints(self) -> List[Vec2]:
+    def get_defpoints(self) -> list[Vec2]:
         return [
             self.dim_line_location,
             self.leg1_start,
@@ -913,8 +913,8 @@ class ArcLengthDimension(_CurvedDimensionLine):
     def __init__(
         self,
         dimension: Dimension,
-        ucs: "UCS" = None,
-        override: DimStyleOverride = None,
+        ucs: Optional[UCS] = None,
+        override: Optional[DimStyleOverride] = None,
     ):
         self.dim_line_location = get_required_defpoint(dimension, "defpoint")
         self.leg1_start = get_required_defpoint(dimension, "defpoint2")
@@ -925,7 +925,7 @@ class ArcLengthDimension(_CurvedDimensionLine):
         self.ext1_start = self.leg1_start
         self.ext2_start = self.leg2_start
 
-    def get_defpoints(self) -> List[Vec2]:
+    def get_defpoints(self) -> list[Vec2]:
         return [
             self.dim_line_location,
             self.leg1_start,
@@ -971,7 +971,7 @@ def detect_closer_defpoint(
 
 def arrow_offset_angles(
     arrow_name: str, size: float, radius: float
-) -> Tuple[float, float]:
+) -> tuple[float, float]:
     start_offset: float = 0.0
     end_offset: float = size / radius
     length = arrow_length(arrow_name, size)
