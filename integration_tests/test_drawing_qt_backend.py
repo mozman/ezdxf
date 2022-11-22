040bef379c75858083e2e0c250d5afb8737622e0
mozman <me@mozman.at>
2022-11-22 05:28:39 +0100

    skip Qt backend test inside PyCharm

--- integration_tests/test_drawing_qt_backend.py
+++ integration_tests/test_drawing_qt_backend.py
@@ -1,31 +1,26 @@
 # Copyright (c) 2020-2022, Matthew Broadway
 # License: MIT License
 import pytest
+import os
 
 pytest.importorskip("PySide6")
 from ezdxf.addons.xqt import QtWidgets as qw
 from ezdxf.addons.drawing.pyqt import PyQtBackend
 
-_app = None
-
 
 @pytest.fixture
 def backend():
-    # This works on Windows outside PyCharm!
-    #global _app
-    #_app = qw.QApplication([])
     scene = qw.QGraphicsScene()
     return PyQtBackend(scene)
 
 
-# Error Message:
-# RuntimeError: Please destroy the QApplication singleton before creating a new
-# QApplication instance.
-#
-# ezdxf "view" command works as expected!
-# Works if launched in PyCharm, but not if launched by command line
-# "pytest ..." ???
-
+# --------------------------------
+# This works only outside PyCharm!
+# --------------------------------
+@pytest.mark.skipif(
+    "PYCHARM_HOSTED" in os.environ,
+    reason="Qt backend test doesn't work inside PyCharm",
+)
 def test_get_text_width(backend):
     assert backend.get_text_line_width(
         "   abc", 100
