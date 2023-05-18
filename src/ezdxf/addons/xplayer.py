fa0ebb145e8a1578cbd12acc6aa30c34a6102aab
mozman <me@mozman.at>
2023-05-18 11:23:33 +0200

    refactor xplayer.py

--- src/ezdxf/addons/xplayer.py
+++ src/ezdxf/addons/xplayer.py
@@ -6,11 +6,14 @@ from ezdxf.math import Vec2
 
 from ezdxf.addons.drawing.backend import BackendInterface
 from ezdxf.addons.drawing.properties import BackendProperties, rgb_to_hex
-from ezdxf.addons.hpgl2 import api
-from ezdxf.addons.hpgl2.backend import Properties as HPGL2Properties, RecordType
+from ezdxf.addons.hpgl2 import api as hpgl2
+from ezdxf.addons.hpgl2.backend import (
+    Properties as HPGL2Properties,
+    RecordType as HPGL2RecordType,
+)
 
 
-def hpgl2_to_drawing(recorder: api.Recorder, backend: BackendInterface) -> None:
+def hpgl2_to_drawing(recorder: hpgl2.Recorder, backend: BackendInterface) -> None:
     """Replays the recordings of the HPGL2 Recorder on a backend of the drawing add-on."""
     backend.set_background("#ffffff")  # plotting on white paper
     props = recorder.properties
@@ -18,7 +21,7 @@ def hpgl2_to_drawing(recorder: api.Recorder, backend: BackendInterface) -> None:
         backend_properties = _make_drawing_backend_properties(
             props[record.property_hash]
         )
-        if record.type == RecordType.POLYLINE:
+        if record.type == HPGL2RecordType.POLYLINE:
             points: list[Vec2] = record.data.vertices()
             size = len(points)
             if size == 1:
@@ -27,7 +30,7 @@ def hpgl2_to_drawing(recorder: api.Recorder, backend: BackendInterface) -> None:
                 backend.draw_line(points[0], points[1], backend_properties)
             else:
                 backend.draw_path(_from_2d_points(points), backend_properties)
-        elif record.type == RecordType.FILLED_POLYGON:
+        elif record.type == HPGL2RecordType.FILLED_POLYGON:
             # filled polygons are stored as single paths! see: PolygonBuffer.get_paths()
             external_paths, holes = path.winding_deconstruction(
                 path.fast_bbox_detection(p.to_path2d() for p in record.data)
