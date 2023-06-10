91b9a80dfe0a339dff8456396e7c8b96e2aec9b5
mozman <me@mozman.at>
2023-06-10 20:31:01 +0200

    add tests for npshapes.to_matplotlib_path

--- tests/test_07_render/test_708c_matplotlib_path_tools.py
+++ tests/test_07_render/test_708c_matplotlib_path_tools.py
@@ -1,4 +1,4 @@
-#  Copyright (c) 2021, Manfred Moitzi
+#  Copyright (c) 2021-2023, Manfred Moitzi
 #  License: MIT License
 
 import pytest
@@ -9,6 +9,7 @@ from matplotlib.textpath import TextPath
 from matplotlib.font_manager import FontProperties
 from ezdxf import path
 from ezdxf.math import Vec2
+from ezdxf import npshapes
 
 
 class TestFromMatplotlibPath:
@@ -46,7 +47,7 @@ class TestFromMatplotlibPath:
 from ezdxf.path.converter import MplCmd as MC
 
 
-class TestToMatplotlibPath:
+class TestPathToMatplotlibPath:
     def test_no_paths(self):
         with pytest.raises(ValueError):
             path.to_matplotlib_path([])
@@ -104,6 +105,80 @@ class TestToMatplotlibPath:
         p.line_to((7, 8, 9))
 
         mpath = path.to_matplotlib_path([p])
+        assert tuple(mpath.codes) == (
+            MC.MOVETO,
+            MC.LINETO,
+            MC.MOVETO,
+            MC.LINETO,
+        )
+        assert Vec2.list(mpath.vertices) == [
+            (0, 0),
+            (4, 5),
+            (0, 0),
+            (7, 8),
+        ]
+
+
+class TestNumpyPath2dToMatplotlibPath:
+    def test_no_paths(self):
+        with pytest.raises(ValueError):
+            npshapes.to_matplotlib_path([])
+
+    def test_line_to(self):
+        p = path.Path()
+        p.line_to((4, 5, 6))
+        p.line_to((7, 8, 6))
+        mpath = npshapes.to_matplotlib_path([npshapes.NumpyPath2d(p)])
+        assert tuple(mpath.codes) == (MC.MOVETO, MC.LINETO, MC.LINETO)
+        assert Vec2.list(mpath.vertices) == [(0, 0), (4, 5), (7, 8)]
+
+    def test_curve3_to(self):
+        p = path.Path()
+        p.curve3_to((4, 0, 2), (2, 1, 7))
+        mpath = npshapes.to_matplotlib_path([npshapes.NumpyPath2d(p)])
+        assert tuple(mpath.codes) == (MC.MOVETO, MC.CURVE3, MC.CURVE3)
+        assert Vec2.list(mpath.vertices) == [(0, 0), (2, 1), (4, 0)]
+
+    def test_curve4_to(self):
+        p = path.Path()
+        p.curve4_to((4, 0, 2), (1, 1, 7), (3, 1, 5))
+        mpath = npshapes.to_matplotlib_path([npshapes.NumpyPath2d(p)])
+        assert tuple(mpath.codes) == (
+            MC.MOVETO,
+            MC.CURVE4,
+            MC.CURVE4,
+            MC.CURVE4,
+        )
+        assert Vec2.list(mpath.vertices) == [(0, 0), (1, 1), (3, 1), (4, 0)]
+
+    def test_two_single_paths(self):
+        p1 = path.Path()
+        p1.line_to((4, 5, 6))
+        p2 = path.Path()
+        p2.line_to((7, 8, 6))
+        mpath = npshapes.to_matplotlib_path(
+            [npshapes.NumpyPath2d(p1), npshapes.NumpyPath2d(p2)]
+        )
+        assert tuple(mpath.codes) == (
+            MC.MOVETO,
+            MC.LINETO,
+            MC.MOVETO,
+            MC.LINETO,
+        )
+        assert Vec2.list(mpath.vertices) == [
+            (0, 0),
+            (4, 5),
+            (0, 0),
+            (7, 8),
+        ]
+
+    def test_one_multi_path(self):
+        p = path.Path()
+        p.line_to((4, 5, 6))
+        p.move_to((0, 0, 0))
+        p.line_to((7, 8, 9))
+
+        mpath = npshapes.to_matplotlib_path([npshapes.NumpyPath2d(p)])
         assert tuple(mpath.codes) == (
             MC.MOVETO,
             MC.LINETO,
