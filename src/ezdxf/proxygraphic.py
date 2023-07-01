4abbd8a02968784b2c6acae120d4b651f3016ebf
mozman <me@mozman.at>
2023-07-01 08:20:10 +0200

    ignore filled flag for polylines in proxy graphic #906

--- src/ezdxf/proxygraphic.py
+++ src/ezdxf/proxygraphic.py
@@ -501,36 +501,37 @@ class ProxyGraphic:
     def _polyline(self, vertices, *, close=False, normal=Z_AXIS):
         # Polyline without bulge values!
         # Current implementation ignores the normal vector!
+        # Polyline ignores the filled flag, see #906
         attribs = self._build_dxf_attribs()
         count = len(vertices)
         if count == 1 or (count == 2 and vertices[0].isclose(vertices[1])):
             attribs["location"] = vertices[0]
             return self._factory("POINT", dxfattribs=attribs)
-
-        if self.fill and count > 2:
-            polyline = self._filled_polygon(vertices, attribs)
-        else:
-            attribs["flags"] = const.POLYLINE_3D_POLYLINE
-            polyline = cast("Polyline", self._factory("POLYLINE", dxfattribs=attribs))
-            polyline.append_vertices(vertices)
-            if close:
-                polyline.close()
-            polyline.new_seqend()
+        attribs["flags"] = const.POLYLINE_3D_POLYLINE
+        polyline = cast("Polyline", self._factory("POLYLINE", dxfattribs=attribs))
+        polyline.append_vertices(vertices)
+        if close:
+            polyline.close()
+        polyline.new_seqend()
         return polyline
 
     def polyline_with_normals(self, data: bytes):
         # Polyline without bulge values!
+        # Polyline ignores the filled flag, see #906
         vertices, normal = self._load_vertices(data, load_normal=True)
         return self._polyline(vertices, normal=normal)
 
     def polyline(self, data: bytes):
         # Polyline without bulge values!
+        # Polyline ignores the filled flag, see #906
         vertices, normal = self._load_vertices(data, load_normal=False)
         return self._polyline(vertices)
 
     def polygon(self, data: bytes):
         # Polyline without bulge values!
         vertices, normal = self._load_vertices(data, load_normal=False)
+        if self.fill:
+            return self._filled_polygon(vertices, self._build_dxf_attribs())
         return self._polyline(vertices, close=True)
 
     def lwpolyline(self, data: bytes):
