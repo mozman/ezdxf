04b9d8f2f83b424dab53b0c68bb90ffdd836de4d
mozman <me@mozman.at>
2023-09-17 07:21:45 +0200

    edit example move_block_base_point.py

--- examples/blocks/move_block_base_point.py
+++ examples/blocks/move_block_base_point.py
@@ -11,40 +11,45 @@ if not CWD.exists():
     CWD = pathlib.Path(".")
 
 
+# This example requires ezdxf v1.1.0 or later.
 def main():
     doc = ezdxf.new()
     msp = doc.modelspace()
 
-    # create a new block definition
+    # create a new block definition, with the default base point at (0, 0):
     circle_block = doc.blocks.new("CIRCLE")
     circle_block.add_circle((100, 100), 3)
-    current_base_point = circle_block.base_point
 
     # add a block reference to the model space
     msp.add_blockref("CIRCLE", (0, 0))
     doc.saveas(CWD / "original.dxf")
 
-    # get new base point from bounding box detection:
+    # get new base point from bounding box of the block definition:
     extents = bbox.extents(circle_block, fast=True)
-    new_base_point = extents.center  # or extents.extmin/extmax ...
-    offset = current_base_point - new_base_point
+    new_base_point = extents.center
+    # extents.extmin is the lower left corner
+    # extents.extmax is the upper right corner
+    # offset vector for block references:
+    offset = new_base_point - circle_block.base_point
 
     circle_block.base_point = new_base_point
     doc.saveas(CWD / "move_base_point.dxf")
 
     # WARNING:
-    # This solution does not update the INSERT entities, therefore the block references
-    # are wrong located!!!
+    # So far the INSERT entities have not been updated, hence the block references are
+    # mislocated!!!
 
     # Update INSERT entities:
     # get ALL block references for block "CIRCLE"
     block_refs = doc.query("INSERT[name=='CIRCLE']")
     # translate all block references by offset vector
-    transform.translate(block_refs, -offset)
+    transform.translate(block_refs, offset)
 
     doc.saveas(CWD / "updated_block_references.dxf")
 
 
 if __name__ == "__main__":
-    assert ezdxf.version >= (1, 1, 0), "This example requires ezdxf v1.1 or newer."
-    main()
+    if ezdxf.version >= (1, 1, 0):
+        main()
+    else:
+        print("This example requires ezdxf v1.1.0 or later.")
