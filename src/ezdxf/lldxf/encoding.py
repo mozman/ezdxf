f4177808bca1e97f1690274e7b3f61be988cb51c
mozman <me@mozman.at>
2023-09-23 13:11:48 +0200

    add MIF decoder #932

--- src/ezdxf/lldxf/encoding.py
+++ src/ezdxf/lldxf/encoding.py
@@ -2,9 +2,11 @@
 # License: MIT License
 import re
 import codecs
+import binascii
 
 surrogate_escape = codecs.lookup_error("surrogateescape")
 BACKSLASH_UNICODE = re.compile(r"(\\U\+[A-F0-9]{4})")
+MIF_ENCODED = re.compile(r"(\\M\+[1-5][A-F0-9]{4})")
 
 
 def dxf_backslash_replace(exc: Exception):
@@ -40,8 +42,7 @@ def _decode(s: str) -> str:
 
 
 def has_dxf_unicode(s: str) -> bool:
-    r"""Detect if string `s` contains encoded DXF unicode characters "\\U+xxxx".
-    """
+    r"""Detect if string `s` contains encoded DXF unicode characters "\\U+xxxx"."""
     return bool(re.search(BACKSLASH_UNICODE, s))
 
 
@@ -49,3 +50,45 @@ def decode_dxf_unicode(s: str) -> str:
     r"""Decode DXF unicode characters "\\U+xxxx" in string `s`."""
 
     return "".join(_decode(part) for part in re.split(BACKSLASH_UNICODE, s))
+
+
+def has_mif_encoding(s: str) -> bool:
+    r"""Detect if string `s` contains encoded MIF encoded characters "\\M+cxxxx"."""
+    return bool(re.search(MIF_ENCODED, s))
+
+
+def decode_mif_to_unicode(s: str) -> str:
+    r"""Decode MIF encoded characters "\\M+cxxxx" in string `s`."""
+    return "".join(_decode_mif(part) for part in re.split(MIF_ENCODED, s))
+
+
+MIF_CODE_PAGE = {
+    # See https://docs.intellicad.org/files/oda/2021_11/oda_drawings_docs/frames.html?frmname=topic&frmfile=FontHandling.html
+    "1": "cp932",  # Japanese (Shift-JIS)
+    "2": "cp950",  # Traditional Chinese (Big 5)
+    "3": "cp949",  # Wansung (KS C-5601-1987)
+    "4": "cp1391",  # Johab (KS C-5601-1992)
+    "5": "cp936",  # Simplified Chinese (GB 2312-80)
+}
+
+
+def _decode_mif(s: str) -> str:
+    if s.startswith(r"\M+"):
+        try:
+            code_page = MIF_CODE_PAGE[s[3]]
+        except KeyError:
+            return s
+        try:
+            codec = codecs.lookup(code_page)
+        except LookupError:
+            return s
+        try:
+            byte_data = binascii.unhexlify(s[4:])
+        except binascii.Error:
+            return s
+        try:
+            return codec.decode(byte_data)[0]
+        except UnicodeDecodeError:
+            return s
+    else:
+        return s
--- tests/test_00_dxf_low_level_structs/test_056_decode_dxf_unicode.py
+++ tests/test_00_dxf_low_level_structs/test_056_decode_dxf_unicode.py
@@ -3,6 +3,9 @@
 
 import pytest
 import ezdxf
+import codecs
+import binascii
+from ezdxf.lldxf.encoding import decode_mif_to_unicode, has_mif_encoding
 
 
 def test_has_dxf_unicode_encoding():
@@ -26,6 +29,18 @@ def test_extra_backslash():
         r"abc\U+039B\\U+0391\\U+0393\\U+0395\\U+03A1xyz"
     )
     assert result == r"abcΛ\Α\Γ\Ε\Ρxyz"
+
+
+def test_has_mif_encoding():
+    assert has_mif_encoding(r"\M+5D7DF\M+5CFDF\M+5BCDC")
+
+
+def test_decode_mif_encoding():
+    s = r"\M+5D7DF\M+5CFDF\M+5BCDC"
+    expected = "走线架"
+    codec = codecs.lookup("cp936")
+    assert codec.decode(binascii.unhexlify("D7DFCFDFBCDC"))[0] == expected
+    assert decode_mif_to_unicode(s) == expected
 
 
 def test_extra_digits():
