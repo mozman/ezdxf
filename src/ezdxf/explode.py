ab120dfdd3fe0b12fff417875a9c68b5ed845e0b
mozman <me@mozman.at>
2023-12-02 13:11:23 +0100

    add support for customizable copy strategies

--- src/ezdxf/explode.py
+++ src/ezdxf/explode.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2020-2022, Manfred Moitzi
+# Copyright (c) 2020-2023, Manfred Moitzi
 # License: MIT License
 from __future__ import annotations
 import logging
@@ -60,6 +60,7 @@ def explode_block_reference(
     target_layout: BaseLayout,
     *,
     redraw_order=False,
+    copy_strategy=default_copy_strategy,
 ) -> EntityQuery:
     """Explode a block reference into DXF primitives.
 
@@ -76,6 +77,7 @@ def explode_block_reference(
         block_ref: Block reference entity (INSERT)
         target_layout: explicit target layout for exploded DXF entities
         redraw_order: create entities in ascending redraw order if ``True``
+        copy_strategy: customizable copy strategy
 
     .. warning::
 
@@ -95,7 +97,7 @@ def explode_block_reference(
 
     def _explode_single_block_ref(block_ref):
         for entity in virtual_block_reference_entities(
-            block_ref, redraw_order=redraw_order
+            block_ref, redraw_order=redraw_order, copy_strategy=copy_strategy
         ):
             dxftype = entity.dxftype()
             target_layout.add_entity(entity)
@@ -162,10 +164,12 @@ def virtual_block_reference_entities(
     *,
     skipped_entity_callback: Optional[Callable[[DXFGraphic, str], None]] = None,
     redraw_order=False,
+    copy_strategy=default_copy_strategy,
 ) -> Iterable[DXFGraphic]:
-    """Yields 'virtual' parts of block reference `block_ref`. This method is meant
-    to examine the block reference entities without the need to explode the
-    block reference. The `skipped_entity_callback()` will be called for all
+    """Yields 'virtual' parts of block reference `block_ref`.
+
+    This method is meant to examine the block reference entities without the need to
+    explode the block reference. The `skipped_entity_callback()` will be called for all
     entities which are not processed, signature:
     :code:`skipped_entity_callback(entity: DXFGraphic, reason: str)`,
     `entity` is the original (untransformed) DXF entity of the block definition,
@@ -179,6 +183,7 @@ def virtual_block_reference_entities(
         skipped_entity_callback: called whenever the transformation of an entity
             is not supported and so was skipped.
         redraw_order: yield entities in ascending redraw order if ``True``
+        copy_strategy: customizable copy strategy
 
     .. warning::
 
@@ -191,19 +196,15 @@ def virtual_block_reference_entities(
     assert block_ref.dxftype() == "INSERT"
     from ezdxf.entities import Ellipse
 
-    skipped_entity_callback = (
-        skipped_entity_callback or default_logging_callback
-    )
+    skipped_entity_callback = skipped_entity_callback or default_logging_callback
 
     def disassemble(layout: BaseLayout) -> Iterable[DXFGraphic]:
-        for entity in (
-            layout.entities_in_redraw_order() if redraw_order else layout
-        ):
+        for entity in layout.entities_in_redraw_order() if redraw_order else layout:
             # Do not explode ATTDEF entities. Already available in Insert.attribs
             if entity.dxftype() == "ATTDEF":
                 continue
             try:
-                copy = entity.copy(copy_strategy=default_copy_strategy)
+                copy = entity.copy(copy_strategy=copy_strategy)
             except CopyNotSupported:
                 if hasattr(entity, "virtual_entities"):
                     yield from entity.virtual_entities()  # type: ignore
@@ -232,9 +233,7 @@ def virtual_block_reference_entities(
                 elif dxftype in {"LWPOLYLINE", "POLYLINE"}:  # has arcs
                     yield from transform(entity.virtual_entities())
                 else:
-                    skipped_entity_callback(
-                        entity, "unsupported non-uniform scaling"
-                    )
+                    skipped_entity_callback(entity, "unsupported non-uniform scaling")
             except InsertTransformationError:
                 # INSERT entity can not be represented in the target coordinate
                 # system defined by transformation matrix `m`.
