96048f703340b3fef311156e0c3515094bffc16b
mozman <me@mozman.at>
2024-02-18 14:36:22 +0100

    remove CppVec3 usage

--- src/ezdxf/acc/construct.pyx
+++ src/ezdxf/acc/construct.pyx
@@ -3,10 +3,21 @@
 # Copyright (c) 2020-2024, Manfred Moitzi
 # License: MIT License
 # type: ignore -- pylance sucks at type-checking cython files
-from typing import Iterable, TYPE_CHECKING, Sequence, Optional, Tuple
+from typing import Iterable, TYPE_CHECKING, Sequence, Optional
 from libc.math cimport fabs
-from .vector cimport Vec2, v2_isclose, Vec3, v3_from_cpp_vec3, isclose
-from ._cpp_vec3 cimport CppVec3
+from .vector cimport (
+    isclose, 
+    Vec2, 
+    v2_isclose, 
+    Vec3, 
+    v3_sub, 
+    v3_add, 
+    v3_mul, 
+    v3_normalize, 
+    v3_cross, 
+    v3_magnitude_sqr,
+    v3_isclose,
+)
 
 import cython
 
@@ -118,14 +129,16 @@ def intersection_line_line_2d(
             return res
     return None
 
-cdef double _determinant(CppVec3 v1, CppVec3 v2, CppVec3 v3):
+cdef double _determinant(Vec3 v1, Vec3 v2, Vec3 v3):
     return v1.x * v2.y * v3.z + v1.y * v2.z * v3.x + \
            v1.z * v2.x * v3.y - v1.z * v2.y * v3.x - \
            v1.x * v2.z * v3.y - v1.y * v2.x * v3.z
 
-def intersection_ray_ray_3d(ray1: Tuple[Vec3, Vec3],
-                            ray2: Tuple[Vec3, Vec3],
-                            double abs_tol=TOLERANCE) -> Sequence[Vec3]:
+def intersection_ray_ray_3d(
+        ray1: tuple[Vec3, Vec3],
+        ray2: tuple[Vec3, Vec3],
+        double abs_tol=TOLERANCE
+    ) -> Sequence[Vec3]:
     """
     Calculate intersection of two 3D rays, returns a 0-tuple for parallel rays,
     a 1-tuple for intersecting rays and a 2-tuple for not intersecting and not
@@ -138,37 +151,36 @@ def intersection_ray_ray_3d(ray1: Tuple[Vec3, Vec3],
 
     """
     # source: http://www.realtimerendering.com/intersections.html#I304
-    cdef CppVec3 o2_o1
-    cdef double det1, det2
-    # Vec3() objects as input are not guaranteed, a hard <Vec3> cast could
-    # crash the interpreter for an invalid input!
-    cdef CppVec3 o1 = Vec3(ray1[0]).to_cpp_vec3()
-    cdef CppVec3 p1 = Vec3(ray1[1]).to_cpp_vec3()
-    cdef CppVec3 o2 = Vec3(ray2[0]).to_cpp_vec3()
-    cdef CppVec3 p2 = Vec3(ray2[1]).to_cpp_vec3()
+    cdef:
+        Vec3 o2_o1
+        double det1, det2
+        Vec3 o1 = Vec3(ray1[0])
+        Vec3 p1 = Vec3(ray1[1])
+        Vec3 o2 = Vec3(ray2[0])
+        Vec3 p2 = Vec3(ray2[1])
+        Vec3 d1 = v3_normalize(v3_sub(p1, o1), 1.0)
+        Vec3 d2 = v3_normalize(v3_sub(p2, o2), 1.0)
+        Vec3 d1xd2 = v3_cross(d1, d2)
+        double denominator = v3_magnitude_sqr(d1xd2)
 
-    cdef CppVec3 d1 = (p1 - o1).normalize(1.0)
-    cdef CppVec3 d2 = (p2 - o2).normalize(1.0)
-    cdef CppVec3 d1xd2 = d1.cross(d2)
-    cdef double denominator = d1xd2.magnitude_sqr()
     if denominator <= abs_tol:
         # ray1 is parallel to ray2
         return tuple()
     else:
-        o2_o1 = o2 - o1
+        o2_o1 = v3_sub(o2, o1)
         det1 = _determinant(o2_o1, d2, d1xd2)
         det2 = _determinant(o2_o1, d1, d1xd2)
         with cython.cdivision(True):  # denominator check is already done
-            p1 = o1 + d1 * (det1 / denominator)
-            p2 = o2 + d2 * (det2 / denominator)
+            p1 = v3_add(o1, v3_mul(d1, (det1 / denominator)))
+            p2 = v3_add(o2, v3_mul(d2, (det2 / denominator)))
 
-        if p1.isclose(p2, abs_tol):
+        if v3_isclose(p1, p2, abs_tol, abs_tol):
             # ray1 and ray2 have an intersection point
-            return v3_from_cpp_vec3(p1),
+            return p1,
         else:
             # ray1 and ray2 do not have an intersection point,
             # p1 and p2 are the points of closest approach on each ray
-            return v3_from_cpp_vec3(p1), v3_from_cpp_vec3(p2)
+            return p1, p2
 
 def arc_angle_span_deg(double start, double end) -> float:
     if isclose(start, end, REL_TOL, DEG_ABS_TOL):
