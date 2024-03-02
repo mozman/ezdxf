8403a6dbecc10c44db94dca6e7ceb503f674672f
mozman <me@mozman.at>
2024-03-02 12:13:44 +0100

    move static setup data to pyproject.toml

--- setup.py
+++ setup.py
@@ -1,12 +1,19 @@
 #!/usr/bin/env python3
 # Copyright (c) 2011-2024 Manfred Moitzi
 # License: MIT License
-import os
 import sys
-from setuptools import setup, find_packages
+from setuptools import setup
 from setuptools import Extension
 
-# setuptools docs: https://setuptools.readthedocs.io/en/latest/setuptools.html
+# setuptools docs: https://setuptools.pypa.io/en/latest/index.html
+# build source distribution
+#
+#   python setup.py sdist --formats=zip,gztar
+#
+# build wheels:
+#
+#   python setup.py bdist_wheel
+#
 # All Cython accelerated modules are optional:
 ext_modules = [
     Extension("ezdxf.acc.vector", ["src/ezdxf/acc/vector.pyx"], optional=True),
@@ -49,86 +56,11 @@ def get_version() -> str:
     raise IOError("__version__ string not found")
 
 
-def read(fname: str) -> str:
-    try:
-        with open(os.path.join(os.path.dirname(__file__), fname)) as f:
-            return f.read()
-    except IOError:
-        return "File '%s' not found.\n" % fname
-
-
-DRAW = ["matplotlib", "PySide6", "PyMuPDF>=1.20.0", "Pillow"]
-DRAW5 = ["matplotlib", "PyQt5", "PyMuPDF>=1.20.0", "Pillow"]
-TEST = ["pytest", "Pillow"]
-DEV = ["setuptools", "wheel", "Cython"]
-
+# static attributes are stored in pyproject.toml
+# https://packaging.python.org/en/latest/guides/writing-pyproject-toml/
+# https://setuptools.pypa.io/en/latest/index.html
 setup(
-    name="ezdxf",
     version=get_version(),
-    description="A Python package to create/manipulate DXF drawings.",
-    author="Manfred Moitzi",
-    url="https://ezdxf.mozman.at",
-    download_url="https://pypi.org/project/ezdxf/",
-    author_email="me@mozman.at",
-    python_requires=">=3.9",
-    package_dir={"": "src"},
-    packages=find_packages("src"),
-    zip_safe=False,
-    package_data={
-        "ezdxf": [
-            "pp/*.html",
-            "pp/*.js",
-            "pp/*.css",
-            "resources/*.png",
-            "py.typed",
-        ]
-    },
-    entry_points={
-        "console_scripts": [
-            "ezdxf = ezdxf.__main__:main",  # ezdxf launcher
-        ]
-    },
-    provides=["ezdxf"],
     cmdclass=commands,
     ext_modules=ext_modules,
-    install_requires=[
-        "pyparsing>=2.0.1",
-        "typing_extensions>=4.6.0",
-        "numpy",
-        "fonttools",
-    ],
-    setup_requires=["setuptools", "wheel"],
-    tests_require=TEST,
-    extras_require={
-        "draw": DRAW,
-        "draw5": DRAW5,
-        "dev": DRAW + TEST + DEV,
-        "dev5": DRAW5 + TEST + DEV,
-    },
-    keywords=["DXF", "CAD"],
-    long_description=read("README.md"),
-    long_description_content_type="text/markdown",
-    platforms="OS Independent",
-    license="MIT License",
-    classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-        "Programming Language :: Python :: 3.11",
-        "Programming Language :: Python :: 3.12",
-        "Programming Language :: Python :: Implementation :: CPython",
-        "Programming Language :: Python :: Implementation :: PyPy",
-        "Intended Audience :: Developers",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-        "Typing :: Typed",
-    ],
 )
-
-# Development Status :: 3 - Alpha
-# Development Status :: 4 - Beta
-# Development Status :: 5 - Production/Stable
-# Development Status :: 6 - Mature
-# Development Status :: 7 - Inactive
