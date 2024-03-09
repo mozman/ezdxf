2487c09c9f669035b9671e04bb90405a1b169c91
mozman <me@mozman.at>
2024-03-09 11:32:31 +0100

    add Point as selection shape

--- src/ezdxf/select.py
+++ src/ezdxf/select.py
@@ -7,24 +7,35 @@ import abc
 
 from ezdxf import bbox
 from ezdxf.entities import DXFEntity
-from ezdxf.math import UVec, BoundingBox2d, AbstractBoundingBox
+from ezdxf.math import UVec, Vec2, BoundingBox2d
 from ezdxf.query import EntityQuery
 
 
-__all__ = ["Window", "inside", "crossing"]
+__all__ = ["SelectionShape", "Window", "Point", "inside", "outside", "crossing"]
 
 
 class SelectionShape(abc.ABC):
-    @abc.abstractmethod
-    def is_inside(self, entity_bbox: AbstractBoundingBox) -> bool: ...
+    """AbstractBaseClass for selection shapes.
+
+    It is guaranteed that all methods get an entity_bbox which has data!
+    """
 
     @abc.abstractmethod
-    def is_crossing(self, entity_bbox: AbstractBoundingBox) -> bool: ...
+    def is_inside(self, entity_bbox: BoundingBox2d) -> bool: ...
+
+    def is_outside(self, entity_bbox: BoundingBox2d) -> bool:
+        return not self.is_inside(entity_bbox)
+
+    @abc.abstractmethod
+    def is_crossing(self, entity_bbox: BoundingBox2d) -> bool: ...
 
 
 class Window(SelectionShape):
     """This selection shape tests entities against a rectangular and axis-aligned 2D
     window.  All entities are projected on the xy-plane.
+
+    The selection tests are performed on the bounding box of the entities.
+    This is a design choice: performance and simplicity over accuracy
 
     Args:
         p1: first corner of the window
@@ -35,12 +46,38 @@ class Window(SelectionShape):
         self._bbox = BoundingBox2d((p1, p2))
 
     @override
-    def is_inside(self, entity_bbox: AbstractBoundingBox) -> bool:
+    def is_inside(self, entity_bbox: BoundingBox2d) -> bool:
         return self._bbox.contains(entity_bbox)
 
     @override
-    def is_crossing(self, entity_bbox: AbstractBoundingBox) -> bool:
+    def is_crossing(self, entity_bbox: BoundingBox2d) -> bool:
         return self._bbox.has_overlap(entity_bbox)
+
+
+class Point(SelectionShape):
+    """This selection shape tests entities against a single point.  All entities are
+    projected on the xy-plane.
+
+    An entity is selected when the selection point is inside the bounding box of the entity.
+    This is a design choice: performance and simplicity over accuracy
+
+    By definition, nothing can be inside a dimensionless point and therefore everything 
+    is outside a point.
+
+    Args:
+        point: selection point
+    """
+
+    def __init__(self, point: UVec):
+        self._point = Vec2(point)
+
+    @override
+    def is_inside(self, entity_bbox: BoundingBox2d) -> bool:
+        return False
+
+    @override
+    def is_crossing(self, entity_bbox: BoundingBox2d) -> bool:
+        return entity_bbox.inside(self._point)
 
 
 def inside(
@@ -57,6 +94,23 @@ def inside(
 
     """
     return select_by_bbox(entities, shape.is_inside, cache)
+
+
+def outside(
+    entities: Iterable[DXFEntity],
+    shape: SelectionShape,
+    cache: bbox.Cache | None = None,
+) -> EntityQuery:
+    """Returns all entities that are located outside the selection shape.
+
+    Args:
+        entities: iterable of DXFEntities
+        shape: seclection shape
+        cache: optional bounding box cache
+
+    """
+    return select_by_bbox(entities, shape.is_outside, cache)
+
 
 def crossing(
     entities: Iterable[DXFEntity],
@@ -76,15 +130,15 @@ def crossing(
 
 def select_by_bbox(
     entities: Iterable[DXFEntity],
-    test_func: Callable[[AbstractBoundingBox], bool],
+    test_func: Callable[[BoundingBox2d], bool],
     cache: bbox.Cache | None = None,
 ) -> EntityQuery:
-    """Calculates the bounding box for each entity and returns all entities for that the 
+    """Calculates the bounding box for each entity and returns all entities for that the
     test function returns ``True``.
 
     Args:
         entities: iterable of DXFEntities
-        func: test function which takes the bounding box of the entity as input and 
+        func: test function which takes the bounding box of the entity as input and
             returns ``True`` if the entity is part of the selection.
         cache: optional bounding box cache
 
@@ -95,6 +149,6 @@ def select_by_bbox(
         extents = bbox.extents((entity,), fast=True, cache=cache)
         if not extents.has_data:
             continue
-        if test_func(extents):
+        if test_func(BoundingBox2d(extents)):
             selection.append(entity)
     return EntityQuery(selection)
--- tests/test_05_tools/test_543_select.py
+++ tests/test_05_tools/test_543_select.py
@@ -44,6 +44,16 @@ class TestWindow:
         assert entities[0].dxftype() == "POINT"
         assert entities[1].dxftype() == "LINE"
 
+    def test_outside_all(self, msp: Modelspace):
+        window = select.Window((-5, -5), (5, 5))
+        entities = select.outside(msp, window)
+        assert len(entities) == 0
+
+    def test_outside_all_except_point(self, msp: Modelspace):
+        window = select.Window((-1, 0), (1, 2))
+        entities = select.outside(msp, window)
+        assert len(entities) == 3
+
     def test_crossing_all(self, msp: Modelspace):
         """The window just overlaps the bounding boxes of the CIRCLE and the
         LWPOLYLINE not the geometry itself.
@@ -58,6 +68,33 @@ class TestWindow:
         entities = select.crossing(msp, window)
         assert len(entities) == 1
         assert entities[0].dxftype() == "CIRCLE"
+
+
+class TestPoint:
+    """The selection functions are testing only the bounding boxes of entities.
+
+    This is a design choice: performance and simplicity over accuracy
+    """
+
+    def test_none_inside_all(self, msp: Modelspace):
+        """By definition, nothing can be inside a dimensionless point."""
+        point = select.Point((0, 0))
+        entities = select.inside(msp, point)
+        assert len(entities) == 0
+
+    def test_everything_is_outside(self, msp: Modelspace):
+        """By definition, nothing can be inside a dimensionless point and therefore 
+        everything is outside a point.
+        """
+        point = select.Point((0, 1))
+        entities = select.outside(msp, point)
+        assert len(entities) == 4
+
+    def test_crossing_all(self, msp: Modelspace):
+        """The point is inside of all entity bounding boxes."""
+        point = select.Point((0, 1))
+        entities = select.crossing(msp, point)
+        assert len(entities) == 4
 
 
 if __name__ == "__main__":
