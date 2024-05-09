90334285ad580a39d82c5f439e739029717b9cc6
mozman <me@mozman.at>
2024-05-09 12:42:44 +0200

    fix typing compatibility issue

--- src/ezdxf/acis/typehints.py
+++ src/ezdxf/acis/typehints.py
@@ -1,8 +1,7 @@
 # Copyright (c) 2024, Manfred Moitzi
 # License: MIT License
-
 from __future__ import annotations
-from typing import Sequence
+from typing import Sequence, Union
 from typing_extensions import TypeAlias
 
-EncodedData: TypeAlias = str | Sequence[str] | bytes | bytearray
+EncodedData: TypeAlias = Union[str, Sequence[str], bytes, bytearray]
