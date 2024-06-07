9bae4db297bec40cb940a143e80471aef1f7def8
mozman <me@mozman.at>
2024-06-07 12:01:35 +0200

    refactor polyline_points()

--- src/ezdxf/edgesmith.py
+++ src/ezdxf/edgesmith.py
@@ -16,6 +16,7 @@ The complementary module to ezdxf.edgeminer.
 """
 from __future__ import annotations
 from typing import Iterator, Iterable, Sequence, Any
+from typing_extensions import TypeAlias
 import math
 import functools
 
@@ -43,62 +44,75 @@ DEG_TOL = 1e-9  # angles in degree
 RAD_TOL = 1e-9  # angles in radians
 
 
+@functools.singledispatch
 def is_closed_entity(entity: et.DXFEntity) -> bool:
     """Returns ``True`` if the given entity represents a closed loop."""
-    if isinstance(entity, et.Arc):  # Arc inherits from Circle!
-        radius = abs(entity.dxf.radius)
-        start_angle = entity.dxf.start_angle
-        end_angle = entity.dxf.end_angle
-        angle_span = arc_angle_span_deg(start_angle, end_angle)
-        return abs(radius) > LEN_TOL and math.isclose(
-            angle_span, 360.0, abs_tol=LEN_TOL
-        )
-    if isinstance(entity, et.Circle):
-        return abs(entity.dxf.radius) > LEN_TOL
+    return False
 
-    if isinstance(entity, et.Ellipse):
-        start_param = entity.dxf.start_param
-        end_param = entity.dxf.end_param
-        span = ellipse_param_span(start_param, end_param)
-        if not math.isclose(span, math.tau, abs_tol=RAD_TOL):
-            return False
-        return True
 
-    if isinstance(entity, et.Spline):
-        try:
-            bspline = entity.construction_tool()
-        except ValueError:
-            return False
-        control_points = bspline.control_points
-        if len(control_points) < 3:
-            return False
-        start = control_points[0]
-        end = control_points[-1]
-        return start.isclose(end, abs_tol=LEN_TOL)
+@is_closed_entity.register(et.Arc)
+def _is_closed_arc(entity: et.Arc) -> bool:
+    radius = abs(entity.dxf.radius)
+    start_angle = entity.dxf.start_angle
+    end_angle = entity.dxf.end_angle
+    angle_span = arc_angle_span_deg(start_angle, end_angle)
+    return abs(radius) > LEN_TOL and math.isclose(angle_span, 360.0, abs_tol=LEN_TOL)
 
-    if isinstance(entity, et.LWPolyline):
-        if len(entity) < 1:
-            return False
-        if entity.closed is True:
-            return True
-        start = Vec2(entity.lwpoints[0][:2])
-        end = Vec2(entity.lwpoints[-1][:2])
-        return start.isclose(end, abs_tol=LEN_TOL)
 
-    if isinstance(entity, et.Polyline):
-        if entity.is_2d_polyline or entity.is_3d_polyline:
-            # Note: does not check if all vertices of a 3D polyline are placed on a
-            # common plane.
-            vertices = entity.vertices
-            if len(vertices) < 2:
-                return False
-            if entity.is_closed:
-                return True
-            p0: Vec3 = vertices[0].dxf.location  # type: ignore
-            p1: Vec3 = vertices[-1].dxf.location  # type: ignore
-            if p0.isclose(p1, abs_tol=LEN_TOL):
-                return True
+@is_closed_entity.register(et.Circle)
+def _is_closed_circle(entity: et.Circle) -> bool:
+    return abs(entity.dxf.radius) > LEN_TOL
+
+
+@is_closed_entity.register(et.Ellipse)
+def _is_closed_ellipse(entity: et.Ellipse) -> bool:
+    start_param = entity.dxf.start_param
+    end_param = entity.dxf.end_param
+    span = ellipse_param_span(start_param, end_param)
+    if not math.isclose(span, math.tau, abs_tol=RAD_TOL):
         return False
+    return True
+
+
+@is_closed_entity.register(et.Spline)
+def _is_closed_spline(entity: et.Spline) -> bool:
+    try:
+        bspline = entity.construction_tool()
+    except ValueError:
+        return False
+    control_points = bspline.control_points
+    if len(control_points) < 3:
+        return False
+    start = control_points[0]
+    end = control_points[-1]
+    return start.isclose(end, abs_tol=LEN_TOL)
+
+
+@is_closed_entity.register(et.LWPolyline)
+def _is_closed_lwpolyline(entity: et.LWPolyline) -> bool:
+    if len(entity) < 1:
+        return False
+    if entity.closed is True:
+        return True
+    start = Vec2(entity.lwpoints[0][:2])
+    end = Vec2(entity.lwpoints[-1][:2])
+    return start.isclose(end, abs_tol=LEN_TOL)
+
+
+@is_closed_entity.register(et.Polyline)
+def _is_closed_polyline2d(entity: et.Polyline) -> bool:
+    if entity.is_2d_polyline or entity.is_3d_polyline:
+        # Note: does not check if all vertices of a 3D polyline are placed on a
+        # common plane.
+        vertices = entity.vertices
+        if len(vertices) < 2:
+            return False
+        if entity.is_closed:
+            return True
+        p0: Vec3 = vertices[0].dxf.location  # type: ignore
+        p1: Vec3 = vertices[-1].dxf.location  # type: ignore
+        if p0.isclose(p1, abs_tol=LEN_TOL):
+            return True
     return False
 
 
@@ -220,6 +234,8 @@ def lwpolyline_from_chain(
         - LINE entities are added as line segments
         - ARC entities are added as bulges
         - ELLIPSE entities with a ratio of 1.0 are added as bulges
+        - LWPOLYLINE will be merged
+        - 2D POLYLINE will be merged
         - Everything else will be added as line segment from Edge.start to Edge.end
         - Gaps between edges are connected by line segments.
 
@@ -242,6 +258,8 @@ def polyline2d_from_chain(
         - LINE entities are added as line segments
         - ARC entities are added as bulges
         - ELLIPSE entities with a ratio of 1.0 are added as bulges
+        - LWPOLYLINE will be merged
+        - 2D POLYLINE will be merged
         - Everything else will be added as line segment from Edge.start to Edge.end
         - Gaps between edges are connected by line segments.
 
@@ -249,26 +267,46 @@ def polyline2d_from_chain(
     polyline = et.Polyline.new(dxfattribs=dxfattribs)
     if len(edges) == 0:
         return polyline
-    polyline.append_formatted_vertices(polyline_points(edges), format="vb")  # type: ignore
+    polyline.append_formatted_vertices(polyline_points(edges), format="vb")
     return polyline
 
 
-def polyline_points(edges: Sequence[em.Edge]) -> Iterable[tuple[Vec2, float]]:
+BulgePoints: TypeAlias = list[tuple[Vec2, float]]
+
+
+def polyline_points(edges: Sequence[em.Edge]) -> BulgePoints:
     """Returns the polyline points to create a :class:`LWPolyline` or a
     2D :class.`Polyline` entity.
     """
 
-    def extend(pts: Iterable[tuple[Vec2, float]]) -> None:
-        for pnt, bulge in pts:
+    def extend(pts: BulgePoints) -> None:
+        if len(pts) < 2:
+            return
+        # ignore first vertex
+        first = pts[0]
+        bulges[-1] = first[1]
+        for pnt, bulge in pts[1:]:
             points.append(pnt)
             bulges.append(bulge)
+
+    def reverse(pts: BulgePoints) -> BulgePoints:
+        if len(pts) < 2:
+            return pts
+        pts.reverse()
+        bulges = [pnt[1] for pnt in pts]
+        if any(bulges):
+            # shift bulge values to previous vertex
+            first = bulges.pop(0)
+            bulges.append(first)
+            return list(zip((pnt[0] for pnt in pts), bulges))
+        return pts
 
     # bulge value is stored in the start vertex of the curved segment
     points: list[Vec3] = [edges[0].start]
     bulges: list[float] = [0.0]
     for edge in edges:
         current_end = points[-1]
-        if not edge.start.isclose(current_end):
+        if edge.start.distance(current_end) < LEN_TOL:
             current_end = edge.start
             points.append(current_end)
             bulges.append(0.0)
@@ -285,18 +323,17 @@ def polyline_points(edges: Sequence[em.Edge]) -> Iterable[tuple[Vec2, float]]:
                 bulge = bulge_from_arc_angle(span)
                 bulges[-1] = -bulge if edge.is_reverse else bulge
         elif isinstance(entity, et.LWPolyline):
-            points = list(entity.get_points(format="vb"))
+            vertices: BulgePoints = list(entity.get_points(format="vb"))  # type: ignore
             if edge.is_reverse:
-                points.reverse()
-            extend(points)
+                vertices = reverse(vertices)
+            extend(vertices)
             continue
         elif isinstance(entity, et.Polyline) and entity.is_2d_polyline:
-            points = [(Vec2(v.dxf.location), v.dxf.bulge) for v in entity.vertices]
+            vertices = [(Vec2(v.dxf.location), v.dxf.bulge) for v in entity.vertices]
             if edge.is_reverse:
-                points.reverse()
-            extend(points)
+                vertices = reverse(vertices)
+            extend(vertices)
             continue
-
         points.append(edge.end)
         bulges.append(0.0)
-    return zip(points, bulges)
+    return list(zip(points, bulges))
