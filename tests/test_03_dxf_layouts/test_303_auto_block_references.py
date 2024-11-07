0b87729180c58e6f997770dc9f33e3d910ba89dd
mozman <me@mozman.at>
2024-11-07 10:03:33 +0100

    add tests for multi-line ATTRIB

--- tests/test_03_dxf_layouts/test_303_auto_block_references.py
+++ tests/test_03_dxf_layouts/test_303_auto_block_references.py
@@ -1,8 +1,9 @@
-# Created: 03.04.2011
-# Copyright (c) 2011-2020, Manfred Moitzi
+# Copyright (c) 2011-2024, Manfred Moitzi
 # License: MIT License
 import pytest
 import ezdxf
+from ezdxf.lldxf import const
+from ezdxf.entities import MText, Insert
 
 
 @pytest.fixture
@@ -25,18 +26,21 @@ def test_auto_blockref(doc, block):
     autoblock = doc.blocks[blockref.dxf.name]
     entities = list(autoblock)
     insert = entities[0]
-    assert insert.dxftype() == "INSERT"
+    assert isinstance(insert, Insert) is True
 
     attrib1, attrib2 = insert.attribs
     assert attrib1.dxftype() == "ATTRIB"
     assert attrib1.dxf.tag == "TAG1"
     assert attrib1.dxf.text == "text1"
+    assert attrib1.dxf.attribute_type == const.ATTRIB_TYPE_SINGLE_LINE
+
     assert attrib2.dxftype() == "ATTRIB"
     assert attrib2.dxf.tag == "TAG2"
     assert attrib2.dxf.text == "text2"
+    assert attrib2.dxf.attribute_type == const.ATTRIB_TYPE_SINGLE_LINE
 
 
-def test_blockref_with_attribs(doc, block):
+def test_add_auto_attribs(doc, block):
     values = {"TAG1": "text1", "TAG2": "text2"}
     msp = doc.modelspace()
     blockref = msp.add_blockref("TEST", (0, 0)).add_auto_attribs(values)
@@ -46,12 +50,15 @@ def test_blockref_with_attribs(doc, block):
     assert attrib1.dxftype() == "ATTRIB"
     assert attrib1.dxf.tag == "TAG1"
     assert attrib1.dxf.text == "text1"
+    assert attrib1.dxf.attribute_type == const.ATTRIB_TYPE_SINGLE_LINE
+
     assert attrib2.dxftype() == "ATTRIB"
     assert attrib2.dxf.tag == "TAG2"
     assert attrib2.dxf.text == "text2"
+    assert attrib2.dxf.attribute_type == const.ATTRIB_TYPE_SINGLE_LINE
 
 
-def test_auto_blockref_with_default_text(doc, block):
+def test_add_auto_attribs_with_default_text(doc, block):
     msp = doc.modelspace()
     blockref = msp.add_blockref("TEST", (0, 0)).add_auto_attribs({})
     attrib1, attrib2 = blockref.attribs
@@ -62,8 +69,8 @@ def test_auto_blockref_with_default_text(doc, block):
 def test_add_auto_attribs_with_missing_tag():
     doc = ezdxf.new()
     msp = doc.modelspace()
-    block = doc.blocks.new("TEST")
-    attdef = block.add_attdef("TAG1", (0, 0))
+    blocks = doc.blocks.new("TEST")
+    attdef = blocks.add_attdef("TAG1", (0, 0))
     attdef.dxf.discard("tag")  # simulate erroneous DXF input
 
     blockref = msp.add_blockref("TEST", (0, 0)).add_auto_attribs({"TAG1": "Test"})
@@ -73,8 +80,8 @@ def test_add_auto_attribs_with_missing_tag():
 def test_add_auto_attribs_with_missing_location():
     doc = ezdxf.new()
     msp = doc.modelspace()
-    block = doc.blocks.new("TEST")
-    attdef = block.add_attdef("TAG1", (0, 0))
+    blocks = doc.blocks.new("TEST")
+    attdef = blocks.add_attdef("TAG1", (0, 0))
     attdef.dxf.discard("insert")  # simulate erroneous DXF input
 
     blockref = msp.add_blockref("TEST", (0, 0)).add_auto_attribs({"TAG1": "Test"})
@@ -125,3 +132,57 @@ def test_attdef_setter_properties(block):
     assert attrib.is_preset is False
     attrib.is_preset = True
     assert attrib.is_preset is True
+
+
+class TestMultiLineAttribs:
+    @pytest.fixture
+    def doc(self):
+        return ezdxf.new()
+
+    @pytest.fixture
+    def block(self, doc):
+        block = doc.blocks.new("TEST")
+        attdef = block.add_attdef("TAG1")
+        # Note: \n is converted to \P automatically at DXF export
+        mtext = MText.new(dxfattribs={"insert": (1, 1), "text": "default\ntext"})
+        attdef.embed_mtext(mtext)
+        return block
+
+    def test_has_multi_line_attdef(self, block):
+        attdef = block.get_attdef("TAG1")
+        assert attdef is not None
+        assert attdef.has_embedded_mtext_entity is True
+        assert attdef.dxf.attribute_type == const.ATTDEF_TYPE_MULTI_LINE
+
+    def test_multi_line_attdef_properties(self, block):
+        attdef = block.get_attdef("TAG1")
+        assert attdef is not None
+        assert attdef.dxf.insert == (1, 1)
+        assert attdef.dxf.text == "default", "expected just first line"
+
+        mtext = attdef.virtual_mtext_entity()
+        assert isinstance(mtext, MText) is True
+
+        assert mtext.dxf.insert == (1, 1)
+        assert mtext.dxf.text == "default\ntext", "expected all lines"
+
+    def test_add_auto_multi_line_attrib(self, doc, block):
+        msp = doc.modelspace()
+        insert = msp.add_blockref(block.name, insert=(2, 3))
+        assert insert is not None
+
+        insert.add_auto_attribs({"TAG1": "MultiLine\nAttribute"})
+
+        attrib = insert.attribs[0]
+        assert attrib is not None
+
+        assert attrib.dxf.insert == (3, 4)
+        assert attrib.dxf.text == "MultiLine", "expected just first line"
+        assert attrib.has_embedded_mtext_entity is True
+        assert attrib.dxf.attribute_type == const.ATTRIB_TYPE_MULTI_LINE
+
+        mtext = attrib.virtual_mtext_entity()
+        assert isinstance(mtext, MText) is True
+
+        assert mtext.dxf.insert == (3, 4)
+        assert mtext.dxf.text == "MultiLine\nAttribute", "expected all lines"
