4b5e6d7e503085e3e75cf4c5461ab3025df77e75
mozman <me@mozman.at>
2024-12-15 12:58:44 +0100

    add example for block with multiline-attrib

--- examples/blocks/insert_with_mtext_attrib.py
+++ examples/blocks/insert_with_mtext_attrib.py
@@ -9,7 +9,37 @@ if not CWD.exists():
     CWD = pathlib.Path(".")
 
 
-def main():
+def block_definition_with_multiline_attdef():
+    doc = ezdxf.new(dxfversion="R2018")
+    block = doc.blocks.new(name="MY_BLOCK")
+
+    # Text, location and height not needed, because copied from MTEXT entity
+    attdef = block.add_attdef("TAG1")
+    # Define the multiline attribute as MTEXT entity
+    mtext = block.add_mtext("Default Value", dxfattribs={"char_height": 0.25})
+    mtext.set_location((0, 0))
+    # Set ATTDEF content from MTEXT entity
+    attdef.set_mtext(mtext)
+
+    attdef = block.add_attdef("TAG2", (0, -1))
+    # reuse existing MTEXT entity
+    mtext.text = "Another Default"
+    mtext.set_location((0, -1))
+    # Set ATTDEF content from MTEXT entity and destroy the MTEXT entity
+    attdef.embed_mtext(mtext)
+
+    # Usage of add_auto_attribs() with multiline ATTDEFs:
+    msp = doc.modelspace()
+    insert = msp.add_blockref("MY_BLOCK", insert=(5, 5))
+    attribs = {
+        "TAG1": "TAG1-Line1\nTAG1-Line2",
+        "TAG2": "TAG2-Line3\nTAG2-Line4",
+    }
+    insert.add_auto_attribs(attribs)
+    doc.saveas(CWD / "block_with_multiline_attdef.dxf")
+
+
+def attach_multiline_attrib_to_block_reference():
     doc = ezdxf.new(dxfversion="R2018")
     # create a new block definition:
     block = doc.blocks.new("TEST")
@@ -27,6 +57,11 @@ def main():
     attrib.embed_mtext(m_text)
 
     doc.saveas(CWD / "insert_with_mtext_attribute.dxf")
+
+
+def main():
+    attach_multiline_attrib_to_block_reference()
+    block_definition_with_multiline_attdef()
 
 
 if __name__ == "__main__":
