b026ff1467c584333f04e7ce879ed62218b9c305
mozman <me@mozman.at>
2026-03-30 21:04:30 +0200

    import Self from typing_extensions

--- src/ezdxf/entities/temporary_transform.py
+++ src/ezdxf/entities/temporary_transform.py
@@ -1,7 +1,8 @@
 # Copyright (c) 2024, Manfred Moitzi
 # License: MIT License
 from __future__ import annotations
-from typing import TYPE_CHECKING, Self
+from typing import TYPE_CHECKING
+from typing_extensions import Self
 import abc
 
 if TYPE_CHECKING:
